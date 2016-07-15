8c9e14f772b6c623f2c0bfcb7e645f56e6f9a1cb
Crows of War <ofwarcrows@gmail.com>
2016-07-15 13:45:43 -0500

    Added javadocs to IEntityProperty and EntityPropertyDataWatcher

--- src/main/java/com/crowsofwar/avatar/common/entityproperty/IEntityProperty.java
+++ src/main/java/com/crowsofwar/avatar/common/entityproperty/IEntityProperty.java
@@ -1,9 +1,25 @@
 package com.crowsofwar.avatar.common.entityproperty;
 
+/**
+ * The basic interface for an entity property. A property
+ * is of a certain type, and can be retrieved and set.
+ * Different implementations would have specific details
+ * about how the property should be get and set.
+ * 
+ * @param <T> The type of object that the property stores.
+ */
 public interface IEntityProperty<T> {
 	
+	/**
+	 * Get the current value from the entity property.
+	 * @return
+	 */
 	T getValue();
 	
+	/**
+	 * Set the value of the property.
+	 * @param value
+	 */
 	void setValue(T value);
 	
 }
