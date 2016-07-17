869b53ad2c6c46646f79e1199857942e0c38adeb
Crows of War <ofwarcrows@gmail.com>
2016-07-17 12:33:10 -0500

    trying new texture? not added yet

--- src/main/java/com/crowsofwar/avatar/client/particles/ParticleTest.java
+++ src/main/java/com/crowsofwar/avatar/client/particles/ParticleTest.java
@@ -2,24 +2,43 @@ package com.crowsofwar.avatar.client.particles;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.EntityFX;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 @SideOnly(Side.CLIENT)
 public class ParticleTest extends EntityFX
 {
+	
+	private static final ResourceLocation TEXTURE = new ResourceLocation("avatarmod", "textures/particles/test.png");
+	private static final ResourceLocation VANILLA_PARTICLES = new ResourceLocation("textures/particle/particles.png");
+	
     float smokeParticleScale;
     private static final String __OBFID = "CL_00000924";
 
-    public ParticleTest(World world, double x, double y, double z, double motionX, double motionY, double motionZ)
+    public ParticleTest(World p_i1198_1_, double p_i1198_2_, double p_i1198_4_, double p_i1198_6_, double p_i1198_8_, double p_i1198_10_, double p_i1198_12_)
     {
-        this(world, x, y, z, motionX, motionY, motionZ, 1);
-    }
+    	super(p_i1198_1_, p_i1198_2_, p_i1198_4_, p_i1198_6_, p_i1198_8_, p_i1198_10_, p_i1198_12_);
+        this.particleRed = 1.0F;
+        this.particleGreen = 1.0F;
+        this.particleBlue = 1.0F;
+        this.setParticleTextureIndex(32);
+        this.setSize(0.02F, 0.02F);
+        this.particleScale *= this.rand.nextFloat() * 0.6F + 0.2F;
+        this.motionX = p_i1198_8_ * 0.20000000298023224D + (double)((float)(Math.random() * 2.0D - 1.0D) * 0.02F);
+        this.motionY = p_i1198_10_ * 0.20000000298023224D + (double)((float)(Math.random() * 2.0D - 1.0D) * 0.02F);
+        this.motionZ = p_i1198_12_ * 0.20000000298023224D + (double)((float)(Math.random() * 2.0D - 1.0D) * 0.02F);
+        this.particleMaxAge = (int)(8.0D / (Math.random() * 0.8D + 0.2D));    }
 
-    public ParticleTest(World world, double x, double y, double z, double motionX, double motionY, double motionZ, float ageScale)
+    public ParticleTest(World world, double xe, double y, double z, double motionX, double motionY, double motionZ, float ageScale)
     {
-        super(world, x, y, z, 0.0D, 0.0D, 0.0D);
+        super(world, xe, y, z, 0.0D, 0.0D, 0.0D);
+        setParticleTextureIndex(32);
+        setSize(.02f, .02f);
         this.motionX *= 0.10000000149011612D;
         this.motionY *= 0.10000000149011612D;
         this.motionZ *= 0.10000000149011612D;
@@ -35,22 +54,30 @@ public class ParticleTest extends EntityFX
         this.noClip = false;
     }
 
-    public void renderParticle(Tessellator p_70539_1_, float p_70539_2_, float p_70539_3_, float p_70539_4_, float p_70539_5_, float p_70539_6_, float p_70539_7_)
+    public void renderParticle(Tessellator t, float p_70539_2_, float p_70539_3_, float p_70539_4_, float p_70539_5_, float p_70539_6_, float p_70539_7_)
     {
-        float f6 = ((float)this.particleAge + p_70539_2_) / (float)this.particleMaxAge * 32.0F;
-
-        if (f6 < 0.0F)
-        {
-            f6 = 0.0F;
-        }
-
-        if (f6 > 1.0F)
-        {
-            f6 = 1.0F;
-        }
-
-        this.particleScale = this.smokeParticleScale * f6;
-        super.renderParticle(p_70539_1_, p_70539_2_, p_70539_3_, p_70539_4_, p_70539_5_, p_70539_6_, p_70539_7_);
+    	t.draw();
+    	Minecraft.getMinecraft().renderEngine.bindTexture(TEXTURE);
+    	t.startDrawingQuads();
+    	super.renderParticle(t, p_70539_2_, p_70539_3_, p_70539_4_, p_70539_5_, p_70539_6_, p_70539_7_);
+    	t.draw();
+    	Minecraft.getMinecraft().renderEngine.bindTexture(VANILLA_PARTICLES);
+    	t.startDrawingQuads();
+    	
+//        float f6 = ((float)this.particleAge + p_70539_2_) / (float)this.particleMaxAge * 32.0F;
+//
+//        if (f6 < 0.0F)
+//        {
+//            f6 = 0.0F;
+//        }
+//
+//        if (f6 > 1.0F)
+//        {
+//            f6 = 1.0F;
+//        }
+//
+//        this.particleScale = this.smokeParticleScale * f6;
+        super.renderParticle(t, p_70539_2_, p_70539_3_, p_70539_4_, p_70539_5_, p_70539_6_, p_70539_7_);
     }
 
     /**
@@ -58,33 +85,51 @@ public class ParticleTest extends EntityFX
      */
     public void onUpdate()
     {
-        this.prevPosX = this.posX;
+    	this.prevPosX = this.posX;
         this.prevPosY = this.posY;
         this.prevPosZ = this.posZ;
+        this.motionY += 0.002D;
+        this.moveEntity(this.motionX, this.motionY, this.motionZ);
+        this.motionX *= 0.8500000238418579D;
+        this.motionY *= 0.8500000238418579D;
+        this.motionZ *= 0.8500000238418579D;
 
-        if (this.particleAge++ >= this.particleMaxAge)
+        if (this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ)).getMaterial() != Material.water)
+        {
+//            this.setDead();
+        }
+
+        if (this.particleMaxAge-- <= 0)
         {
             this.setDead();
         }
-
-        this.setParticleTextureIndex(7 - this.particleAge * 8 / this.particleMaxAge);
-        this.motionY += 0.004D;
-        this.moveEntity(this.motionX, this.motionY, this.motionZ);
-
-        if (this.posY == this.prevPosY)
-        {
-            this.motionX *= 1.1D;
-            this.motionZ *= 1.1D;
-        }
-
-        this.motionX *= 0.9599999785423279D;
-        this.motionY *= 0.9599999785423279D;
-        this.motionZ *= 0.9599999785423279D;
-
-        if (this.onGround)
-        {
-            this.motionX *= 0.699999988079071D;
-            this.motionZ *= 0.699999988079071D;
-        }
+//        this.prevPosX = this.posX;
+//        this.prevPosY = this.posY;
+//        this.prevPosZ = this.posZ;
+//
+//        if (this.particleAge++ >= this.particleMaxAge)
+//        {
+//            this.setDead();
+//        }
+//
+//        this.setParticleTextureIndex(7 - this.particleAge * 8 / this.particleMaxAge);
+//        this.motionY += 0.004D;
+//        this.moveEntity(this.motionX, this.motionY, this.motionZ);
+//
+//        if (this.posY == this.prevPosY)
+//        {
+//            this.motionX *= 1.1D;
+//            this.motionZ *= 1.1D;
+//        }
+//
+//        this.motionX *= 0.9599999785423279D;
+//        this.motionY *= 0.9599999785423279D;
+//        this.motionZ *= 0.9599999785423279D;
+//
+//        if (this.onGround)
+//        {
+//            this.motionX *= 0.699999988079071D;
+//            this.motionZ *= 0.699999988079071D;
+//        }
     }
 }
