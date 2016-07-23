ecf15400c6e9755fe2450b9a5d61b8f7aa0303f3
CrowsOfWar <ofwarcrows@gmail.com>
2016-07-23 10:29:13 -0500

    Added splashes on land- but not very noticeable because of splashes already there

--- src/main/java/com/crowsofwar/avatar/common/entity/EntityWaterArc.java
+++ src/main/java/com/crowsofwar/avatar/common/entity/EntityWaterArc.java
@@ -1,5 +1,7 @@
 package com.crowsofwar.avatar.common.entity;
 
+import java.util.Random;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
@@ -19,6 +21,51 @@ public class EntityWaterArc extends EntityArc {
 	
 	@Override
 	protected void onCollideWithBlock() {
+		
+		if (worldObj.isRemote) {
+			Random random = new Random();
+			
+			double xVel = 0, yVel = 0, zVel = 0;
+			double offX = 0, offY = 0, offZ = 0;
+			
+			if (isCollidedVertically) {
+				
+				xVel = 5;
+				yVel = 3.5;
+				zVel = 5;
+				offX = 0;
+				offY = 0.6;
+				offZ = 0;
+				
+			} else {
+				
+				xVel = 7;
+				yVel = 2;
+				zVel = 7;
+				offX = 0.6;
+				offY = 0.2;
+				offZ = 0.6;
+				
+			}
+			
+			xVel *= 0.0;
+			yVel *= 0.0;
+			zVel *= 0.0;
+			
+			int particles = random.nextInt(3) + 4;
+			for (int i = 0; i < particles; i++) {
+				
+				worldObj.spawnParticle("splash", 
+						posX + random.nextGaussian() * offX,
+						posY + random.nextGaussian() * offY + 0.2,
+						posZ + random.nextGaussian() * offZ,
+						random.nextGaussian() * xVel,
+						random.nextGaussian() * yVel,
+						random.nextGaussian() * zVel);
+				
+			}
+			
+		}
 		
 	}
 	
