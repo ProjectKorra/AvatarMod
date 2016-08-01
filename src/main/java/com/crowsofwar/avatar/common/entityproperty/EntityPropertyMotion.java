2e8b20db7b964b5f81f5b0fb2618a449c0cca181
Crows of War <ofwarcrows@gmail.com>
2016-08-01 10:11:51 -0500

    EntityPropertyMotion is m/s instead of m/tick

--- src/main/java/com/crowsofwar/avatar/common/entityproperty/EntityPropertyMotion.java
+++ src/main/java/com/crowsofwar/avatar/common/entityproperty/EntityPropertyMotion.java
@@ -3,6 +3,13 @@ package com.crowsofwar.avatar.common.entityproperty;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Vec3;
 
+/**
+ * An entity property which allows simple access to
+ * the entity's motion vector by manipulating the
+ * motionX, motionY, and motionZ fields. All methods
+ * for velocity are in m/s.
+ *
+ */
 public class EntityPropertyMotion implements IEntityProperty<Vec3> {
 	
 	private final Vec3 internalVelocity;
@@ -15,17 +22,17 @@ public class EntityPropertyMotion implements IEntityProperty<Vec3> {
 	
 	@Override
 	public Vec3 getValue() {
-		internalVelocity.xCoord = entity.motionX;
-		internalVelocity.yCoord = entity.motionY;
-		internalVelocity.zCoord = entity.motionZ;
+		internalVelocity.xCoord = entity.motionX * 20;
+		internalVelocity.yCoord = entity.motionY * 20;
+		internalVelocity.zCoord = entity.motionZ * 20;
 		return internalVelocity;
 	}
 	
 	@Override
 	public void setValue(Vec3 value) {
-		entity.motionX = value.xCoord;
-		entity.motionY = value.yCoord;
-		entity.motionZ = value.zCoord;
+		entity.motionX = value.xCoord / 20;
+		entity.motionY = value.yCoord / 20;
+		entity.motionZ = value.zCoord / 20;
 	}
 	
 }
