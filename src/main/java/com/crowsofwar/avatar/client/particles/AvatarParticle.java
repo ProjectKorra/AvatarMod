e2701ab88f7896672d8a38b685b3cc202aeae0a3
CrowsOfWar <ofwarcrows@gmail.com>
2016-09-07 19:14:44 -0500

    avatar particle supports different u/v for texture

--- src/main/java/com/crowsofwar/avatar/client/particles/AvatarParticle.java
+++ src/main/java/com/crowsofwar/avatar/client/particles/AvatarParticle.java
@@ -7,10 +7,11 @@ import net.minecraft.client.renderer.GlStateManager.DestFactor;
 import net.minecraft.client.renderer.GlStateManager.SourceFactor;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -31,7 +32,6 @@ public abstract class AvatarParticle extends Particle {
 		super(world, x, y, z, velX, velY, velZ);
 		
 		this.additiveBlending = false;
-		this.setParticleTexture(getTextureFrames()[0]);
 		
 	}
 	
@@ -39,20 +39,75 @@ public abstract class AvatarParticle extends Particle {
 	public void renderParticle(VertexBuffer vb, Entity entity, float partialTicks, float rotationX,
 			float rotationZ, float rotationYZ, float rotationXY, float rotationXZ) {
 		
-		setParticleTexture(getTextureFrames()[getCurrentFrame()]);
+		ParticleFrame frame = getTextureFrames()[getCurrentFrame()];
 		
 		Tessellator t = Tessellator.getInstance();
 		Minecraft mc = Minecraft.getMinecraft();
 		
 		t.draw();
-		mc.getTextureManager().bindTexture(AVATAR_PARTICLES);
+		mc.getTextureManager().bindTexture(frame.texture);
 		vb.begin(7, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
 		if (additiveBlending) {
 			GlStateManager.blendFunc(SourceFactor.ONE, DestFactor.ONE);
 		}
 		
-		super.renderParticle(vb, entity, partialTicks, rotationX, rotationZ, rotationYZ, rotationXY,
-				rotationXZ);
+		// CUSTOM RENDER PARTICLE
+		
+		float minU = frame.minU;
+		float maxU = frame.maxU;
+		float minV = frame.minV;
+		float maxV = frame.maxV;
+		float f4 = 0.1F * this.particleScale;
+		
+		float f5 = (float) (this.prevPosX + (this.posX - this.prevPosX) * (double) partialTicks - interpPosX);
+		float f6 = (float) (this.prevPosY + (this.posY - this.prevPosY) * (double) partialTicks - interpPosY);
+		float f7 = (float) (this.prevPosZ + (this.posZ - this.prevPosZ) * (double) partialTicks - interpPosZ);
+		int i = this.getBrightnessForRender(partialTicks);
+		int j = i >> 16 & 65535;
+		int k = i & 65535;
+		Vec3d[] avec3d = new Vec3d[] {
+				new Vec3d((double) (-rotationX * f4 - rotationXY * f4), (double) (-rotationZ * f4),
+						(double) (-rotationYZ * f4 - rotationXZ * f4)),
+				new Vec3d((double) (-rotationX * f4 + rotationXY * f4), (double) (rotationZ * f4),
+						(double) (-rotationYZ * f4 + rotationXZ * f4)),
+				new Vec3d((double) (rotationX * f4 + rotationXY * f4), (double) (rotationZ * f4),
+						(double) (rotationYZ * f4 + rotationXZ * f4)),
+				new Vec3d((double) (rotationX * f4 - rotationXY * f4), (double) (-rotationZ * f4),
+						(double) (rotationYZ * f4 - rotationXZ * f4)) };
+		
+		if (this.field_190014_F != 0.0F) {
+			float f8 = this.field_190014_F + (this.field_190014_F - this.field_190015_G) * partialTicks;
+			float f9 = MathHelper.cos(f8 * 0.5F);
+			float f10 = MathHelper.sin(f8 * 0.5F) * (float) field_190016_K.xCoord;
+			float f11 = MathHelper.sin(f8 * 0.5F) * (float) field_190016_K.yCoord;
+			float f12 = MathHelper.sin(f8 * 0.5F) * (float) field_190016_K.zCoord;
+			Vec3d vec3d = new Vec3d((double) f10, (double) f11, (double) f12);
+			
+			for (int l = 0; l < 4; ++l) {
+				avec3d[l] = vec3d.scale(2.0D * avec3d[l].dotProduct(vec3d))
+						.add(avec3d[l].scale((double) (f9 * f9) - vec3d.dotProduct(vec3d)))
+						.add(vec3d.crossProduct(avec3d[l]).scale((double) (2.0F * f9)));
+			}
+		}
+		
+		vb.pos((double) f5 + avec3d[0].xCoord, (double) f6 + avec3d[0].yCoord, (double) f7 + avec3d[0].zCoord)
+				.tex((double) maxU, (double) maxV)
+				.color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha)
+				.lightmap(j, k).endVertex();
+		vb.pos((double) f5 + avec3d[1].xCoord, (double) f6 + avec3d[1].yCoord, (double) f7 + avec3d[1].zCoord)
+				.tex((double) maxU, (double) minV)
+				.color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha)
+				.lightmap(j, k).endVertex();
+		vb.pos((double) f5 + avec3d[2].xCoord, (double) f6 + avec3d[2].yCoord, (double) f7 + avec3d[2].zCoord)
+				.tex((double) minU, (double) minV)
+				.color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha)
+				.lightmap(j, k).endVertex();
+		vb.pos((double) f5 + avec3d[3].xCoord, (double) f6 + avec3d[3].yCoord, (double) f7 + avec3d[3].zCoord)
+				.tex((double) minU, (double) maxV)
+				.color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha)
+				.lightmap(j, k).endVertex();
+		
+		// CUSTOM RENDER PARTICLE STOP
 		
 		t.draw();
 		if (additiveBlending) {
@@ -70,10 +125,29 @@ public abstract class AvatarParticle extends Particle {
 		additiveBlending = true;
 	}
 	
-	protected abstract TextureAtlasSprite[] getTextureFrames();
+	protected abstract ParticleFrame[] getTextureFrames();
 	
 	protected int getCurrentFrame() {
 		return (int) (((double) particleAge / particleMaxAge) * getTextureFrames().length);
+	}
+	
+	public static class ParticleFrame {
+		
+		private final ResourceLocation texture;
+		private final int minU, maxU, minV, maxV;
+		
+		public ParticleFrame(ResourceLocation texture, int minU, int maxU, int minV, int maxV) {
+			this.texture = texture;
+			this.minU = minU;
+			this.maxU = maxU;
+			this.minV = minV;
+			this.maxV = maxV;
+		}
+		
+		public ParticleFrame(int minU, int maxU, int minV, int maxV) {
+			this(AVATAR_PARTICLES, minU, maxU, minV, maxV);
+		}
+		
 	}
 	
 }
