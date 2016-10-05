8ece31a22e0c9533da95796f51481b31a97c03f1
CrowsOfWar <ofwarcrows@gmail.com>
2016-10-04 19:08:32 -0500

    Changed statuscontrol so that it is an enum, fixes NPE upon first creating statusControl

--- src/main/java/com/crowsofwar/avatar/common/bending/StatusControl.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/StatusControl.java
@@ -1,40 +1,44 @@
 package com.crowsofwar.avatar.common.bending;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 
+import com.crowsofwar.avatar.common.controls.AvatarControl;
+
 /**
- * 
+ * Describes a temporary effect where a callback listener is added to a control event. The listener
+ * then will perform certain actions associated with that control.
+ * <p>
+ * For example, the player receives a place-block Status Control, which subscribes to right-click.
+ * The status control receives a callback whenever the player uses the right-click control. Then,
+ * the status control is removed.
+ * <p>
+ * Status controls are stored in player-data, but are also sent to the client via packets, which
+ * render over the crosshair.
  * 
  * @author CrowsOfWar
  */
-public class StatusControl {
+public enum StatusControl {
 	
-	public static final StatusControl AIR_JUMP = new StatusControl(ctx -> {
+	AIR_JUMP(ctx -> {
 		System.out.println("jump " + ctx.getPlayerEntity());
-	}, 0);
-	
-	private static int nextId = 1;
-	private static final List<StatusControl> allControls = new ArrayList<>();
+	}, 0, AvatarControl.CONTROL_LEFT_CLICK); // TODO Add control_space
 	
 	private final int texture;
 	private final Consumer<AbilityContext> callback;
-	private final int id;
+	private final AvatarControl control;
 	
-	private StatusControl(Consumer<AbilityContext> callback, int texture) {
+	private StatusControl(Consumer<AbilityContext> callback, int texture, AvatarControl subscribeTo) {
 		this.texture = texture;
 		this.callback = callback;
-		this.id = nextId++;
-		allControls.add(this);
+		this.control = subscribeTo;
 	}
 	
 	public int id() {
-		return id;
+		return ordinal() + 1;
 	}
 	
 	public static StatusControl lookup(int id) {
-		return allControls.get(id - 1);
+		return values()[id - 1];
 	}
 	
 }
