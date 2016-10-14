f5ef07aaa1baa5a728a385a0c001a4d5dc905cec
CrowsOfWar <ofwarcrows@gmail.com>
2016-10-14 17:40:39 -0500

    actually.... was fine.

--- src/main/java/com/crowsofwar/avatar/common/entity/data/FloatingBlockBehavior.java
+++ src/main/java/com/crowsofwar/avatar/common/entity/data/FloatingBlockBehavior.java
@@ -15,9 +15,7 @@ import com.crowsofwar.gorecore.util.Vector;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.datasync.DataSerializer;
 import net.minecraft.network.datasync.DataSerializers;
@@ -162,15 +160,6 @@ public abstract class FloatingBlockBehavior extends Behavior<EntityFloatingBlock
 						motion.mul(0.3);
 						motion.setY(0.08);
 						collided.addVelocity(motion.x(), motion.y(), motion.z());
-					}
-					
-					if (!world.isRemote && entity.areItemDropsEnabled()) {
-						List<ItemStack> drops = entity.getBlock().getDrops(world, new BlockPos(entity),
-								entity.getBlockState(), 0);
-						for (ItemStack is : drops) {
-							EntityItem ei = new EntityItem(world, entity.posX, entity.posY, entity.posZ, is);
-							world.spawnEntityInWorld(ei);
-						}
 					}
 					
 				}
