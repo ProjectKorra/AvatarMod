5e9827648f0ceff760ed574d5f31aa137357dc43
CrowsOfWar <ofwarcrows@gmail.com>
2016-10-16 13:45:42 -0500

    Player tick will try to load request PD on client side

--- src/main/java/com/crowsofwar/avatar/common/AvatarPlayerTick.java
+++ src/main/java/com/crowsofwar/avatar/common/AvatarPlayerTick.java
@@ -1,13 +1,16 @@
 package com.crowsofwar.avatar.common;
 
+import com.crowsofwar.avatar.common.data.AvatarPlayerData;
+
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class AvatarPlayerTick {
 	
 	@SubscribeEvent
 	public void onPlayerTick(PlayerTickEvent e) {
-		
+		if (e.side == Side.CLIENT) AvatarPlayerData.fetcher().fetchPerformance(e.player);
 	}
 	
 }
