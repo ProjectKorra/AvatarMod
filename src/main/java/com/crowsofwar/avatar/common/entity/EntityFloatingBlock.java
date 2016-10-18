49d6518c0ccb9045dcd179c0faaea39df759befa
CrowsOfWar <ofwarcrows@gmail.com>
2016-10-18 17:50:13 -0500

    Removing unnecessary stuff

--- src/main/java/com/crowsofwar/avatar/common/entity/EntityFloatingBlock.java
+++ src/main/java/com/crowsofwar/avatar/common/entity/EntityFloatingBlock.java
@@ -252,7 +252,7 @@ public class EntityFloatingBlock extends AvatarEntity {
 		motionY = velocity().y() / 20;
 		motionZ = velocity().z() / 20;
 		
-		moveEntity(velocity().x() / 20, velocity().y() / 20, velocity().z() / 20);
+		modifiedMoveEntity(velocity().x() / 20, velocity().y() / 20, velocity().z() / 20);
 		
 		getBehavior().setEntity(this);
 		FloatingBlockBehavior nextBehavior = (FloatingBlockBehavior) getBehavior().onUpdate();
@@ -270,20 +270,6 @@ public class EntityFloatingBlock extends AvatarEntity {
 			double d1 = this.posY;
 			double d2 = this.posZ;
 			
-			if (this.isInWeb) {
-				this.isInWeb = false;
-				x *= 0.25D;
-				y *= 0.05000000074505806D;
-				z *= 0.25D;
-				this.motionX = 0.0D;
-				this.motionY = 0.0D;
-				this.motionZ = 0.0D;
-			}
-			
-			double d3 = x;
-			double d4 = y;
-			double d5 = z;
-			
 			List<AxisAlignedBB> list1 = this.worldObj.getCollisionBoxes(this,
 					this.getEntityBoundingBox().addCoord(x, y, z));
 			AxisAlignedBB axisalignedbb = this.getEntityBoundingBox();
@@ -294,7 +280,7 @@ public class EntityFloatingBlock extends AvatarEntity {
 			}
 			
 			this.setEntityBoundingBox(this.getEntityBoundingBox().offset(0.0D, y, 0.0D));
-			boolean i_ = this.onGround || d4 != y && d4 < 0.0D;
+			boolean i_ = this.onGround || y < 0.0D;
 			int j4 = 0;
 			
 			for (int k = list1.size(); j4 < k; ++j4) {
@@ -310,103 +296,12 @@ public class EntityFloatingBlock extends AvatarEntity {
 			
 			this.setEntityBoundingBox(this.getEntityBoundingBox().offset(0.0D, 0.0D, z));
 			
-			if (this.stepHeight > 0.0F && i_ && (d3 != x || d5 != z)) {
-				double d11 = x;
-				double d7 = y;
-				double d8 = z;
-				AxisAlignedBB axisalignedbb1 = this.getEntityBoundingBox();
-				this.setEntityBoundingBox(axisalignedbb);
-				y = (double) this.stepHeight;
-				List<AxisAlignedBB> list = this.worldObj.getCollisionBoxes(this,
-						this.getEntityBoundingBox().addCoord(d3, y, d5));
-				AxisAlignedBB axisalignedbb2 = this.getEntityBoundingBox();
-				AxisAlignedBB axisalignedbb3 = axisalignedbb2.addCoord(d3, 0.0D, d5);
-				double d9 = y;
-				int l = 0;
-				
-				for (int i1 = list.size(); l < i1; ++l) {
-					d9 = ((AxisAlignedBB) list.get(l)).calculateYOffset(axisalignedbb3, d9);
-				}
-				
-				axisalignedbb2 = axisalignedbb2.offset(0.0D, d9, 0.0D);
-				double d15 = d3;
-				int j1 = 0;
-				
-				for (int k1 = list.size(); j1 < k1; ++j1) {
-					d15 = ((AxisAlignedBB) list.get(j1)).calculateXOffset(axisalignedbb2, d15);
-				}
-				
-				axisalignedbb2 = axisalignedbb2.offset(d15, 0.0D, 0.0D);
-				double d16 = d5;
-				int l1 = 0;
-				
-				for (int i2 = list.size(); l1 < i2; ++l1) {
-					d16 = ((AxisAlignedBB) list.get(l1)).calculateZOffset(axisalignedbb2, d16);
-				}
-				
-				axisalignedbb2 = axisalignedbb2.offset(0.0D, 0.0D, d16);
-				AxisAlignedBB axisalignedbb4 = this.getEntityBoundingBox();
-				double d17 = y;
-				int j2 = 0;
-				
-				for (int k2 = list.size(); j2 < k2; ++j2) {
-					d17 = ((AxisAlignedBB) list.get(j2)).calculateYOffset(axisalignedbb4, d17);
-				}
-				
-				axisalignedbb4 = axisalignedbb4.offset(0.0D, d17, 0.0D);
-				double d18 = d3;
-				int l2 = 0;
-				
-				for (int i3 = list.size(); l2 < i3; ++l2) {
-					d18 = ((AxisAlignedBB) list.get(l2)).calculateXOffset(axisalignedbb4, d18);
-				}
-				
-				axisalignedbb4 = axisalignedbb4.offset(d18, 0.0D, 0.0D);
-				double d19 = d5;
-				int j3 = 0;
-				
-				for (int k3 = list.size(); j3 < k3; ++j3) {
-					d19 = ((AxisAlignedBB) list.get(j3)).calculateZOffset(axisalignedbb4, d19);
-				}
-				
-				axisalignedbb4 = axisalignedbb4.offset(0.0D, 0.0D, d19);
-				double d20 = d15 * d15 + d16 * d16;
-				double d10 = d18 * d18 + d19 * d19;
-				
-				if (d20 > d10) {
-					x = d15;
-					z = d16;
-					y = -d9;
-					this.setEntityBoundingBox(axisalignedbb2);
-				} else {
-					x = d18;
-					z = d19;
-					y = -d17;
-					this.setEntityBoundingBox(axisalignedbb4);
-				}
-				
-				int l3 = 0;
-				
-				for (int i4 = list.size(); l3 < i4; ++l3) {
-					y = ((AxisAlignedBB) list.get(l3)).calculateYOffset(this.getEntityBoundingBox(), y);
-				}
-				
-				this.setEntityBoundingBox(this.getEntityBoundingBox().offset(0.0D, y, 0.0D));
-				
-				if (d11 * d11 + d8 * d8 >= x * x + z * z) {
-					x = d11;
-					y = d7;
-					z = d8;
-					this.setEntityBoundingBox(axisalignedbb1);
-				}
-			}
-			
 			this.worldObj.theProfiler.endSection();
 			this.worldObj.theProfiler.startSection("rest");
 			this.resetPositionToBB();
-			this.isCollidedHorizontally = d3 != x || d5 != z;
-			this.isCollidedVertically = d4 != y;
-			this.onGround = this.isCollidedVertically && d4 < 0.0D;
+			this.isCollidedHorizontally = x != x || z != z;
+			this.isCollidedVertically = y != y;
+			this.onGround = this.isCollidedVertically && y < 0.0D;
 			this.isCollided = this.isCollidedHorizontally || this.isCollidedVertically;
 			j4 = MathHelper.floor_double(this.posX);
 			int l4 = MathHelper.floor_double(this.posY - 0.20000000298023224D);
@@ -428,33 +323,7 @@ public class EntityFloatingBlock extends AvatarEntity {
 			
 			this.updateFallState(y, this.onGround, iblockstate, blockpos);
 			
-			if (d3 != x) {
-				this.motionX = 0.0D;
-			}
-			
-			if (d5 != z) {
-				this.motionZ = 0.0D;
-			}
-			
 			Block block = iblockstate.getBlock();
-			
-			if (d4 != y) {
-				block.onLanded(this.worldObj, this);
-			}
-			
-			if (this.canTriggerWalking() && !this.isRiding()) {
-				double d12 = this.posX - d0;
-				double d13 = this.posY - d1;
-				double d14 = this.posZ - d2;
-				
-				if (block != Blocks.LADDER) {
-					d13 = 0.0D;
-				}
-				
-				if (block != null && this.onGround) {
-					block.onEntityWalk(this.worldObj, blockpos, this);
-				}
-			}
 			
 			try {
 				this.doBlockCollisions();
