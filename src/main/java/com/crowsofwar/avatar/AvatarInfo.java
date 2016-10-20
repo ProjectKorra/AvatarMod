f32905eb3ad1de31fc23418581421d54e8f633f5
CrowsOfWar <ofwarcrows@gmail.com>
2016-10-19 19:50:06 -0500

    Updated versioning

--- src/main/java/com/crowsofwar/avatar/AvatarInfo.java
+++ src/main/java/com/crowsofwar/avatar/AvatarInfo.java
@@ -1,13 +1,16 @@
 package com.crowsofwar.avatar;
 
 /**
- * Not using semantic versioning. This isn't an API, and I'm too lazy to be backwards-compatible.
+ * Not using semantic versioning. This isn't an API, and I'm too lazy to be
+ * backwards-compatible.
  * <p>
- * Versioning scheme: MC-{MCVERSION}-{UPDATE}.{PATCH}{DEV_STAGE}{DEV_BUILD}
+ * Versioning scheme: AV{DEV_STAGE}{UPDATE}.{PATCH}{DEV_BUILD}
  * <p>
  * DEV_STAGE is for alpha("a"), beta("b"), or full release("").
  * <p>
- * If development version, DEV_BUILD is "(dev)"
+ * If development version, DEV_BUILD is "_dev"
+ * <p>
+ * E.g. AV_B3.2-dev -> Beta, Update 3, patch 2, development build
  * 
  * @author CrowsOfWar
  */
@@ -35,8 +38,8 @@ public class AvatarInfo {
 	 * "" for full release.
 	 */
 	public static final String DEV_STAGE = "a";
-	public static final String MC_VERSION = "1.7.10";
-	public static final String VERSION = "MC" + MC_VERSION + "-AV" + VERSION_UPDATE + "." + VERSION_PATCH + DEV_STAGE
-			+ (IS_DEV_BUILD ? "(dev)" : "");
+	public static final String MC_VERSION = "1.10.2";
+	public static final String VERSION = "AV_" + DEV_STAGE + VERSION_UPDATE + "." + VERSION_PATCH
+			+ (IS_DEV_BUILD ? "_dev" : "");
 	
 }
