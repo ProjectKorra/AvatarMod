f95426cf49f1e487eda3cc9565fa5902aadaaa80
CrowsOfWar <ofwarcrows@gmail.com>
2016-11-01 18:06:55 -0500

    Added arguments for range

---
+++ src/main/java/com/crowsofwar/gorecore/tree/ArgumentRangeDouble.java
@@ -0,0 +1,76 @@
+package com.crowsofwar.gorecore.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.command.ICommandSender;
+
+/**
+ * 
+ * 
+ * @author CrowsOfWar
+ */
+public class ArgumentRangeDouble implements IArgument<Double> {
+	
+	private final double defaultValue;
+	private final boolean optional;
+	private final double min;
+	private final double max;
+	private final String name;
+	
+	public ArgumentRangeDouble(String name, double min, double max) {
+		this.name = name;
+		this.defaultValue = 0;
+		this.optional = false;
+		this.min = min;
+		this.max = max;
+	}
+	
+	public ArgumentRangeDouble(String name, double min, double max, double defaultValue) {
+		this.name = name;
+		this.defaultValue = defaultValue;
+		this.optional = true;
+		this.min = min;
+		this.max = max;
+	}
+	
+	@Override
+	public boolean isOptional() {
+		return optional;
+	}
+	
+	@Override
+	public Double getDefaultValue() {
+		return defaultValue;
+	}
+	
+	@Override
+	public Double convert(String input) {
+		return ITypeConverter.CONVERTER_DOUBLE.convert(input);
+	}
+	
+	@Override
+	public String getArgumentName() {
+		return name;
+	}
+	
+	@Override
+	public String getHelpString() {
+		char open = isOptional() ? '[' : '<';
+		char close = isOptional() ? ']' : '>';
+		return open + "any number " + min + "-" + max + close;
+	}
+	
+	@Override
+	public String getSpecificationString() {
+		char open = isOptional() ? '[' : '<';
+		char close = isOptional() ? ']' : '>';
+		return open + name + close;
+	}
+	
+	@Override
+	public List<String> getCompletionSuggestions(ICommandSender sender, String currentInput) {
+		return new ArrayList<>();
+	}
+	
+}
---
+++ src/main/java/com/crowsofwar/gorecore/tree/ArgumentRangeInteger.java
@@ -0,0 +1,76 @@
+package com.crowsofwar.gorecore.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.command.ICommandSender;
+
+/**
+ * 
+ * 
+ * @author CrowsOfWar
+ */
+public class ArgumentRangeInteger implements IArgument<Integer> {
+	
+	private final int defaultValue;
+	private final boolean optional;
+	private final int min;
+	private final int max;
+	private final String name;
+	
+	public ArgumentRangeInteger(String name, int min, int max) {
+		this.name = name;
+		this.defaultValue = 0;
+		this.optional = false;
+		this.min = min;
+		this.max = max;
+	}
+	
+	public ArgumentRangeInteger(String name, int min, int max, int defaultValue) {
+		this.name = name;
+		this.defaultValue = defaultValue;
+		this.optional = true;
+		this.min = min;
+		this.max = max;
+	}
+	
+	@Override
+	public boolean isOptional() {
+		return optional;
+	}
+	
+	@Override
+	public Integer getDefaultValue() {
+		return defaultValue;
+	}
+	
+	@Override
+	public Integer convert(String input) {
+		return ITypeConverter.CONVERTER_INTEGER.convert(input);
+	}
+	
+	@Override
+	public String getArgumentName() {
+		return name;
+	}
+	
+	@Override
+	public String getHelpString() {
+		char open = isOptional() ? '[' : '<';
+		char close = isOptional() ? ']' : '>';
+		return open + "any number " + min + "-" + max + close;
+	}
+	
+	@Override
+	public String getSpecificationString() {
+		char open = isOptional() ? '[' : '<';
+		char close = isOptional() ? ']' : '>';
+		return open + name + close;
+	}
+	
+	@Override
+	public List<String> getCompletionSuggestions(ICommandSender sender, String currentInput) {
+		return new ArrayList<>();
+	}
+	
+}
