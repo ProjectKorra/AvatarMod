2946c3c18c10d0e62303d9f8c60545a6af430f9c
CrowsOfWar <ofwarcrows@gmail.com>
2016-11-10 17:15:04 -0600

    Updated particle in code

--- src/main/java/com/crowsofwar/avatar/client/particles/AvatarParticleAir.java
+++ src/main/java/com/crowsofwar/avatar/client/particles/AvatarParticleAir.java
@@ -11,12 +11,12 @@ import net.minecraft.world.World;
 public class AvatarParticleAir extends AvatarParticle {
 	
 	private static final ResourceLocation TEXTURE = new ResourceLocation("avatarmod",
-			"textures/particles/air2.png");
+			"textures/particles/cloud.png");
 	
 	private static final ParticleFrame[] FRAMES = new ParticleFrame[8];
 	static {
 		for (int i = 0; i < FRAMES.length; i++) {
-			FRAMES[i] = new ParticleFrame(TEXTURE, 256, i * 32, 0, 32, 32);
+			FRAMES[i] = new ParticleFrame(TEXTURE, 256, (i % 4) * 64, i / 64, 64, 64);
 		}
 	}
 	
