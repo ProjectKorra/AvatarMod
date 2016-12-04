faa697e671a980e03683b9d19406b9b803a453f5
CrowsOfWar <ofwarcrows@gmail.com>
2016-12-03 18:12:17 -0600

    Update GC version

--- src/main/java/com/crowsofwar/gorecore/GoreCore.java
+++ src/main/java/com/crowsofwar/gorecore/GoreCore.java
@@ -40,7 +40,7 @@ public class GoreCore {
 	
 	public static final String MOD_ID = "gorecore";
 	public static final String MOD_NAME = "GoreCore";
-	public static final String MOD_VERSION = "1.10-0.10.0";
+	public static final String MOD_VERSION = "1.11-0.2.0";
 	
 	@SidedProxy(clientSide = "com.crowsofwar.gorecore.proxy.GoreCoreClientProxy", serverSide = "com.crowsofwar.gorecore.proxy.GoreCoreCommonProxy")
 	public static GoreCoreCommonProxy proxy;
