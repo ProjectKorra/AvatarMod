940cecc3b77fec40a2049e7a0fc28ff654217a1d
CrowsOfWar <ofwarcrows@gmail.com>
2016-12-04 11:03:09 -0600

    Moved navbar script -> js file

---
+++ docs/navbar.js
@@ -0,0 +1,11 @@
+var $;
+
+$(document).ready(function () {
+    "use strict";
+    $.ajax({
+        url: "navbar.html",
+        isLocal: true
+    }).done(function (data) {
+        document.getElementById("navbar").innerHTML = (data);
+    });
+});
