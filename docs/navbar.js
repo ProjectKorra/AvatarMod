e381f9c8bf55ff6ac6fdc6b9580785e001ddec7a
CrowsOfWar <ofwarcrows@gmail.com>
2016-12-05 19:46:16 -0600

    Using OFFICIAL bootstrap navbar system

--- docs/navbar.js
+++ docs/navbar.js
@@ -6,6 +6,6 @@ $(document).ready(function () {
         url: "navbar.html",
         isLocal: true
     }).done(function (data) {
-        document.getElementById("navbar").innerHTML = data;
+        document.getElementsByTagName("nav")[0].innerHTML = data;
     });
 });
