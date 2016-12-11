4307d18cf4afe6d0729893371f3eabed72781d3d
CrowsOfWar <ofwarcrows@gmail.com>
2016-12-11 10:45:49 -0600

    Fixed nav links staying :focus after click

--- docs/navbar.js
+++ docs/navbar.js
@@ -3,12 +3,14 @@ var $;
 addListeners = function() {
   $('a').click(function() {
       var href = this.href.substring(this.href.indexOf("#"));
-      console.log(href);
       $('html, body').animate({
           scrollTop: ($(href).offset().top - $("nav").height() / 2)
       }, 750);
       return false;
-  })
+  });
+  $(".nav-link").mouseup(function() {
+      $(this).blur();
+  });
 };
 checkNav = function() {
     $nav = $("nav");
