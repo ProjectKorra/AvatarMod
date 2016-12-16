bcfdbb713689f8238cbb34babb8edbf8db3d4d26
CrowsOfWar <ofwarcrows@gmail.com>
2016-12-16 16:41:07 -0600

    Created WallBehavior

---
+++ src/main/java/com/crowsofwar/avatar/common/entity/data/WallBehavior.java
@@ -0,0 +1,105 @@
+package com.crowsofwar.avatar.common.entity.data;
+
+import com.crowsofwar.avatar.common.entity.EntityWallSegment;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.datasync.DataSerializer;
+import net.minecraft.network.datasync.DataSerializers;
+
+/**
+ * 
+ * 
+ * @author CrowsOfWar
+ */
+public abstract class WallBehavior extends Behavior<EntityWallSegment> {
+	
+	public static DataSerializer<WallBehavior> SERIALIZER = new Behavior.BehaviorSerializer<>();
+	
+	public static void register() {
+		DataSerializers.registerSerializer(SERIALIZER);
+		registerBehavior(Drop.class);
+		registerBehavior(Rising.class);
+		registerBehavior(Waiting.class);
+	}
+	
+	public static class Drop extends WallBehavior {
+		
+		@Override
+		public Behavior onUpdate() {
+			entity.velocity().add(0, -4 / 20, 0);
+			if (entity.isCollided) {
+				// also drops blocks
+				entity.setDead();
+			}
+			return this;
+		}
+		
+		@Override
+		public void fromBytes(PacketBuffer buf) {}
+		
+		@Override
+		public void toBytes(PacketBuffer buf) {}
+		
+		@Override
+		public void load(NBTTagCompound nbt) {}
+		
+		@Override
+		public void save(NBTTagCompound nbt) {}
+		
+	}
+	
+	public static class Rising extends WallBehavior {
+		
+		private int ticks = 0;
+		
+		@Override
+		public Behavior onUpdate() {
+			if (ticks == 0)
+				entity.velocity().set(0, 5, 0);
+			else
+				entity.velocity().setY(entity.velocity().y() * 0.9);
+			ticks++;
+			return entity.velocity().y() <= 0.2 ? new Waiting() : this;
+		}
+		
+		@Override
+		public void fromBytes(PacketBuffer buf) {}
+		
+		@Override
+		public void toBytes(PacketBuffer buf) {}
+		
+		@Override
+		public void load(NBTTagCompound nbt) {}
+		
+		@Override
+		public void save(NBTTagCompound nbt) {}
+		
+	}
+	
+	public static class Waiting extends WallBehavior {
+		
+		private int ticks = 0;
+		
+		@Override
+		public Behavior onUpdate() {
+			entity.velocity().set(0, 0, 0);
+			ticks++;
+			return ticks < 100 ? this : new Drop();
+		}
+		
+		@Override
+		public void fromBytes(PacketBuffer buf) {}
+		
+		@Override
+		public void toBytes(PacketBuffer buf) {}
+		
+		@Override
+		public void load(NBTTagCompound nbt) {}
+		
+		@Override
+		public void save(NBTTagCompound nbt) {}
+		
+	}
+	
+}
