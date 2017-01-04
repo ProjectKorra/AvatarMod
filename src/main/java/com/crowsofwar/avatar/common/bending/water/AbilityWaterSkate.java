c4897c1d492153e421ce4fbde740859f33f00aec
CrowsOfWar <ofwarcrows@gmail.com>
2017-01-04 11:36:59 -0600

    Better activation for water skating-- no countdown

--- src/main/java/com/crowsofwar/avatar/common/bending/water/AbilityWaterSkate.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/water/AbilityWaterSkate.java
@@ -17,12 +17,19 @@
 package com.crowsofwar.avatar.common.bending.water;
 
 import static com.crowsofwar.avatar.common.bending.BendingType.WATERBENDING;
+import static com.crowsofwar.avatar.common.util.AvatarUtils.afterVelocityAdded;
 
 import com.crowsofwar.avatar.common.bending.AbilityContext;
 import com.crowsofwar.avatar.common.bending.BendingAbility;
 import com.crowsofwar.avatar.common.data.AvatarPlayerData;
+import com.crowsofwar.avatar.common.util.Raytrace;
+import com.crowsofwar.gorecore.util.Vector;
+import com.crowsofwar.gorecore.util.VectorI;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 
 /**
  * 
@@ -39,7 +46,30 @@ public class AbilityWaterSkate extends BendingAbility {
 	public void execute(AbilityContext ctx) {
 		EntityPlayer player = ctx.getPlayerEntity();
 		AvatarPlayerData data = AvatarPlayerData.fetcher().fetch(player);
-		data.setSkateTime(60);
+		World world = ctx.getWorld();
+		
+		Vector look = Vector.toRectangular(Math.toRadians(player.rotationYaw), 0);
+		Raytrace.Result result = Raytrace.predicateRaytrace(world, Vector.getEntityPos(player).add(0, -1, 0),
+				look, 2, (pos, blockState) -> blockState.getBlock() == Blocks.WATER);
+		if (result.hitSomething()) {
+			
+			VectorI pos = result.getPos();
+			IBlockState hitBlockState = world.getBlockState(pos.toBlockPos());
+			IBlockState up = world.getBlockState(pos.toBlockPos().up());
+			
+			for (int i = 0; i < 3; i++) {
+				if (world.getBlockState(pos.toBlockPos().up()).getBlock() == Blocks.AIR) {
+					player.setPosition(pos.x() + .5, pos.y() + 1.2, pos.z() + .5);
+					data.setSkateTime(20);
+					
+					afterVelocityAdded(player);
+					
+					break;
+				}
+				pos.add(0, 1, 0);
+			}
+			
+		}
 	}
 	
 	@Override
