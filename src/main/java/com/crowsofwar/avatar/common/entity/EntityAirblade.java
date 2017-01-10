6b1624cea69f037bf2fc953318430f1d76df8b23
CrowsOfWar <ofwarcrows@gmail.com>
2017-01-10 17:37:32 -0600

    Airblade entity

---
+++ src/main/java/com/crowsofwar/avatar/common/entity/EntityAirblade.java
@@ -0,0 +1,66 @@
+/* 
+  This file is part of AvatarMod.
+    
+  AvatarMod is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  AvatarMod is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with AvatarMod. If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.crowsofwar.avatar.common.entity;
+
+import com.crowsofwar.avatar.common.entity.data.OwnerAttribute;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.world.World;
+
+/**
+ * 
+ * 
+ * @author CrowsOfWar
+ */
+public class EntityAirblade extends AvatarEntity {
+	
+	public static final DataParameter<String> SYNC_OWNER = EntityDataManager.createKey(EntityAirblade.class,
+			DataSerializers.STRING);
+	
+	private final OwnerAttribute ownerAttr;
+	
+	public EntityAirblade(World world) {
+		super(world);
+		setSize(1.5f, .2f);
+		this.ownerAttr = new OwnerAttribute(this, SYNC_OWNER);
+	}
+	
+	public EntityPlayer getOwner() {
+		return ownerAttr.getOwner();
+	}
+	
+	public void setOwner(EntityPlayer owner) {
+		ownerAttr.setOwner(owner);
+	}
+	
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound nbt) {
+		super.readEntityFromNBT(nbt);
+		ownerAttr.load(nbt);
+	}
+	
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound nbt) {
+		super.writeEntityToNBT(nbt);
+		ownerAttr.save(nbt);
+	}
+	
+}
