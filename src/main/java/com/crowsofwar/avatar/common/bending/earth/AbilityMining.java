ee438ad2b8e314ceedf087afed22c72f3a1325c8
CrowsOfWar <ofwarcrows@gmail.com>
2017-01-17 17:29:21 -0600

    Starting better calculation

--- src/main/java/com/crowsofwar/avatar/common/bending/earth/AbilityMining.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/earth/AbilityMining.java
@@ -17,14 +17,15 @@
 package com.crowsofwar.avatar.common.bending.earth;
 
 import static com.crowsofwar.avatar.common.config.ConfigStats.STATS_CONFIG;
+import static java.lang.Math.floor;
 
 import com.crowsofwar.avatar.common.bending.AbilityContext;
 import com.crowsofwar.avatar.common.data.AvatarWorldData;
+import com.crowsofwar.gorecore.util.VectorI;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -45,11 +46,27 @@ public class AbilityMining extends EarthAbility {
 		EntityPlayer player = ctx.getPlayerEntity();
 		World world = ctx.getWorld();
 		
-		EnumFacing facing = player.getHorizontalFacing();
+		// EnumFacing facing = player.getHorizontalFacing();
+		
+		//@formatter:off
+		// 0 = S 0x +z    1 = SW -x +z
+		// 2 = W -x 0z    3 = NW -x -z
+		// 4 = N 0x -z    5 = NE +x -z
+		// 6 = E +x 0z    7 = SE +x +z
+		//@formatter:on
+		int yaw = (int) floor((player.rotationYaw * 8 / 360) + 0.5) & 7;
+		int x = 0, z = 0;
+		if (yaw == 1 || yaw == 2 || yaw == 3) x = -1;
+		if (yaw == 5 || yaw == 6 || yaw == 7) x = 1;
+		if (yaw == 3 || yaw == 4 || yaw == 5) z = -1;
+		if (yaw == 0 || yaw == 1 || yaw == 7) z = 1;
+		
+		VectorI diff = new VectorI(x, 0, z);
+		System.out.println("Look is " + diff);
 		
 		for (int y = 0; y <= 1; y++) {
 			for (int i = 1; i <= 5; i++) {
-				BlockPos pos = player.getPosition().offset(facing, i).up(y);
+				BlockPos pos = player.getPosition().add(diff.x() * i, diff.y() * i, diff.z() * i).up(y);
 				Block block = world.getBlockState(pos).getBlock();
 				
 				boolean bendable = STATS_CONFIG.bendableBlocks.contains(block);
@@ -62,9 +79,6 @@ public class AbilityMining extends EarthAbility {
 				}
 			}
 		}
-		
-		BlockPos pos = player.getPosition().offset(facing);
-		world.setBlockToAir(pos);
 		
 	}
 	
