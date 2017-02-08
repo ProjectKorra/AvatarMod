2c626501dbe5b9ebfd0a31786dde3be97dd34076
CrowsOfWar <ofwarcrows@gmail.com>
2017-02-07 19:42:38 -0600

    Add 'testing' tooltip to the scroll

--- src/main/java/com/crowsofwar/avatar/common/item/ItemScroll.java
+++ src/main/java/com/crowsofwar/avatar/common/item/ItemScroll.java
@@ -16,7 +16,14 @@
 */
 package com.crowsofwar.avatar.common.item;
 
+import java.util.List;
+
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * 
@@ -28,6 +35,15 @@ public class ItemScroll extends Item {
 	public ItemScroll() {
 		setUnlocalizedName("scroll");
 		setMaxStackSize(1);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltips,
+			boolean advanced) {
+		
+		tooltips.add(I18n.format("avatar.tooltip.scroll"));
+		
 	}
 	
 }
