aab94dbb452711333080ee5505bad4beed9da464
CrowsOfWar <ofwarcrows@gmail.com>
2017-02-28 18:21:20 -0600

    WindowAbility

---
+++ src/main/java/com/crowsofwar/avatar/client/gui/skills/WindowAbility.java
@@ -0,0 +1,66 @@
+/* 
+  This file is part of AvatarMod.
+    
+  AvatarMod is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  AvatarMod is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with AvatarMod. If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.crowsofwar.avatar.client.gui.skills;
+
+import static com.crowsofwar.avatar.client.gui.AvatarUiTextures.getAbilityTexture;
+import static com.crowsofwar.avatar.client.uitools.Measurement.fromPercent;
+import static com.crowsofwar.avatar.client.uitools.Measurement.fromPixels;
+
+import com.crowsofwar.avatar.client.uitools.ComponentImage;
+import com.crowsofwar.avatar.client.uitools.ComponentText;
+import com.crowsofwar.avatar.client.uitools.Frame;
+import com.crowsofwar.avatar.client.uitools.StartingPosition;
+import com.crowsofwar.avatar.client.uitools.UiComponent;
+import com.crowsofwar.avatar.common.bending.BendingAbility;
+
+import net.minecraft.client.resources.I18n;
+
+/**
+ * 
+ * 
+ * @author CrowsOfWar
+ */
+public class WindowAbility {
+	
+	private final BendingAbility ability;
+	private Frame frame;
+	private UiComponent icon, title;
+	
+	public WindowAbility(BendingAbility ability) {
+		this.ability = ability;
+		
+		frame = new Frame();
+		frame.setDimensions(fromPercent(80, 80));
+		frame.setPosition(fromPercent((100 - 80) / 2, (100 - 80) / 2));
+		
+		title = new ComponentText(I18n.format("avatar.ability." + ability.getName()));
+		title.setFrame(frame);
+		title.setPosition(StartingPosition.MIDDLE_TOP);
+		
+		icon = new ComponentImage(getAbilityTexture(ability), 0, 0, 256, 256);
+		icon.setFrame(frame);
+		icon.setPosition(StartingPosition.MIDDLE_TOP);
+		icon.setOffset(fromPixels(0, title.height()));
+		
+	}
+	
+	public void draw(float partialTicks) {
+		title.draw(partialTicks);
+		icon.draw(partialTicks);
+	}
+	
+}
