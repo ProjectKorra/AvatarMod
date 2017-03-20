4f478fc0439f901ebdae962c79f8131b2c48d74a
CrowsOfWar <ofwarcrows@gmail.com>
2017-03-20 08:22:31 -0500

    Fixed melee attack not working

--- src/main/java/com/crowsofwar/avatar/common/entity/mob/EntityHumanBender.java
+++ src/main/java/com/crowsofwar/avatar/common/entity/mob/EntityHumanBender.java
@@ -18,16 +18,18 @@ package com.crowsofwar.avatar.common.entity.mob;
 
 import javax.annotation.Nullable;
 
-import com.crowsofwar.avatar.common.bending.BendingAbility;
 import com.crowsofwar.avatar.common.data.BendingData;
 import com.crowsofwar.avatar.common.data.ctx.Bender;
 import com.crowsofwar.avatar.common.entity.data.EntityBenderData;
 
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
@@ -36,9 +38,12 @@ import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.loot.LootTableList;
@@ -68,23 +73,29 @@ public class EntityHumanBender extends EntityCreature implements Bender {
 		super.applyEntityAttributes();
 		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20);
 		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25);
+		this.getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3);
 	}
 	
 	@Override
 	protected void initEntityAI() {
 		this.tasks.addTask(0, new EntityAISwimming(this));
 		
-		this.targetTasks.addTask(6,
+		this.targetTasks.addTask(2,
 				new EntityAINearestAttackableTarget(this, EntityPlayer.class, true, false));
-		this.targetTasks.addTask(2, BendingAbility.ABILITY_AIR_GUST.getAi(this, this));
-		this.targetTasks.addTask(5, BendingAbility.ABILITY_AIRBLADE.getAi(this, this));
-		this.targetTasks.addTask(1, BendingAbility.ABILITY_AIR_BUBBLE.getAi(this, this));
-		this.targetTasks.addTask(3, new EntityAIAttackMelee(this, 0.35, true));
+		// this.tasks.addTask(2,
+		// BendingAbility.ABILITY_AIR_GUST.getAi(this, this));
+		// this.tasks.addTask(5,
+		// BendingAbility.ABILITY_AIRBLADE.getAi(this, this));
+		// this.tasks.addTask(1,
+		// BendingAbility.ABILITY_AIR_BUBBLE.getAi(this, this));
+		this.tasks.addTask(3, new EntityAIAttackMelee(this, 1, true));
 		
-		this.tasks.addTask(2, new EntityAiKeepDistance(this, 3, 2));
+		// this.tasks.addTask(2, new EntityAiKeepDistance(this, 3, 2));
 		this.tasks.addTask(5, new EntityAIWanderAvoidWater(this, 1.0D));
 		this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
 		this.tasks.addTask(7, new EntityAILookIdle(this));
+		
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true, new Class[0]));
 		
 	}
 	
@@ -92,7 +103,6 @@ public class EntityHumanBender extends EntityCreature implements Bender {
 	protected void setEquipmentBasedOnDifficulty(DifficultyInstance difficulty) {
 		super.setEquipmentBasedOnDifficulty(difficulty);
 		this.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, new ItemStack(Items.IRON_SWORD));
-		System.out.println("ALALA");
 	}
 	
 	@Override
@@ -101,6 +111,58 @@ public class EntityHumanBender extends EntityCreature implements Bender {
 		livingdata = super.onInitialSpawn(difficulty, livingdata);
 		setEquipmentBasedOnDifficulty(difficulty);
 		return livingdata;
+	}
+	
+	// Copied from EntityMob
+	@Override
+	public boolean attackEntityAsMob(Entity entityIn) {
+		float f = (float) this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
+		int i = 0;
+		
+		if (entityIn instanceof EntityLivingBase) {
+			f += EnchantmentHelper.getModifierForCreature(this.getHeldItemMainhand(),
+					((EntityLivingBase) entityIn).getCreatureAttribute());
+			i += EnchantmentHelper.getKnockbackModifier(this);
+		}
+		
+		boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), f);
+		
+		if (flag) {
+			if (i > 0 && entityIn instanceof EntityLivingBase) {
+				((EntityLivingBase) entityIn).knockBack(this, i * 0.5F,
+						MathHelper.sin(this.rotationYaw * 0.017453292F),
+						(-MathHelper.cos(this.rotationYaw * 0.017453292F)));
+				this.motionX *= 0.6D;
+				this.motionZ *= 0.6D;
+			}
+			
+			int j = EnchantmentHelper.getFireAspectModifier(this);
+			
+			if (j > 0) {
+				entityIn.setFire(j * 4);
+			}
+			
+			if (entityIn instanceof EntityPlayer) {
+				EntityPlayer entityplayer = (EntityPlayer) entityIn;
+				ItemStack itemstack = this.getHeldItemMainhand();
+				ItemStack itemstack1 = entityplayer.isHandActive() ? entityplayer.getActiveItemStack()
+						: ItemStack.field_190927_a;
+				
+				if (!itemstack.func_190926_b() && !itemstack1.func_190926_b()
+						&& itemstack.getItem() instanceof ItemAxe && itemstack1.getItem() == Items.SHIELD) {
+					float f1 = 0.25F + EnchantmentHelper.getEfficiencyModifier(this) * 0.05F;
+					
+					if (this.rand.nextFloat() < f1) {
+						entityplayer.getCooldownTracker().setCooldown(Items.SHIELD, 100);
+						this.worldObj.setEntityState(entityplayer, (byte) 30);
+					}
+				}
+			}
+			
+			this.applyEnchantments(this, entityIn);
+		}
+		
+		return flag;
 	}
 	
 	@Override
@@ -119,6 +181,7 @@ public class EntityHumanBender extends EntityCreature implements Bender {
 	public void onUpdate() {
 		super.onUpdate();
 		data.decrementCooldown();
+		System.out.println(getAttackTarget());
 	}
 	
 	@Override
