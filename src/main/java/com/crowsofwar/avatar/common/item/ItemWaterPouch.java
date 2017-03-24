e5fcfb29b446959cbc368e3b09f5db98b5c5ce67
CrowsOfWar <ofwarcrows@gmail.com>
2017-03-23 20:43:14 -0500

    Add refill ability to water pouch

--- src/main/java/com/crowsofwar/avatar/common/item/ItemWaterPouch.java
+++ src/main/java/com/crowsofwar/avatar/common/item/ItemWaterPouch.java
@@ -18,12 +18,22 @@ package com.crowsofwar.avatar.common.item;
 
 import java.util.List;
 
+import com.crowsofwar.avatar.common.util.Raytrace;
+import com.crowsofwar.gorecore.util.Vector;
+
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -72,6 +82,28 @@ public class ItemWaterPouch extends Item implements AvatarItem {
 			subItems.add(new ItemStack(item, 1, meta));
 		}
 		
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		
+		ItemStack stack = player.getHeldItem(hand);
+		
+		Vector eye = Vector.getEntityPos(player);
+		Vector look = Vector.getLookRectangular(player);
+		
+		Raytrace.Result raytrace = Raytrace.predicateRaytrace(world, eye, look, 4,
+				(pos, state) -> state.getBlock() == Blocks.WATER || state.getBlock() == Blocks.FLOWING_WATER);
+		
+		if (raytrace.hitSomething()) {
+			BlockPos pos = raytrace.getPos().toBlockPos();
+			IBlockState state = world.getBlockState(pos);
+			if (state.getBlock() == Blocks.WATER || state.getBlock() == Blocks.FLOWING_WATER) {
+				stack.setItemDamage(3);
+			}
+		}
+		
+		return new ActionResult(EnumActionResult.SUCCESS, stack);
 	}
 	
 }
