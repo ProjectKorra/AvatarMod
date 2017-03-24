80e369a6b69b97eacd82618087f6e822d9200732
CrowsOfWar <ofwarcrows@gmail.com>
2017-03-23 20:16:42 -0500

    impl for PlayerBender

--- src/main/java/com/crowsofwar/avatar/common/data/ctx/PlayerBender.java
+++ src/main/java/com/crowsofwar/avatar/common/data/ctx/PlayerBender.java
@@ -18,9 +18,12 @@ package com.crowsofwar.avatar.common.data.ctx;
 
 import com.crowsofwar.avatar.common.data.AvatarPlayerData;
 import com.crowsofwar.avatar.common.data.BendingData;
+import com.crowsofwar.avatar.common.item.AvatarItems;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
 
 /**
  * 
@@ -53,6 +56,43 @@ public class PlayerBender implements Bender {
 	@Override
 	public boolean isFlying() {
 		return player.capabilities.isFlying;
+	}
+	
+	@Override
+	public boolean consumeWaterLevel(int amount) {
+		
+		int total = 0;
+		InventoryPlayer inv = player.inventory;
+		
+		int inventorySlots = 36;
+		for (int i = 0; i < inventorySlots; i++) {
+			ItemStack stack = inv.getStackInSlot(i);
+			if (stack.getItem() == AvatarItems.itemWaterPouch) {
+				total += stack.getMetadata();
+			}
+		}
+		
+		if (total >= amount) {
+			
+			int i = 0;
+			while (amount > 0) {
+				ItemStack stack = inv.getStackInSlot(i);
+				if (stack.getItem() == AvatarItems.itemWaterPouch) {
+					int oldMetadata = stack.getMetadata();
+					int newMetadata = stack.getMetadata() - amount;
+					if (newMetadata < 0) newMetadata = 0;
+					amount -= oldMetadata - newMetadata;
+					System.out.println("Consuming " + (oldMetadata - newMetadata) + " water in slot " + i);
+				}
+				i++;
+			}
+			
+			return true;
+			
+		} else {
+			return false;
+		}
+		
 	}
 	
 }
