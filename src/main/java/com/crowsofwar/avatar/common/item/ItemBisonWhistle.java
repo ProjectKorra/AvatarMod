b55d4e163bc54ee9c772dd8ca73571e303c701b3
CrowsOfWar <ofwarcrows@gmail.com>
2017-04-13 19:35:16 -0500

    Bison whistle is tied to 1 bison

--- src/main/java/com/crowsofwar/avatar/common/item/ItemBisonWhistle.java
+++ src/main/java/com/crowsofwar/avatar/common/item/ItemBisonWhistle.java
@@ -16,16 +16,23 @@
 */
 package com.crowsofwar.avatar.common.item;
 
+import static com.crowsofwar.gorecore.util.GoreCoreNBTUtil.stackCompound;
+
 import java.util.List;
+import java.util.UUID;
+
+import javax.annotation.Nullable;
 
 import com.crowsofwar.avatar.common.AvatarChatMessages;
 import com.crowsofwar.avatar.common.data.AvatarPlayerData;
 import com.crowsofwar.avatar.common.data.TickHandler;
 import com.crowsofwar.avatar.common.entity.mob.EntitySkyBison;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
@@ -45,6 +52,18 @@ public class ItemBisonWhistle extends Item implements AvatarItem {
 	}
 	
 	@Override
+	public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target,
+			EnumHand hand) {
+		
+		if (target instanceof EntitySkyBison) {
+			setBoundTo(stack, target.getUniqueID());
+		}
+		
+		return false;
+		
+	}
+	
+	@Override
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
 		
 		ItemStack stack = player.getHeldItem(hand);
@@ -53,22 +72,27 @@ public class ItemBisonWhistle extends Item implements AvatarItem {
 			return new ActionResult<>(EnumActionResult.PASS, stack);
 		}
 		
-		// Make sure there's a bison to teleport first
-		List<EntitySkyBison> entities = world.getEntities(EntitySkyBison.class,
-				bison -> bison.getOwner() == player);
-		
-		if (!entities.isEmpty()) {
+		UUID boundTo = getBoundTo(stack);
+		if (boundTo != null) {
 			
-			EntitySkyBison bison = entities.get(0);
-			double dist = player.getDistanceToEntity(bison);
+			// Make sure there's a bison to teleport first
+			List<EntitySkyBison> entities = world.getEntities(EntitySkyBison.class,
+					bison -> bison.getUniqueID() == boundTo);
 			
-			double seconds = dist / 20;
-			
-			AvatarPlayerData data = AvatarPlayerData.fetcher().fetch(player);
-			data.setPetSummonCooldown((int) (seconds * 20));
-			data.addTickHandler(TickHandler.BISON_SUMMONER);
-			
-			AvatarChatMessages.MSG_SKY_BISON_SUMMON.send(player, (int) seconds);
+			if (!entities.isEmpty()) {
+				
+				EntitySkyBison bison = entities.get(0);
+				double dist = player.getDistanceToEntity(bison);
+				
+				double seconds = dist / 20;
+				
+				AvatarPlayerData data = AvatarPlayerData.fetcher().fetch(player);
+				data.setPetSummonCooldown((int) (seconds * 20));
+				data.addTickHandler(TickHandler.BISON_SUMMONER);
+				
+				AvatarChatMessages.MSG_SKY_BISON_SUMMON.send(player, (int) seconds);
+				
+			}
 			
 		}
 		
@@ -84,6 +108,21 @@ public class ItemBisonWhistle extends Item implements AvatarItem {
 	@Override
 	public String getModelName(int meta) {
 		return "bison_whistle";
+	}
+	
+	@Nullable
+	private static UUID getBoundTo(ItemStack stack) {
+		NBTTagCompound nbt = stackCompound(stack);
+		return nbt.hasKey("SkyBison") ? nbt.getUniqueId("SkyBison") : null;
+	}
+	
+	public static void setBoundTo(ItemStack stack, @Nullable UUID id) {
+		NBTTagCompound nbt = stackCompound(stack);
+		if (id != null) {
+			nbt.setUniqueId("SkyBison", id);
+		} else {
+			nbt.removeTag("SkyBison");
+		}
 	}
 	
 }
