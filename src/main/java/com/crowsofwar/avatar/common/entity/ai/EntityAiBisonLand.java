5948dc78be8ca7b425f0568838c85ca4858182e0
CrowsOfWar <ofwarcrows@gmail.com>
2017-04-17 21:09:42 -0500

    Fix bug when bison isn't eating grass but is landing

--- src/main/java/com/crowsofwar/avatar/common/entity/ai/EntityAiBisonLand.java
+++ src/main/java/com/crowsofwar/avatar/common/entity/ai/EntityAiBisonLand.java
@@ -92,7 +92,7 @@ public class EntityAiBisonLand extends EntityAIBase {
 		}
 		
 		// Don't wander off until we have food!
-		return !bison.isFull();
+		return !bison.isFull() && bison.isEatingGrass();
 	}
 	
 	private Vector findLandingPoint() {
