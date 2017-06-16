3d1f4496d8e2825e7d75cc7e51da8cf6b08c7f93
CrowsOfWar <ofwarcrows@gmail.com>
2017-06-16 14:10:20 -0500

    tryUseInCode();

--- src/main/java/com/crowsofwar/avatar/client/gui/skills/ComponentBendingTab.java
+++ src/main/java/com/crowsofwar/avatar/client/gui/skills/ComponentBendingTab.java
@@ -19,27 +19,58 @@ package com.crowsofwar.avatar.client.gui.skills;
 import com.crowsofwar.avatar.AvatarMod;
 import com.crowsofwar.avatar.client.gui.AvatarUiTextures;
 import com.crowsofwar.avatar.client.uitools.ComponentImage;
+import com.crowsofwar.avatar.client.uitools.UiComponent;
 import com.crowsofwar.avatar.common.bending.BendingType;
 import com.crowsofwar.avatar.common.network.packets.PacketSSkillsMenu;
+
+import net.minecraft.util.ResourceLocation;
 
 /**
  * 
  * 
  * @author CrowsOfWar
  */
-public class ComponentBendingTab extends ComponentImage {
+public class ComponentBendingTab extends UiComponent {
 	
 	private final BendingType type;
+	private final boolean fullTab;
 	
-	public ComponentBendingTab(BendingType type, boolean isFullTab, boolean isSelected) {
-		super(AvatarUiTextures.skillsGui, !isFullTab && isSelected ? 216 : 236, type.ordinal() * 20, 20,
-				isFullTab || isSelected ? 20 : 17);
+	private final ComponentImage tabImg, tabImgHover, bendingImg;
+	
+	public ComponentBendingTab(BendingType type, boolean fullTab) {
+		
+		tabImg = new ComponentImage(AvatarUiTextures.skillsGui, fullTab ? 216 : 236, 0, 20, 20);
+		tabImgHover = new ComponentImage(AvatarUiTextures.skillsGui, fullTab ? 216 : 236, 20, 20, 20);
+		
+		ResourceLocation iconRl = new ResourceLocation(
+				"avatarmod:textures/gui/tab/" + type.name().toLowerCase());
+		bendingImg = new ComponentImage(iconRl, 0, 0, 128, 128);
+		
 		this.type = type;
+		this.fullTab = fullTab;
+		
 	}
 	
 	@Override
 	protected void click(int button) {
 		AvatarMod.network.sendToServer(new PacketSSkillsMenu(type));
+	}
+	
+	@Override
+	protected float componentWidth() {
+		return 20;
+	}
+	
+	@Override
+	protected float componentHeight() {
+		return 20;
+	}
+	
+	@Override
+	protected void componentDraw(float partialTicks, boolean mouseHover) {
+		if (mouseHover) {
+			tabImgHover.componentDraw(partialTicks, mouseHover);
+		}
 	}
 	
 }
