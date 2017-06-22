0554d5195c491a860e3051c245d05db549bd43b4
CrowsOfWar <ofwarcrows@gmail.com>
2017-06-22 16:48:00 -0500

    Move raytrace into the correct file; fix detect through walls

--- src/main/java/com/crowsofwar/avatar/common/bending/ice/AbilityIcePrison.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/ice/AbilityIcePrison.java
@@ -16,18 +16,16 @@
 */
 package com.crowsofwar.avatar.common.bending.ice;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.crowsofwar.avatar.common.bending.BendingAbility;
 import com.crowsofwar.avatar.common.bending.BendingManager;
 import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
+import com.crowsofwar.avatar.common.util.Raytrace;
 import com.crowsofwar.gorecore.util.Vector;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 /**
@@ -43,34 +41,14 @@ public class AbilityIcePrison extends BendingAbility {
 	
 	@Override
 	public void execute(AbilityContext ctx) {
-		System.out.println("Imprision");
 		
-		EntityLivingBase entity = ctx.getBenderEntity();
-		Vector start = Vector.getEyePos(entity);
-		Vector end = start.plus(Vector.getLookRectangular(entity).times(10));
+		EntityLivingBase caster = ctx.getBenderEntity();
+		World world = ctx.getWorld();
+		Vector start = Vector.getEyePos(caster);
+		Vector direction = Vector.getLookRectangular(caster);
 		
-		raytraceEntities(ctx.getWorld(), start, end);
-		
-		// Item
-	}
-	
-	private List<Entity> raytraceEntities(World world, Vector start, Vector end) {
-		
-		List<Entity> hit = new ArrayList<>();
-		
-		AxisAlignedBB aabb = new AxisAlignedBB(start.x(), start.y(), start.z(), end.x(), end.y(), end.z());
-		List<Entity> entities = world.getEntitiesWithinAABB(Entity.class, aabb);
-		
-		for (Entity entity : entities) {
-			AxisAlignedBB collisionBox = entity.getEntityBoundingBox();
-			RayTraceResult result = collisionBox.calculateIntercept(start.toMinecraft(), end.toMinecraft());
-			if (result != null) {
-				System.out.println("Hit " + entity);
-				hit.add(entity);
-			}
-		}
-		
-		return hit;
+		List<Entity> hit = Raytrace.entityRaytrace(world, start, direction, 10, entity -> entity != caster);
+		System.out.println(hit);
 		
 	}
 	
