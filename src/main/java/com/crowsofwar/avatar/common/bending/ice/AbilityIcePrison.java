4377717e70183769ef1690bca75c3c30ef546b4a
CrowsOfWar <ofwarcrows@gmail.com>
2017-06-22 16:39:12 -0500

    Raytrace!

--- src/main/java/com/crowsofwar/avatar/common/bending/ice/AbilityIcePrison.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/ice/AbilityIcePrison.java
@@ -16,9 +16,19 @@
 */
 package com.crowsofwar.avatar.common.bending.ice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.crowsofwar.avatar.common.bending.BendingAbility;
 import com.crowsofwar.avatar.common.bending.BendingManager;
 import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
+import com.crowsofwar.gorecore.util.Vector;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
 
 /**
  * 
@@ -34,7 +44,34 @@ public class AbilityIcePrison extends BendingAbility {
 	@Override
 	public void execute(AbilityContext ctx) {
 		System.out.println("Imprision");
+		
+		EntityLivingBase entity = ctx.getBenderEntity();
+		Vector start = Vector.getEyePos(entity);
+		Vector end = start.plus(Vector.getLookRectangular(entity).times(10));
+		
+		raytraceEntities(ctx.getWorld(), start, end);
+		
 		// Item
+	}
+	
+	private List<Entity> raytraceEntities(World world, Vector start, Vector end) {
+		
+		List<Entity> hit = new ArrayList<>();
+		
+		AxisAlignedBB aabb = new AxisAlignedBB(start.x(), start.y(), start.z(), end.x(), end.y(), end.z());
+		List<Entity> entities = world.getEntitiesWithinAABB(Entity.class, aabb);
+		
+		for (Entity entity : entities) {
+			AxisAlignedBB collisionBox = entity.getEntityBoundingBox();
+			RayTraceResult result = collisionBox.calculateIntercept(start.toMinecraft(), end.toMinecraft());
+			if (result != null) {
+				System.out.println("Hit " + entity);
+				hit.add(entity);
+			}
+		}
+		
+		return hit;
+		
 	}
 	
 }
