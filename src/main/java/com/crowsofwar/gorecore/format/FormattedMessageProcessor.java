eddc93fab67867ecf651c036606a7c89854b1877
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-05 11:19:50 -0500

    Move formatting related inner classes to FormattingMessageProcessor

--- src/main/java/com/crowsofwar/gorecore/format/FormattedMessageProcessor.java
+++ src/main/java/com/crowsofwar/gorecore/format/FormattedMessageProcessor.java
@@ -16,15 +16,17 @@
 */
 package com.crowsofwar.gorecore.format;
 
+import static com.crowsofwar.gorecore.format.FormattedMessageProcessor.Setting.*;
+
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.crowsofwar.gorecore.GoreCore;
 import com.crowsofwar.gorecore.format.ChatSender.ProcessingException;
-import com.crowsofwar.gorecore.format.FormattingState.ChatFormatSet;
-import com.crowsofwar.gorecore.format.FormattingState.Setting;
 
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.text.TextFormatting;
@@ -55,16 +57,19 @@ public class FormattedMessageProcessor {
 			formatSet.addFormat(color.getKey(), color.getValue(), Setting.UNKNOWN, Setting.UNKNOWN);
 		}
 		
+		// Apply format arguments
 		String[] translateArgs = msg.getTranslationArgs();
 		for (int i = 0; i < translateArgs.length; i++) {
 			text = text.replace("${" + translateArgs[i] + "}", formatValues[i].toString());
 		}
 		
+		// Apply constants from MessageConfiguration
 		Set<Map.Entry<String, String>> consts = cfg.getAllConstants();
 		for (Map.Entry<String, String> entry : consts) {
 			text = text.replace("${" + entry.getKey() + "}", entry.getValue());
 		}
 		
+		// Apply chat formats
 		FormattingState format = new FormattingState();
 		
 		String newText = "";
@@ -147,6 +152,179 @@ public class FormattedMessageProcessor {
 		}
 		
 		return newText;
+		
+	}
+	
+	/**
+	 * Represents the current state while
+	 * #{@link FormattedMessageProcessor#formatText(FormattedMessage, String, Object...)
+	 * formatting text}.
+	 * <p>
+	 * The formatting state is really a stack of {@link ChatFormat} objects.
+	 * 
+	 * <p>
+	 * Here is an example of stepping through the following text:<br />
+	 * 
+	 * <pre>
+	 * Hello, [bold]my name is [blue]Joe[/blue]! Nice to meet you![/bold]
+	 * </pre>
+	 * 
+	 * <pre>
+	 * ^
+	 * </pre>
+	 * 
+	 * At this point, the stack is empty and there are no formats applied.
+	 * <p>
+	 * 
+	 * <pre>
+	 * Hello, [bold]my name is [blue]Joe[/blue]! Nice to meet you![/bold]
+	 * </pre>
+	 * 
+	 * <pre>
+	 *              ^
+	 * </pre>
+	 * 
+	 * @author CrowsOfWar
+	 */
+	private static class FormattingState {
+		
+		private final Stack<ChatFormat> formats;
+		
+		public FormattingState() {
+			formats = new Stack<>();
+		}
+		
+		public void pushFormat(ChatFormat format) {
+			formats.push(format);
+		}
+		
+		public void popFormat() {
+			formats.pop();
+		}
+		
+		public ChatFormat topFormat() {
+			return formats.peek();
+		}
+		
+		public boolean hasFormat() {
+			return !formats.isEmpty();
+		}
+		
+		public boolean isBold() {
+			boolean bold = false;
+			for (int i = 0; i < formats.size(); i++) {
+				ChatFormat format = formats.get(i);
+				if (!format.isBold.isUnknown()) bold = format.isBold.value();
+			}
+			return bold;
+		}
+		
+		public boolean isItalic() {
+			boolean italic = false;
+			for (int i = 0; i < formats.size(); i++) {
+				ChatFormat format = formats.get(i);
+				if (!format.isItalic.isUnknown()) italic = format.isItalic.value();
+			}
+			return italic;
+		}
+		
+		public TextFormatting getColor() {
+			TextFormatting color = TextFormatting.WHITE;
+			for (int i = 0; i < formats.size(); i++) {
+				ChatFormat format = formats.get(i);
+				if (format.color != null) color = format.color;
+			}
+			return color;
+		}
+		
+		@Override
+		public String toString() {
+			return "FormattingState " + formats;
+		}
+	}
+	
+	/**
+	 * Represents a set of chat formatting settings
+	 * 
+	 * @author CrowsOfWar
+	 */
+	private static class ChatFormat {
+		
+		private final String name;
+		private final Setting isBold;
+		private final Setting isItalic;
+		private final TextFormatting color;
+		
+		private ChatFormat(String name, Setting isBold, Setting isItalic, TextFormatting color) {
+			this.name = name;
+			this.isBold = isBold;
+			this.isItalic = isItalic;
+			this.color = color;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+	}
+	
+	/**
+	 * A set of chat formats, which contains the default ones, but you can also
+	 * add more.
+	 * 
+	 * @author CrowsOfWar
+	 */
+	private static class ChatFormatSet {
+		
+		// set has better performance than list, and we don't need duplicate
+		// entries anyways
+		private final Set<ChatFormat> formats;
+		
+		public ChatFormatSet() {
+			formats = new HashSet<>();
+			
+			// add colors
+			for (TextFormatting tf : TextFormatting.values()) {
+				if (tf.isColor()) addFormat(tf.getFriendlyName(), tf, UNKNOWN, UNKNOWN);
+			}
+			// add special
+			addFormat("bold", null, TRUE, UNKNOWN);
+			addFormat("italic", null, UNKNOWN, TRUE);
+			addFormat("noformat", null, FALSE, FALSE);
+			
+		}
+		
+		public void addFormat(String name, TextFormatting color, Setting bold, Setting italic) {
+			formats.add(new ChatFormat(name, bold, italic, color));
+		}
+		
+		public ChatFormat lookup(String name) {
+			for (ChatFormat format : formats) {
+				if (format.name.equals(name)) {
+					return format;
+				}
+			}
+			return null;
+		}
+		
+		public boolean isFormatFor(String name) {
+			return lookup(name) != null;
+		}
+		
+	}
+	
+	public enum Setting {
+		UNKNOWN,
+		TRUE,
+		FALSE;
+		
+		public boolean value() {
+			return this == TRUE;
+		}
+		
+		public boolean isUnknown() {
+			return this == UNKNOWN;
+		}
 		
 	}
 	
