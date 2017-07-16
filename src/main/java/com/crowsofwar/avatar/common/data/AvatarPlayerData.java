85901fadcf6b7b523883581ac8131b39a0922fe7
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-16 12:37:18 -0500

    Remove EntityBenderData, fix AvatarPlayerData

--- src/main/java/com/crowsofwar/avatar/common/data/AvatarPlayerData.java
+++ src/main/java/com/crowsofwar/avatar/common/data/AvatarPlayerData.java
@@ -18,9 +18,7 @@
 package com.crowsofwar.avatar.common.data;
 
 import com.crowsofwar.avatar.AvatarMod;
-import com.crowsofwar.avatar.common.bending.*;
 import com.crowsofwar.avatar.common.network.packets.PacketCPlayerData;
-import com.crowsofwar.avatar.common.util.AvatarUtils;
 import com.crowsofwar.gorecore.data.*;
 import net.minecraft.entity.EntityTracker;
 import net.minecraft.entity.player.EntityPlayer;
@@ -31,13 +29,11 @@ import net.minecraftforge.fml.common.network.NetworkRegistry.TargetPoint;
 
 import java.util.*;
 
-import static com.crowsofwar.gorecore.util.GoreCoreNBTUtil.nestedCompound;
-
 public class AvatarPlayerData extends PlayerData {
 	
 	private static PlayerDataFetcher<AvatarPlayerData> fetcher;
 
-	private final AbstractBendingData bendingData;
+	private final BendingData bendingData;
 
 	/**
 	 * Changed DataCategories since last sent a packet
@@ -49,91 +45,20 @@ public class AvatarPlayerData extends PlayerData {
 		
 		boolean isClient = !(player instanceof EntityPlayerMP);
 		
-		bendingData = new AbstractBendingData() {
-			@Override
-			public void save(DataCategory category) {
-				AvatarPlayerData.this.save(category);
-			}
-		};
+		bendingData = new BendingData(this::save, this::saveAll);
 		
 		changed = new TreeSet<>();
 		
 	}
 	
 	@Override
-	protected void readPlayerDataFromNBT(NBTTagCompound readFrom) {
-		
-		List<BendingStyle> bendings = new ArrayList<>();
-		AvatarUtils.readList(bendings,
-				compound -> BendingStyles.get(compound.getUniqueId("ControllerID")), readFrom,
-				"BendingControllers");
-		clearBending();
-		for (BendingStyle bending : bendings) {
-			addBending(bending);
-		}
-		
-		List<StatusControl> scs = new ArrayList<>();
-		AvatarUtils.readList(scs, nbtTag -> StatusControl.lookup(nbtTag.getInteger("Id")), readFrom,
-				"StatusControls");
-		clearStatusControls();
-		for (StatusControl sc : scs) {
-			addStatusControl(sc);
-		}
-		
-		Map<Ability, AbilityData> abilityData = new HashMap<>();
-		AvatarUtils.readMap(abilityData, nbt -> Abilities.get(nbt.getUniqueId("Id")), nbt -> {
-			Ability ability = Abilities.get(nbt.getUniqueId("AbilityId"));
-			AbilityData data = new AbilityData(this, ability);
-			data.readFromNbt(nbt);
-			return data;
-		}, readFrom, "AbilityData");
-		clearAbilityData();
-		for (Map.Entry<Ability, AbilityData> entry : abilityData.entrySet()) {
-			setAbilityData(entry.getKey().getId(), entry.getValue());
-		}
-		
-		getMiscData().readFromNbt(nestedCompound(readFrom, "Misc"));
-		
-		chi().readFromNBT(readFrom);
-		
-		List<TickHandler> tickHandlers = new ArrayList<>();
-		AvatarUtils.readList(tickHandlers, //
-				nbt -> TickHandler.fromId(nbt.getInteger("Id")), //
-				readFrom, "TickHandlers");
-		clearTickHandlers();
-		for (TickHandler handler : tickHandlers) {
-			addTickHandler(handler);
-		}
-		
+	protected void readPlayerDataFromNBT(NBTTagCompound nbt) {
+		bendingData.readFromNbt(nbt);
 	}
 	
 	@Override
-	protected void writePlayerDataToNBT(NBTTagCompound writeTo) {
-		
-		AvatarUtils.writeList(getAllBending(),
-				(compound, controller) -> compound.setUniqueId("ControllerID", controller.getId()), writeTo,
-				"BendingControllers");
-		
-		AvatarUtils.writeList(getAllStatusControls(),
-				(nbtTag, control) -> nbtTag.setInteger("Id", control.id()), writeTo, "StatusControls");
-		
-		AvatarUtils.writeMap(getAbilityDataMap(), //
-				(nbt, abilityId) -> {
-					nbt.setUniqueId("Id", abilityId);
-					nbt.setString("_AbilityName", Abilities.getName(abilityId) + "");
-				}, (nbt, data) -> {
-					nbt.setUniqueId("AbilityId", data.getAbility().getId());
-					data.writeToNbt(nbt);
-				}, writeTo, "AbilityData");
-		
-		getMiscData().writeToNbt(nestedCompound(writeTo, "Misc"));
-		
-		chi().writeToNBT(writeTo);
-		
-		AvatarUtils.writeList(getAllTickHandlers(), //
-				(nbt, handler) -> nbt.setInteger("Id", handler.id()), //
-				writeTo, "TickHandlers");
-		
+	protected void writePlayerDataToNBT(NBTTagCompound nbt) {
+		bendingData.writeToNbt(nbt);
 	}
 	
 	public void save(DataCategory category) {
