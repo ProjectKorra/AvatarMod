b774bbbe50e2a4dbc14e89fe6cea3db53406fb4e
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-21 13:45:51 -0500

    generally improve ostrich horse animations

--- src/main/java/com/crowsofwar/avatar/client/render/ostrich/ModelOstrichHorse.java
+++ src/main/java/com/crowsofwar/avatar/client/render/ostrich/ModelOstrichHorse.java
@@ -4,14 +4,16 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
+import static java.lang.Math.sin;
+import static java.lang.Math.toRadians;
+
 /**
  * The superclass of all ostrich horse models. There are different models for different layers of
  * armor. This class is responsible only for animations.
  *
  * @author CrowsOfWar
  */
-public abstract class ModelOstrichHorse extends ModelBase
-{
+public abstract class ModelOstrichHorse extends ModelBase {
 
 	/**
 	 * Get the neck box to be animated
@@ -19,20 +21,20 @@ public abstract class ModelOstrichHorse extends ModelBase
 	protected abstract ModelRenderer getNeck();
 
 	protected abstract ModelRenderer getLeftLeg();
+
 	protected abstract ModelRenderer getRightLeg();
 
 	@Override
 	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
 								  float headPitch, float scaleFactor, Entity ostrich) {
 
-		getNeck().rotateAngleY = (float) Math.toRadians(netHeadYaw);
-		getNeck().rotateAngleX = (float) Math.toRadians(headPitch);
+		getNeck().rotateAngleY = (float) (toRadians(netHeadYaw));
+		getNeck().rotateAngleX = (float) (toRadians(headPitch) + sin(limbSwing * 0.2) * 0.05);
 
-		getLeftLeg().rotateAngleX = (float) (Math.sin(limbSwing * 0.2) * limbSwingAmount * 0.5 +
+		getLeftLeg().rotateAngleX = (float) (sin(limbSwing * 0.4) * limbSwingAmount * 0.5 +
 				0.1);
-		getRightLeg().rotateAngleX = (float) (Math.sin(limbSwing * 0.2 + Math.PI) * limbSwingAmount
-				* 0.5 +
-				0.1);
+		getRightLeg().rotateAngleX = (float) (sin(limbSwing * 0.4 + Math.PI) * limbSwingAmount
+				* 0.5 + 0.1);
 
 	}
 
