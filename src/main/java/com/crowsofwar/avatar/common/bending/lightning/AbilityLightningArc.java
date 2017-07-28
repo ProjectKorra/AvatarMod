41e193913b14969b7af0206de9df0aa0f67f225e
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-28 16:31:21 -0500

    hit entity fx

--- src/main/java/com/crowsofwar/avatar/common/bending/lightning/AbilityLightningArc.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/lightning/AbilityLightningArc.java
@@ -3,10 +3,12 @@ package com.crowsofwar.avatar.common.bending.lightning;
 import com.crowsofwar.avatar.common.bending.Ability;
 import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
 import com.crowsofwar.avatar.common.entity.EntityLightningArc;
+import com.crowsofwar.avatar.common.util.AvatarUtils;
 import com.crowsofwar.avatar.common.util.Raytrace;
 import com.crowsofwar.gorecore.util.Vector;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -67,7 +69,16 @@ public class AbilityLightningArc extends Ability {
 				.getBenderEntity());
 
 		if (!hitEntity.isEmpty()) {
-			return Vector.getEntityPos(hitEntity.get(0));
+
+			Entity hit = hitEntity.get(0);
+
+			hit.attackEntityFrom(DamageSource.LIGHTNING_BOLT, 8);
+			hit.setFire(4);
+			Vector velocity = look.times(3);
+			hit.addVelocity(velocity.x(), 0.6, velocity.z());
+			AvatarUtils.afterVelocityAdded(hit);
+
+			return Vector.getEntityPos(hit);
 		}
 
 		Raytrace.Result raytrace = Raytrace.raytrace(ctx.getWorld(), pos, look, maxRange, false);
