063958f91ba4fda3f278489f133bcb168f2d565a
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-31 12:33:41 -0500

    Lightning arc no raytrace

--- src/main/java/com/crowsofwar/avatar/common/bending/lightning/AbilityLightningArc.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/lightning/AbilityLightningArc.java
@@ -3,14 +3,10 @@ package com.crowsofwar.avatar.common.bending.lightning;
 import com.crowsofwar.avatar.common.bending.Ability;
 import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
 import com.crowsofwar.avatar.common.entity.EntityLightningArc;
-import com.crowsofwar.avatar.common.util.Raytrace;
 import com.crowsofwar.gorecore.util.Vector;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.world.World;
 
-import javax.annotation.Nullable;
-import java.util.List;
 import java.util.UUID;
 
 /**
@@ -29,62 +25,30 @@ public class AbilityLightningArc extends Ability {
 		EntityLivingBase entity = ctx.getBenderEntity();
 		World world = entity.world;
 
-		Vector hitPos = performRaytrace(ctx);
-		if (hitPos != null) {
+		float[] turbulenceValues = { 0.6f, 1.2f };
 
-			float[] turbulenceValues = { 0.6f, 1.2f };
+		for (float turbulence : turbulenceValues) {
 
-			for (float turbulence : turbulenceValues) {
+			EntityLightningArc lightning = new EntityLightningArc(world);
+			lightning.setOwner(entity);
+			lightning.setTurbulence(turbulence);
 
-				EntityLightningArc lightning = new EntityLightningArc(world);
-				lightning.setOwner(entity);
-				lightning.setTurbulence(turbulence);
+			lightning.setPosition(Vector.getEyePos(entity));
+			lightning.setEndPos(Vector.getEyePos(entity));
 
-				lightning.setPosition(Vector.getEyePos(entity));
-				lightning.setEndPos(Vector.getEyePos(entity));
+			Vector velocity = Vector.getLookRectangular(entity);
+			velocity = velocity.normalize().times(30);
+			lightning.setVelocity(velocity);
 
-				Vector velocity = hitPos.minus(lightning.position());
-				velocity = velocity.normalize().times(30);
-				lightning.setVelocity(velocity);
-
-				world.spawnEntity(lightning);
-
-			}
+			world.spawnEntity(lightning);
 
 		}
-
 
 	}
 
 	@Override
 	public UUID getId() {
 		return ID;
-	}
-
-	/**
-	 * Performs a raytrace to find the closest hit block or entity. Returns Vector.ZERO if
-	 * nothing is hit
-	 */
-	@Nullable
-	private Vector performRaytrace(AbilityContext ctx) {
-
-		final double maxRange = 20;
-
-		Vector pos = Vector.getEyePos(ctx.getBenderEntity());
-		Vector look = Vector.getLookRectangular(ctx.getBenderEntity());
-
-		List<Entity> hitEntity = Raytrace.entityRaytrace(ctx.getWorld(), pos, look, maxRange, ent
-				-> ent != ctx
-				.getBenderEntity());
-
-		if (!hitEntity.isEmpty()) {
-			return Vector.getEntityPos(hitEntity.get(0));
-		}
-
-		Raytrace.Result raytrace = Raytrace.raytrace(ctx.getWorld(), pos, look, maxRange, false);
-
-		return raytrace.getPosPrecise();
-
 	}
 
 }
