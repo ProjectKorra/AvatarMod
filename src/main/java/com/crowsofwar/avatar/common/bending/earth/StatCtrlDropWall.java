3f9f41eca9afd2d61c24001e7afcfa86c8da43f0
CrowsOfWar <ofwarcrows@gmail.com>
2017-08-12 18:19:46 -0500

    Fix not lowering all walls when multiple walls raised

--- src/main/java/com/crowsofwar/avatar/common/bending/earth/StatCtrlDropWall.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/earth/StatCtrlDropWall.java
@@ -4,10 +4,12 @@ import com.crowsofwar.avatar.common.bending.StatusControl;
 import com.crowsofwar.avatar.common.controls.AvatarControl;
 import com.crowsofwar.avatar.common.data.ctx.BendingContext;
 import com.crowsofwar.avatar.common.entity.AvatarEntity;
-import com.crowsofwar.avatar.common.entity.EntityWall;
+import com.crowsofwar.avatar.common.entity.EntityWallSegment;
 import com.crowsofwar.avatar.common.entity.data.WallBehavior;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 /**
  * @author CrowsOfWar
@@ -24,13 +26,16 @@ public class StatCtrlDropWall extends StatusControl {
 		World world = ctx.getWorld();
 		EntityLivingBase entity = ctx.getBenderEntity();
 
-		EntityWall wall = AvatarEntity.lookupOwnedEntity(world, EntityWall.class, entity);
-		if (wall != null) {
+		// TODO: When upgrade to a5.0 , call setOwner on the wall itself , then lookup based on wall
 
-			for (int i = 0; i < 5; i++) {
-				wall.getSegment(i).setBehavior(new WallBehavior.Drop());
-			}
+		// Wall has no owner so we go for segments
+		EntityWallSegment wallSegment = AvatarEntity.lookupOwnedEntity(world, EntityWallSegment.class, entity);
 
+		List<EntityWallSegment> segments = world.getEntities(EntityWallSegment.class, seg -> seg
+				.getOwner() == entity);
+		
+		for (EntityWallSegment seg : segments) {
+			seg.setBehavior(new WallBehavior.Drop());
 		}
 
 		return true;
