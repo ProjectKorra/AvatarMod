df9460d25a847b238c732645e4cc8cbf6da7146e
CrowsOfWar <ofwarcrows@gmail.com>
2017-09-03 09:42:04 -0500

    Add manipulable controller to lightning arc
    Controller entity can be set/get like owner

--- src/main/java/com/crowsofwar/avatar/common/entity/EntityLightningArc.java
+++ src/main/java/com/crowsofwar/avatar/common/entity/EntityLightningArc.java
@@ -5,9 +5,11 @@ import com.crowsofwar.avatar.common.data.AbilityData;
 import com.crowsofwar.avatar.common.data.Bender;
 import com.crowsofwar.avatar.common.data.BendingData;
 import com.crowsofwar.avatar.common.entity.data.LightningFloodFill;
+import com.crowsofwar.avatar.common.entity.data.SyncedEntity;
 import com.crowsofwar.avatar.common.util.AvatarDataSerializers;
 import com.crowsofwar.avatar.common.util.AvatarUtils;
 import com.crowsofwar.gorecore.util.Vector;
+import com.google.common.base.Optional;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Blocks;
@@ -23,6 +25,7 @@ import org.joml.SimplexNoise;
 import org.joml.Vector4d;
 
 import javax.annotation.Nullable;
+import java.util.UUID;
 
 import static com.crowsofwar.avatar.common.config.ConfigSkills.SKILLS_CONFIG;
 import static com.crowsofwar.gorecore.util.Vector.getEntityPos;
@@ -43,6 +46,9 @@ public class EntityLightningArc extends EntityArc<EntityLightningArc.LightningCo
 
 	private static final DataParameter<Boolean> SYNC_MAIN_ARC = EntityDataManager.createKey
 			(EntityLightningArc.class, DataSerializers.BOOLEAN);
+
+	private static final DataParameter<Optional<UUID>> SYNC_CONTROLLER = EntityDataManager
+			.createKey(EntityLightningArc.class, DataSerializers.OPTIONAL_UNIQUE_ID);
 
 	/**
 	 * If the lightning hits an entity, the lightning "sticks to" that entity and continues to
@@ -68,10 +74,13 @@ public class EntityLightningArc extends EntityArc<EntityLightningArc.LightningCo
 
 	private LightningFloodFill floodFill;
 
+	private final SyncedEntity<EntityLivingBase> controllerRef;
+
 	public EntityLightningArc(World world) {
 		super(world);
 		setSize(0.5f, 0.5f);
 		damage = 8;
+		controllerRef = new SyncedEntity<>(this, SYNC_CONTROLLER);
 	}
 
 	@Override
@@ -81,6 +90,7 @@ public class EntityLightningArc extends EntityArc<EntityLightningArc.LightningCo
 		dataManager.register(SYNC_TURBULENCE, 0.6f);
 		dataManager.register(SYNC_SIZE, 1f);
 		dataManager.register(SYNC_MAIN_ARC, true);
+		dataManager.register(SYNC_CONTROLLER, Optional.absent());
 	}
 
 	@Override
@@ -289,6 +299,15 @@ public class EntityLightningArc extends EntityArc<EntityLightningArc.LightningCo
 	 */
 	public void setMainArc(boolean mainArc) {
 		dataManager.set(SYNC_MAIN_ARC, mainArc);
+	}
+
+	@Override
+	public EntityLivingBase getController() {
+		return controllerRef.getEntity();
+	}
+
+	public void setController(EntityLivingBase controller) {
+		controllerRef.setEntity(controller);
 	}
 
 	public class LightningControlPoint extends ControlPoint {
