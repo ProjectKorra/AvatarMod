9f0fd76750022b3e984790ceff4213855ff7da1e
CrowsOfWar <ofwarcrows@gmail.com>
2017-09-09 16:57:47 -0500

    Perform Bender#onUpdate logic in EntityBender

--- src/main/java/com/crowsofwar/avatar/common/entity/mob/EntityBender.java
+++ src/main/java/com/crowsofwar/avatar/common/entity/mob/EntityBender.java
@@ -66,7 +66,7 @@ public abstract class EntityBender extends EntityCreature {
 	@Override
 	public void onUpdate() {
 		super.onUpdate();
-		bender.getData().decrementCooldown();
+		bender.onUpdate();
 	}
 
 	public Bender getBender() {
