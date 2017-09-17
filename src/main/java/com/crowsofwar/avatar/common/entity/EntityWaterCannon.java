3cd1444926c71479130c523ffd937c0d10bf53bd
FavouriteDragon <ninjadragon008@icloud.com>
2017-09-17 10:41:52 -0700

    adds the entity class

---
+++ src/main/java/com/crowsofwar/avatar/common/entity/EntityWaterCannon.java
@@ -0,0 +1,12 @@
+package com.crowsofwar.avatar.common.entity;
+
+import net.minecraft.world.World;
+
+public class EntityWaterCannon extends AvatarEntity {
+    /**
+     * @param world
+     */
+    public EntityWaterCannon(World world) {
+        super(world);
+    }
+}
