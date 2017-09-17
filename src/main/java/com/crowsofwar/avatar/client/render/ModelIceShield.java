7d8abe5d80c63022f1c30cc6d73962bd8aff082b
CrowsOfWar <ofwarcrows@gmail.com>
2017-09-17 11:31:34 -0500

    Use ice shield model in renderer

--- src/main/java/com/crowsofwar/avatar/client/render/ModelIceShield.java
+++ src/main/java/com/crowsofwar/avatar/client/render/ModelIceShield.java
@@ -1,4 +1,4 @@
-package tn;
+package com.crowsofwar.avatar.client.render;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
--- src/main/java/com/crowsofwar/avatar/client/render/RenderIceShield.java
+++ src/main/java/com/crowsofwar/avatar/client/render/RenderIceShield.java
@@ -1,6 +1,9 @@
 package com.crowsofwar.avatar.client.render;
 
 import com.crowsofwar.avatar.common.entity.EntityIceShield;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
@@ -15,8 +18,27 @@ public class RenderIceShield extends Render<EntityIceShield> {
 	private static final ResourceLocation TEXTURE = new ResourceLocation("avatarmod",
 			"textures/entity/ice-shield.png");
 
+	private ModelBase model;
+
 	protected RenderIceShield(RenderManager renderManager) {
 		super(renderManager);
+		model = new ModelIceShield();
+	}
+
+	@Override
+	public void doRender(EntityIceShield entity, double x, double y, double z, float entityYaw,
+						 float partialTicks) {
+
+		Minecraft.getMinecraft().renderEngine.bindTexture(TEXTURE);
+		GlStateManager.enableBlend();
+
+		GlStateManager.pushMatrix();
+		GlStateManager.translate(x, y, z);
+		model.render(entity, 0, 0, 0, 0, 0, 0.0625f);
+		GlStateManager.popMatrix();
+
+		GlStateManager.disableBlend();
+
 	}
 
 	@Nullable
