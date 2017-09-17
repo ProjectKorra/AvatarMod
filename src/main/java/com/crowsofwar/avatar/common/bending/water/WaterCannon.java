60b3ae435ce9bbb7c86c765ae3c787b81babd099
FavouriteDragon <ninjadragon008@icloud.com>
2017-09-17 10:38:28 -0700

    water cannon fixes

--- src/main/java/com/crowsofwar/avatar/common/bending/water/WaterCannon.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/water/WaterCannon.java
@@ -1,4 +1,17 @@
 package com.crowsofwar.avatar.common.bending.water;
 
-public class WaterCannon {
+import com.crowsofwar.avatar.common.bending.Ability;
+import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
+
+import java.util.UUID;
+
+public class WaterCannon extends Ability{
+    public WaterCannon() {
+        super(Waterbending.ID, "water_cannon");
+    }
+
+    @Override
+    public void execute(AbilityContext ctx) {
+
+    }
 }
