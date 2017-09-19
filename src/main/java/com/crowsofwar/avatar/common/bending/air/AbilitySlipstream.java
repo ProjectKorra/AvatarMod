a1623140483620e40abdd06c3ec9349b7f4755ef
FavouriteDragon <ninjadragon008@icloud.com>
2017-09-18 19:30:48 -0700

    added a if statement for lvl 1
    for some reason, level 3 isn't working

--- src/main/java/com/crowsofwar/avatar/common/bending/air/AbilitySlipstream.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/air/AbilitySlipstream.java
@@ -28,31 +28,35 @@ public class AbilitySlipstream extends Ability {
         World world = ctx.getWorld();
         if (bender.consumeChi(STATS_CONFIG.chiSlipstream)) {
             AbilityData abilityData = data.getAbilityData(this);
-            entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
-            float xp = SKILLS_CONFIG.blockPlaced;
-            data.getAbilityData("slipstream").addXp(xp);
+            if (abilityData.getLevel() == 0) {
 
-
-            if (abilityData.getLevel() == 1) {
-                entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100));
                 entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
+                float xp = SKILLS_CONFIG.blockPlaced;
+                data.getAbilityData("slipstream").addXp(xp);
 
-                data.getAbilityData("slipstream").addXp(xp - 0.5F);
-                if (abilityData.getLevel() == 2) {
-                    entity.addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 100));
-                    entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100, 1));
-                    entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100, 1));
-                    data.getAbilityData("slipstream").addXp(xp - 1F);
-                    if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
-                        entity.addPotionEffect(new PotionEffect(MobEffects.SATURATION, 100));
-                        entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100));
+
+                if (abilityData.getLevel() == 1) {
+                    entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100));
+                    entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
+
+                    data.getAbilityData("slipstream").addXp(xp - 0.5F);
+                    if (abilityData.getLevel() == 2) {
                         entity.addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 100));
                         entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100, 1));
                         entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100, 1));
-                        if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
-                            entity.addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 200));
-                            entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 200, 2));
-                            entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 200, 2));
+
+                        data.getAbilityData("slipstream").addXp(xp - 1F);
+
+                        if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
+                            entity.addPotionEffect(new PotionEffect(MobEffects.SATURATION, 100));
+                            entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100));
+                            entity.addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 100));
+                            entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100, 1));
+                            entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100, 1));
+                            if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
+                                entity.addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 200));
+                                entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 200, 2));
+                                entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 200, 2));
 
                             }
 
@@ -62,5 +66,6 @@ public class AbilitySlipstream extends Ability {
             }
         }
     }
+}
 
 
