34061449d8737b5454e727db19291114a659f676
FavouriteDragon <ninjadragon008@icloud.com>
2017-09-19 16:57:11 -0700

    disabled rotation
    xD

--- src/main/java/com/crowsofwar/avatar/client/render/RenderLightningSpear.java
+++ src/main/java/com/crowsofwar/avatar/client/render/RenderLightningSpear.java
@@ -36,7 +36,7 @@ public class RenderLightningSpear extends Render<EntityLightningSpear> {
 
         Matrix4f mat = new Matrix4f();
         mat.translate((float) x, (float) y + .1f, (float) z);
-        mat.rotate(ticks, 0, 1, 0);
+        
 
         //@formatter:off
         float n = -.75f, p = .75f;
