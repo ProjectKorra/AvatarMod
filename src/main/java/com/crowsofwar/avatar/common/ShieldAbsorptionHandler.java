15ea4a13a19a0e154a0d3502cd2041779355c8cd
CrowsOfWar <ofwarcrows@gmail.com>
2017-09-22 17:50:58 -0500

    Make ShieldAbsorptionHandler work for any type of shield

--- src/main/java/com/crowsofwar/avatar/common/ShieldAbsorptionHandler.java
+++ src/main/java/com/crowsofwar/avatar/common/ShieldAbsorptionHandler.java
@@ -1,11 +1,8 @@
 package com.crowsofwar.avatar.common;
 
 import com.crowsofwar.avatar.AvatarInfo;
-import com.crowsofwar.avatar.common.bending.StatusControl;
-import com.crowsofwar.avatar.common.data.Bender;
-import com.crowsofwar.avatar.common.data.BendingData;
 import com.crowsofwar.avatar.common.entity.AvatarEntity;
-import com.crowsofwar.avatar.common.entity.EntityAirBubble;
+import com.crowsofwar.avatar.common.entity.EntityShield;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.util.SoundCategory;
@@ -13,6 +10,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import scala.tools.ant.sabbus.Make;
 
 /**
  * @author CrowsOfWar
@@ -22,22 +20,18 @@ public class ShieldAbsorptionHandler {
 
 	@SubscribeEvent
 	public static void shieldEntityAbsorb(LivingAttackEvent e) {
-		World world = e.getEntity().world;
 
+		World world = e.getEntity().world;
 		EntityLivingBase attacked = (EntityLivingBase) e.getEntity();
 
-		if (Bender.isBenderSupported(attacked)) {
-			BendingData data = Bender.get(attacked).getData();
-			if (data.hasStatusControl(StatusControl.BUBBLE_CONTRACT)) {
-				EntityAirBubble bubble = AvatarEntity.lookupControlledEntity(world, EntityAirBubble.class,
-						attacked);
-				if (bubble != null) {
-					if (bubble.attackEntityFrom(e.getSource(), e.getAmount())) {
-						e.setCanceled(true);
-						world.playSound(null, attacked.getPosition(), SoundEvents.BLOCK_CLOTH_HIT,
-								SoundCategory.PLAYERS, 1, 1);
-					}
-				}
+		EntityShield shield = AvatarEntity.lookupControlledEntity(world, EntityShield.class,
+				attacked);
+
+		if (shield != null) {
+			if (shield.attackEntityFrom(e.getSource(), e.getAmount())) {
+				e.setCanceled(true);
+				world.playSound(null, attacked.getPosition(), SoundEvents.BLOCK_CLOTH_HIT,
+						SoundCategory.PLAYERS, 1, 1);
 			}
 		}
 
