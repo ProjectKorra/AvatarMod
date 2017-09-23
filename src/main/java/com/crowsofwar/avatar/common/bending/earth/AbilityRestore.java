2160da6cf6adaf92f859cecef0d6523176d4f157
FavouriteDragon <ninjadragon008@icloud.com>
2017-09-23 09:45:00 -0700

    levels boiii

--- src/main/java/com/crowsofwar/avatar/common/bending/earth/AbilityRestore.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/earth/AbilityRestore.java
@@ -30,17 +30,20 @@ public class AbilityRestore extends Ability {
         if (bender.consumeChi(STATS_CONFIG.chiRestore)){
             AbilityData abilityData = data.getAbilityData(this);
             entity.addPotionEffect(new PotionEffect (MobEffects.INSTANT_HEALTH, 1));
-            if (abilityData.getLevel()==1){
-                entity.addPotionEffect(new PotionEffect (MobEffects.INSTANT_HEALTH, 1));
+            if (abilityData.getLevel()==1) {
+                entity.addPotionEffect(new PotionEffect(MobEffects.INSTANT_HEALTH, 1));
                 entity.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 100));
-                if (abilityData.getLevel()==2){
+            }
+                if (abilityData.getLevel()==2) {
                     entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100));
-                    entity.addPotionEffect(new PotionEffect (MobEffects.INSTANT_HEALTH, 1));
-                    entity.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 100, 1));
-                    if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)){
-                        entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100, 1 ));
-                        entity.addPotionEffect(new PotionEffect (MobEffects.INSTANT_HEALTH, 1, 1));
+                    entity.addPotionEffect(new PotionEffect(MobEffects.INSTANT_HEALTH, 1));
+                    entity.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 100));
+                }
+                    if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
+                        entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100, 1));
+                        entity.addPotionEffect(new PotionEffect(MobEffects.INSTANT_HEALTH, 1, 1));
                         entity.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 300, 1));
+                    }
                         if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)){
                             entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100));
                             entity.addPotionEffect(new PotionEffect (MobEffects.INSTANT_HEALTH, 1));
@@ -53,7 +56,7 @@ public class AbilityRestore extends Ability {
                 }
 
             }
-        }
 
-    }
-}
+
+
+
