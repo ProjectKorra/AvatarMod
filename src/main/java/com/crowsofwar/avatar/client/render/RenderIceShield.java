a68990c6adf65dd2cf366aaf3194babfc66a45ca
CrowsOfWar <ofwarcrows@gmail.com>
2017-09-23 12:06:09 -0500

    Previous ModelRenderer-rendering classes use RenderModel; cleanup

--- src/main/java/com/crowsofwar/avatar/client/render/RenderIceShield.java
+++ src/main/java/com/crowsofwar/avatar/client/render/RenderIceShield.java
@@ -1,10 +1,7 @@
 package com.crowsofwar.avatar.client.render;
 
 import com.crowsofwar.avatar.common.entity.EntityIceShield;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 
@@ -13,33 +10,13 @@ import javax.annotation.Nullable;
 /**
  * @author CrowsOfWar
  */
-public class RenderIceShield extends Render<EntityIceShield> {
+public class RenderIceShield extends RenderModel<EntityIceShield> {
 
 	private static final ResourceLocation TEXTURE = new ResourceLocation("avatarmod",
 			"textures/entity/ice-shield.png");
 
-	private ModelBase model;
-
 	public RenderIceShield(RenderManager renderManager) {
-		super(renderManager);
-		model = new ModelIceShield();
-	}
-
-	@Override
-	public void doRender(EntityIceShield entity, double x, double y, double z, float entityYaw,
-						 float partialTicks) {
-
-		Minecraft.getMinecraft().renderEngine.bindTexture(TEXTURE);
-		GlStateManager.enableBlend();
-
-		GlStateManager.pushMatrix();
-		GlStateManager.translate(x, y, z);
-		GlStateManager.translate(0, 0.3, 0);
-		model.render(entity, 0, 0, 0, 0, 0, 0.0625f);
-		GlStateManager.popMatrix();
-
-		GlStateManager.disableBlend();
-
+		super(renderManager, new ModelIceShield());
 	}
 
 	@Nullable
@@ -48,4 +25,8 @@ public class RenderIceShield extends Render<EntityIceShield> {
 		return TEXTURE;
 	}
 
+	@Override
+	protected void performGlTransforms(EntityIceShield entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		GlStateManager.translate(0, 0.3, 0);
+	}
 }
