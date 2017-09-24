d971643d56288ca4dea566b4e8c6daa2df8143a0
CrowsOfWar <ofwarcrows@gmail.com>
2017-09-24 07:33:31 -0500

    Power rating modifiers act for a finite amount of time

--- src/main/java/com/crowsofwar/avatar/common/data/PowerRating.java
+++ src/main/java/com/crowsofwar/avatar/common/data/PowerRating.java
@@ -3,6 +3,7 @@ package com.crowsofwar.avatar.common.data;
 import com.crowsofwar.avatar.common.data.ctx.BendingContext;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
@@ -31,10 +32,25 @@ public class PowerRating {
 
 	public void addModifier(PowerRatingModifier modifier) {
 		modifiers.add(modifier);
+		modifier.setTicks(20);
 	}
 
 	public void removeModifier(PowerRatingModifier modifier) {
 		modifiers.remove(modifier);
+	}
+
+	/**
+	 * Modifiers are present for a finite amount of time. Counts down each modifier's time left and
+	 * removes them if necessary.
+	 */
+	public void tickModifiers() {
+		Iterator<PowerRatingModifier> iterator = modifiers.iterator();
+		while (iterator.hasNext()) {
+			PowerRatingModifier modifier = iterator.next();
+			if (modifier.onUpdate()) {
+				iterator.remove();
+			}
+		}
 	}
 
 }
