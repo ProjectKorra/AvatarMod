e54a8fe49066dcd1fdf0767ab4f736e186d26426
FavouriteDragon <ninjadragon008@icloud.com>
2017-10-05 19:41:56 -0700

    ugh silly updating code

--- src/main/java/com/crowsofwar/avatar/common/bending/earth/AbilityPickUpBlock.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/earth/AbilityPickUpBlock.java
@@ -113,26 +113,24 @@ public class AbilityPickUpBlock extends Ability {
 				floating.setPosition(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
 				floating.setItemDropsEnabled(!bender.isCreativeMode());
 				float damageMult = abilityData.getLevel() >= 2 ? 2 : 1;
-<<<<<<< HEAD
+
 				damageMult += ctx.getPowerRating() / 200;
 
-=======
+
 				//damageMult += ctx.getPowerRating() / 200;
 				floating.setDamageMult(damageMult);
 				
->>>>>>> FavouriteDragon/a5.0+
+
 				double dist = 2.5;
 				Vector force = new Vector(0, Math.sqrt(19.62 * dist), 0);
 				floating.setVelocity(force);
 				floating.setBehavior(new FloatingBlockBehavior.PickUp());
 				floating.setOwner(entity);
 				floating.setDamageMult(damageMult);
-<<<<<<< HEAD
-=======
 
 
 
->>>>>>> FavouriteDragon/a5.0+
+
 				
 				if (STATS_CONFIG.preventPickupBlockGriefing) {
 					floating.setItemDropsEnabled(false);
