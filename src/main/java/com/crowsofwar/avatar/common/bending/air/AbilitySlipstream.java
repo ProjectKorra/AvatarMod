8f5158c587c9786faf69455a06f90bc9d2b69b59
FavouriteDragon <ninjadragon008@icloud.com>
2017-10-08 16:23:04 -0700

    cleaned up code and did power ratings for slipstream!

--- src/main/java/com/crowsofwar/avatar/common/bending/air/AbilitySlipstream.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/air/AbilitySlipstream.java
@@ -1,8 +1,6 @@
 package com.crowsofwar.avatar.common.bending.air;
 
-import com.crowsofwar.avatar.common.bending.Abilities;
 import com.crowsofwar.avatar.common.bending.Ability;
-import com.crowsofwar.avatar.common.bending.earth.Earthbending;
 import com.crowsofwar.avatar.common.data.AbilityData;
 import com.crowsofwar.avatar.common.data.Bender;
 import com.crowsofwar.avatar.common.data.BendingData;
@@ -10,7 +8,6 @@ import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.world.World;
 
 import static com.crowsofwar.avatar.common.config.ConfigSkills.SKILLS_CONFIG;
 import static com.crowsofwar.avatar.common.config.ConfigStats.STATS_CONFIG;
@@ -25,9 +22,9 @@ public class AbilitySlipstream extends Ability {
         BendingData data = ctx.getData();
         EntityLivingBase entity = ctx.getBenderEntity();
         Bender bender = ctx.getBender();
-        World world = ctx.getWorld();
         if (bender.consumeChi(STATS_CONFIG.chiSlipstream)) {
             float xp = SKILLS_CONFIG.blockPlaced;
+
             AbilityData abilityData = data.getAbilityData(this);
 
                 entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
@@ -42,6 +39,7 @@ public class AbilitySlipstream extends Ability {
                     entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
                     data.getAbilityData("slipstream").addXp(xp-0.5F);
 
+
                 }
 
                     if (abilityData.getLevel()==2) {
@@ -50,6 +48,7 @@ public class AbilitySlipstream extends Ability {
                         entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100, 1));
                         entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100, 1));
                         data.getAbilityData("slipstream").addXp(xp - 1.0F);
+
                     }
 
 
@@ -59,16 +58,18 @@ public class AbilitySlipstream extends Ability {
                             entity.addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 100));
                             entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100, 1));
                             entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100, 1));
+
                         }
 
                             if (data.getAbilityData("slipstream").isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
                                 entity.addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 200));
                                 entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 200, 2));
                                 entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 200, 2));
+
+                                }
                             }
 
-                            }
-
+             data.getPowerRatingManager(getBendingId()).addModifier(new SlipstreamPowerModifier());
                         }
                     }
 
---
+++ src/main/java/com/crowsofwar/avatar/common/bending/air/SlipstreamPowerModifier.java
@@ -0,0 +1,19 @@
+package com.crowsofwar.avatar.common.bending.air;
+
+import com.crowsofwar.avatar.common.data.AbilityData;
+import com.crowsofwar.avatar.common.data.BendingData;
+import com.crowsofwar.avatar.common.data.PowerRatingModifier;
+import com.crowsofwar.avatar.common.data.ctx.BendingContext;
+
+public class SlipstreamPowerModifier extends PowerRatingModifier {
+    @Override
+    public double get(BendingContext ctx) {
+        AbilitySlipstream slipstream = new AbilitySlipstream();
+        BendingData data = ctx.getData();
+        AbilityData abilityData = data.getAbilityData(slipstream);
+        return 10*2*abilityData.getLevel();
+
+
+    }
+
+}
