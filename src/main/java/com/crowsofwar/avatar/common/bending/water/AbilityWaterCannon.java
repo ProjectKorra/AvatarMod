2b3d70471f9a4e432c9c77f580a8c9721e908c79
CrowsOfWar <ofwarcrows@gmail.com>
2017-10-14 14:15:01 -0500

    Cleanup AbilityWaterCannon

--- src/main/java/com/crowsofwar/avatar/common/bending/water/AbilityWaterCannon.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/water/AbilityWaterCannon.java
@@ -28,57 +28,16 @@ public class AbilityWaterCannon extends Ability{
 
 		Bender bender = ctx.getBender();
 		BendingData data = ctx.getData();
-		EntityLivingBase entity = ctx.getBenderEntity();
 
 		boolean hasChi = bender.consumeChi(STATS_CONFIG.chiLightning);
-		boolean hasLightningCharge = data.hasTickHandler(TickHandler.WATER_CHARGE);
-		Vector targetPos = getClosestWaterBlock(entity, ctx.getLevel());
+		boolean hasWaterCharge = data.hasTickHandler(TickHandler.WATER_CHARGE);
 
-		if (targetPos != null || ctx.consumeWater(3)) {
-			if (targetPos == null) {
-				targetPos = Vector.getEyePos(entity).plus(Vector.getLookRectangular(entity).times(4));
-			}
-
-			if (hasChi && !hasLightningCharge) {
+		if (ctx.consumeWater(3)) {
+			if (hasChi && !hasWaterCharge) {
 				ctx.getData().addTickHandler(TickHandler.WATER_CHARGE);
 			}
 		}
 	}
 
-
-
-	private Vector getClosestWaterBlock(EntityLivingBase entity, int level) {
-		World world = entity.world;
-
-		Vector eye = Vector.getEyePos(entity);
-
-		double rangeMult = 0.6;
-		if (level >= 1) {
-			rangeMult = 1;
-		}
-
-		double range = STATS_CONFIG.waterArcSearchRadius * rangeMult;
-		for (int i = 0; i < STATS_CONFIG.waterArcAngles; i++) {
-			for (int j = 0; j < STATS_CONFIG.waterArcAngles; j++) {
-
-				double yaw = entity.rotationYaw + i * 360.0 / STATS_CONFIG.waterArcAngles;
-				double pitch = entity.rotationPitch + j * 360.0 / STATS_CONFIG.waterArcAngles;
-
-				BiPredicate<BlockPos, IBlockState> isWater = (pos, state) -> state.getBlock() == Blocks.WATER
-						|| state.getBlock() == Blocks.FLOWING_WATER;
-
-				Vector angle = Vector.toRectangular(toRadians(yaw), toRadians(pitch));
-				Raytrace.Result result = Raytrace.predicateRaytrace(world, eye, angle, range, isWater);
-				if (result.hitSomething()) {
-					return result.getPosPrecise();
-				}
-
-			}
-
-		}
-
-		return null;
-
-	}
 }
 
