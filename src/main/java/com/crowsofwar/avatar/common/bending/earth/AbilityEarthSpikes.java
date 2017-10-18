266be02aad0cae3ae3bf2acdd11f9f72c8169ddb
CrowsOfWar <ofwarcrows@gmail.com>
2017-10-18 18:50:02 -0500

    Start debugging earthspikes

--- src/main/java/com/crowsofwar/avatar/common/bending/earth/AbilityEarthSpikes.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/earth/AbilityEarthSpikes.java
@@ -22,108 +22,93 @@ import net.minecraftforge.fml.common.FMLLog;
 import static com.crowsofwar.avatar.common.config.ConfigStats.STATS_CONFIG;
 
 public class AbilityEarthSpikes extends Ability {
-    public double i = 0;
-    public float damage = 0.90F;
+	public float damage = 0.90F;
 
-    private Logger logger = LogManager.getLogger(this.getClass());
+	private Logger logger = LogManager.getLogger(this.getClass());
 
-    public AbilityEarthSpikes() {
-        super(Earthbending.ID, "earthspike");
-    }
+	public AbilityEarthSpikes() {
+		super(Earthbending.ID, "earthspike");
+	}
 
-    @Override
-    public void execute(AbilityContext ctx) {
-        AbilityData abilityData = ctx.getData().getAbilityData(this);
-        float xp = abilityData.getTotalXp();
-        EntityLivingBase entity = ctx.getBenderEntity();
-        World world = ctx.getWorld();
-        Bender bender = ctx.getBender();
-        if (i >= 8){
-            i = 0;
-        }
+	@Override
+	public void execute(AbilityContext ctx) {
 
-        float chi = STATS_CONFIG.chiEarthspike;
-        if (ctx.isMasterLevel(AbilityData.AbilityTreePath.FIRST)) {
-            chi *= 2.5f;
-            damage = 1.25f;
-        }
-        if (ctx.getLevel() == 1 || ctx.getLevel() == 2) {
-            chi *= 1.5f;
-            damage = 1F;
-        }
-        if (ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND)) {
-            chi *= 2f;
-            damage = 1.5F;
-        }
+		System.out.println("EXecute");
 
-        if (bender.consumeChi(chi)) {
-            EntityEarthspikeSpawner earthspike = new EntityEarthspikeSpawner(world);
-            Vector look = Vector.toRectangular(Math.toRadians(entity.rotationYaw), 0);
-            double mult = ctx.getLevel() >= 1 ? 14 : 8;
-            // Vector speed =(look.times(mult));
-            double speed = 3;
+		AbilityData abilityData = ctx.getData().getAbilityData(this);
+		float xp = abilityData.getTotalXp();
+		EntityLivingBase entity = ctx.getBenderEntity();
+		World world = ctx.getWorld();
+		Bender bender = ctx.getBender();
 
-            if (abilityData.getLevel() == 1) {
-                earthspike.setVelocity(look.times(mult * 5));
+		float chi = STATS_CONFIG.chiEarthspike;
+		if (ctx.isMasterLevel(AbilityData.AbilityTreePath.FIRST)) {
+			chi *= 2.5f;
+			damage = 1.25f;
+		}
+		if (ctx.getLevel() == 1 || ctx.getLevel() == 2) {
+			chi *= 1.5f;
+			damage = 1F;
+		}
+		if (ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND)) {
+			chi *= 2f;
+			damage = 1.5F;
+		}
 
-            }
-            if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
-                earthspike.setVelocity(look.times(mult * 10));
-            }
+		System.out.println("AAA");
 
-          //  Double angle = i * 45;
-            // Vector direction =  Vector.toRectangular(Math.toRadians(entity.rotationYaw + i*45), Math.toRadians(entity.rotationPitch));
-            // Vector velocity = direction.times(speed);
+		if (bender.consumeChi(chi)) {
+			EntityEarthspikeSpawner earthspike = new EntityEarthspikeSpawner(world);
+			Vector look = Vector.toRectangular(Math.toRadians(entity.rotationYaw), 0);
+			double mult = ctx.getLevel() >= 1 ? 14 : 8;
+			// Vector speed =(look.times(mult));
+			double speed = 3;
 
-            earthspike.setOwner(entity);
-            earthspike.setPosition(entity.posX, entity.posY, entity.posZ);
-            earthspike.setVelocity(look.times(mult));
-            earthspike.setDamageMult(damage + xp / 100);
-            earthspike.setDistance(ctx.getLevel() >= 2 ? 16 : 10);
-            earthspike.setBreakBlocks(ctx.isMasterLevel(AbilityData.AbilityTreePath.FIRST));
-            earthspike.setDropEquipment(ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND));
-            world.spawnEntity(earthspike);
+			if (abilityData.getLevel() == 1) {
+				earthspike.setVelocity(look.times(mult * 5));
 
-            if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
-//                for (double i = 0; i < 8; i++) {
-//                    EntityEarthspikeSpawner spawner = new EntityEarthspikeSpawner(world);
-//                    spawner.setVelocity(velocity);
-//                    world.spawnEntity(spawner);
-//
-//
-//                }
-//
-//                //
-               //  Start facing north
+			}
+			if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
+				earthspike.setVelocity(look.times(mult * 10));
+			}
+			earthspike.setOwner(entity);
+			earthspike.setPosition(entity.posX, entity.posY, entity.posZ);
+			earthspike.setVelocity(look.times(mult));
+			earthspike.setDamageMult(damage + xp / 100);
+			earthspike.setDistance(ctx.getLevel() >= 2 ? 16 : 10);
+			earthspike.setBreakBlocks(ctx.isMasterLevel(AbilityData.AbilityTreePath.FIRST));
+			earthspike.setDropEquipment(ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND));
+//			world.spawnEntity(earthspike);
 
+			System.out.println("AAA");
 
-/*            logger.warn("Inside condition");
-                EnumFacing facing = EnumFacing.NORTH;
-                 Vector direction = new Vector();
-                for (int i = 0; i < 4; i++) {
-                    EntityEarthspikeSpawner spawner = new EntityEarthspikeSpawner(world);
-                    spawner.setVelocity(new Vector(facing));
-                    world.spawnEntity(spawner);
-                    facing.rotateY();
-                    logger.warn("Spawn: "+i);
+			if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
 
-                }**/
+				for (int i = 0; i < 8; i++) {
 
-                logger.warn("Inside condition");
-              for (double i = 0; i < 8; i++) {
-                    Vector direction1 = Vector.toRectangular(Math.toRadians(entity.rotationYaw + i*45), Math.toRadians(entity.rotationPitch));
-                    Vector velocity = direction1.times(speed);
-                    EntityEarthspikeSpawner spawner = new EntityEarthspikeSpawner(world);
-                    spawner.setVelocity(velocity);
-                    world.spawnEntity(spawner);
-                  logger.warn("Spawn: "+i);
-                  logger.warn("direction: "+direction1.toString());
-                }
-            }
-            else {
-                logger.warn("not in condition");
-            }
+					System.out.println(i);
 
-        }
-    }
+					Vector direction1 = Vector.toRectangular(Math.toRadians(entity.rotationYaw + i * 45), Math.toRadians(entity.rotationPitch));
+					Vector velocity = direction1.times(speed);
+					EntityEarthspikeSpawner spawner = new EntityEarthspikeSpawner(world);
+					spawner.setVelocity(look.times(mult));
+
+					spawner.setOwner(entity);
+					spawner.setPosition(entity.posX, entity.posY, entity.posZ);
+					spawner.setDamageMult(damage + xp / 100);
+					spawner.setDistance(ctx.getLevel() >= 2 ? 16 : 10);
+					spawner.setBreakBlocks(ctx.isMasterLevel(AbilityData.AbilityTreePath.FIRST));
+					spawner.setDropEquipment(ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND));
+
+					world.spawnEntity(spawner);
+					System.out.println(velocity);
+					logger.warn("Spawn: " + i);
+					logger.warn("direction: " + direction1.toString());
+				}
+			} else {
+				logger.warn("not in condition");
+			}
+
+		}
+	}
 }
