57ab610aee0c7893950dee68093fa273c3a56382
CrowsOfWar <ofwarcrows@gmail.com>
2017-10-21 18:05:06 -0500

    Sandstorm ability spawns a sandstorm ability

--- src/main/java/com/crowsofwar/avatar/common/bending/sand/AbilitySandstorm.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/sand/AbilitySandstorm.java
@@ -2,6 +2,12 @@ package com.crowsofwar.avatar.common.bending.sand;
 
 import com.crowsofwar.avatar.common.bending.Ability;
 import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
+import com.crowsofwar.avatar.common.entity.EntitySandstorm;
+import com.crowsofwar.avatar.common.util.Raytrace;
+import com.crowsofwar.gorecore.data.WorldData;
+import com.crowsofwar.gorecore.util.Vector;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.world.World;
 
 import java.util.UUID;
 
@@ -13,6 +19,21 @@ public class AbilitySandstorm extends Ability {
 
 	@Override
 	public void execute(AbilityContext ctx) {
+
+		World world = ctx.getWorld();
+		EntityLivingBase entity = ctx.getBenderEntity();
+
+		Raytrace.Result raytrace = Raytrace.getTargetBlock(entity, 20, false);
+		if (raytrace.hitSomething()) {
+
+			Vector hitPos = raytrace.getPosPrecise();
+
+			EntitySandstorm sandstorm = new EntitySandstorm(world);
+			sandstorm.setPosition(hitPos);
+			sandstorm.setOwner(entity);
+			world.spawnEntity(sandstorm);
+
+		}
 
 	}
 
