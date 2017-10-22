54011e7ad6d443eb5ce1be075f79512b1926c0cb
CrowsOfWar <ofwarcrows@gmail.com>
2017-10-22 12:38:35 -0500

    Clean up sandstorm twirling code

--- src/main/java/com/crowsofwar/avatar/common/entity/EntitySandstorm.java
+++ src/main/java/com/crowsofwar/avatar/common/entity/EntitySandstorm.java
@@ -38,62 +38,22 @@ public class EntitySandstorm extends AvatarEntity {
 	@Override
 	protected void onCollideWithEntity(Entity entity) {
 
-		// The overall acceleration for the target in m/s
-		Vector acceleration = new Vector(0, 1, 0);
-
-		/*
-		Orbital mechanics:
-		The sandstorm twirling targets in the air is basically orbital mechanics. The target has
-		an existing velocity tangent to its orbit trajectory, and the only force acting on it is
-		"gravity" towards the center of the sandstorm.
-
-		Speed required to "orbit" is :: sqrt(2dg - g^2)
-		where d = distance from sandstorm center, g = "gravity" towards center of sandstorm (m/s)
-		 */
-
-//		final double gravity = 2.5;
-//		double distance = entity.getDistanceToEntity(this);
-//
-//		if (2 * gravity * distance - gravity * gravity <= 0) {
-//			distance = 2 / gravity + 1;
-//		}
-//
-//		double orbitalSpeed = Math.sqrt(2 * gravity * distance - gravity * gravity) * 2;
-//
-//		Vector gravityForce = this.position().minus(Vector.getEntityPos(entity)).times(orbitalSpeed);
-//
-//		acceleration = acceleration.plus(gravityForce);
-//
-//		entity.addVelocity(acceleration.x() / 20, acceleration.y() / 20, acceleration.z() / 20);
-//
-//		if (acceleration.sqrMagnitude() < 0.8) {
-//			entity.setPosition(entity.posX + 1, entity.posY, entity.posZ);
-//			entity.addVelocity(0, )
-//		}
+		// Rotates the entity around this sandstorm
+		// First: calculates current angle, and the next angle
+		// Then, calculates position with that next angle
+		// Finally, finds a velocity which will move towards that point
 
 		double currentAngle = Vector.getRotationTo(position(), Vector.getEntityPos(entity)).y();
 		double nextAngle = currentAngle + Math.toRadians(360 / 20);
-//		System.out.println(currentAngle + " -> " + nextAngle);
 
-		Vector myNextPos = position();
-		Vector theirNextPos = myNextPos.plus(Vector.toRectangular(nextAngle, 0)).plusY(2);
-		Vector delta = theirNextPos.minus(Vector.getEntityPos(entity));
+		Vector nextPos = position().plus(Vector.toRectangular(nextAngle, 0)).plusY(2);
+		Vector delta = nextPos.minus(Vector.getEntityPos(entity));
 
-		Vector theirNextVelocity = velocity();
-		theirNextVelocity = theirNextVelocity.plus(delta.times(20));
-		entity.setVelocity(theirNextVelocity.x() / 20, theirNextVelocity.y() / 20,
-				theirNextVelocity.z
-				() / 20);
-//		entity.setPosition(nextPos.x(), nextPos.y(), nextPos.z());
-//
-//entity.motionY += 0.1;
-//
+		Vector nextVelocity = velocity().plus(delta.times(20));
+		entity.setVelocity(nextVelocity.x() / 20, nextVelocity.y() / 20, nextVelocity.z() / 20);
+
 		AvatarUtils.afterVelocityAdded(entity);
-//
-//		setVelocity(Vector.NORTH.times(3));
-//		setVelocity(Vector.ZERO);
 
-//		setDead();
 	}
 
 	public SandstormMovementHandler getMovementHandler() {
