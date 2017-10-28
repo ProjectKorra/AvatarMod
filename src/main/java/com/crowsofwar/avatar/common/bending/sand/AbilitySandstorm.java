841f16277d5a7512facf3e516436443233b2b5b0
CrowsOfWar <ofwarcrows@gmail.com>
2017-10-28 18:03:57 -0500

    Add inital chi cost to sandstorm

--- src/main/java/com/crowsofwar/avatar/common/bending/sand/AbilitySandstorm.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/sand/AbilitySandstorm.java
@@ -2,6 +2,8 @@ package com.crowsofwar.avatar.common.bending.sand;
 
 import com.crowsofwar.avatar.common.bending.Ability;
 import com.crowsofwar.avatar.common.bending.StatusControl;
+import com.crowsofwar.avatar.common.config.ConfigStats;
+import com.crowsofwar.avatar.common.data.Bender;
 import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
 import com.crowsofwar.avatar.common.entity.EntitySandstorm;
 import com.crowsofwar.gorecore.util.Vector;
@@ -19,16 +21,21 @@ public class AbilitySandstorm extends Ability {
 
 		World world = ctx.getWorld();
 		EntityLivingBase entity = ctx.getBenderEntity();
+		Bender bender = ctx.getBender();
 
-		Vector velocity = Vector.toRectangular(Math.toRadians(entity.rotationYaw), 0).times(8);
+		if (bender.consumeChi(ConfigStats.STATS_CONFIG.chiSandstorm)) {
 
-		EntitySandstorm sandstorm = new EntitySandstorm(world);
-		sandstorm.setPosition(Vector.getEntityPos(entity));
-		sandstorm.setOwner(entity);
-		sandstorm.setVelocity(velocity);
-		world.spawnEntity(sandstorm);
+			Vector velocity = Vector.toRectangular(Math.toRadians(entity.rotationYaw), 0).times(8);
 
-		ctx.getData().addStatusControl(StatusControl.SANDSTORM_REDIRECT);
+			EntitySandstorm sandstorm = new EntitySandstorm(world);
+			sandstorm.setPosition(Vector.getEntityPos(entity));
+			sandstorm.setOwner(entity);
+			sandstorm.setVelocity(velocity);
+			world.spawnEntity(sandstorm);
+
+			ctx.getData().addStatusControl(StatusControl.SANDSTORM_REDIRECT);
+
+		}
 
 	}
 
