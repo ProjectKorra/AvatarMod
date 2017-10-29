897a1ac23e8090f584fa437a5838f798316ccb92
CrowsOfWar <ofwarcrows@gmail.com>
2017-10-28 19:32:03 -0500

    Properly position sandstorm model

--- src/main/java/com/crowsofwar/avatar/client/render/RenderSandstorm.java
+++ src/main/java/com/crowsofwar/avatar/client/render/RenderSandstorm.java
@@ -1,6 +1,7 @@
 package com.crowsofwar.avatar.client.render;
 
 import com.crowsofwar.avatar.common.entity.EntitySandstorm;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 
@@ -16,6 +17,15 @@ public class RenderSandstorm extends RenderModel<EntitySandstorm> {
 
 	public RenderSandstorm(RenderManager renderManager) {
 		super(renderManager, new ModelSandstorm());
+	}
+
+	@Override
+	protected void performGlTransforms(EntitySandstorm entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		GlStateManager.rotate(180, 1, 0, 0);
+		GlStateManager.translate(0, -2.5, 0);
+		GlStateManager.scale(3, 3, 3);
+
+
 	}
 
 	@Nullable
