157286a13f9b36d8cf02f826074cfc58bebc1e40
Crows Of War <ofwarcrows@gmail.com>
2017-10-29 21:29:02 -0500

    Fix sandstorm not animating itself

--- src/main/java/com/crowsofwar/avatar/client/render/ModelSandstorm.java
+++ src/main/java/com/crowsofwar/avatar/client/render/ModelSandstorm.java
@@ -35,6 +35,7 @@ public class ModelSandstorm extends ModelBase {
 
 	@Override
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
 		this.mainPart.render(f5);
 	}
 
