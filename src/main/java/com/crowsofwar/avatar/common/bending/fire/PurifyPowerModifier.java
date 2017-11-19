4b9a6bc4076f9f2bba6adb976688c1d69859f24c
CrowsOfWar <ofwarcrows@gmail.com>
2017-11-19 13:51:17 -0600

    PurifyPowerModifier sets Vision to PURIFY

--- src/main/java/com/crowsofwar/avatar/common/bending/fire/PurifyPowerModifier.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/fire/PurifyPowerModifier.java
@@ -4,15 +4,31 @@ package com.crowsofwar.avatar.common.bending.fire;
 import com.crowsofwar.avatar.common.data.AbilityData;
 import com.crowsofwar.avatar.common.data.BendingData;
 import com.crowsofwar.avatar.common.data.PowerRatingModifier;
+import com.crowsofwar.avatar.common.data.Vision;
 import com.crowsofwar.avatar.common.data.ctx.BendingContext;
 
 public class PurifyPowerModifier extends PowerRatingModifier {
     @Override
     public double get(BendingContext ctx) {
-        BendingData data = ctx.getData();
+    	BendingData data = ctx.getData();
         AbilityData abilityData = data.getAbilityData(new AbilityPurify());
         return 10+(3*abilityData.getLevel());
 
     }
+
+	@Override
+	public void onAdded(BendingContext ctx) {
+		if (ctx.getData().getVision() == null) {
+			ctx.getData().setVision(Vision.PURIFY);
+		}
+	}
+
+	@Override
+	public void onRemoval(BendingContext ctx) {
+    	if (ctx.getData().getVision() == Vision.PURIFY) {
+			ctx.getData().setVision(null);
+		}
+	}
+
 }
 
