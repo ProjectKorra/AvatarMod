b6218707a4389ee2261ec78682916eebf259e5f3
CrowsOfWar <ofwarcrows@gmail.com>
2017-11-23 19:45:33 -0600

    Format Purify code

--- src/main/java/com/crowsofwar/avatar/common/bending/fire/AbilityPurify.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/fire/AbilityPurify.java
@@ -13,70 +13,64 @@ import static com.crowsofwar.avatar.common.config.ConfigSkills.SKILLS_CONFIG;
 import static com.crowsofwar.avatar.common.config.ConfigStats.STATS_CONFIG;
 
 public class AbilityPurify extends Ability {
-    public AbilityPurify() {
-        super(Firebending.ID, "purify");
-    }
 
-    @Override
-    public void execute(AbilityContext ctx) {
-        BendingData data = ctx.getData();
-        EntityLivingBase entity = ctx.getBenderEntity();
-        Bender bender = ctx.getBender();
-        AbilityData abilityData = data.getAbilityData(this);
-        float chi = STATS_CONFIG.chiBuff;
-        if (abilityData.getLevel()==1){
-            chi *= 1.5f;
-        }
-        if (abilityData.getLevel()==2){
-            chi *= 2f;
-        }
-        if (ctx.isMasterLevel(AbilityData.AbilityTreePath.FIRST)){
-            chi *= 2.5F;
-        }
-        if (ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND)){
-            chi *= 2.5F;
-        }
-        if (bender.consumeChi(chi)) {
-            float xp = SKILLS_CONFIG.buffUsed;
+	public AbilityPurify() {
+		super(Firebending.ID, "purify");
+	}
 
-            entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100));
-            data.getAbilityData("purify").addXp(xp);
+	@Override
+	public void execute(AbilityContext ctx) {
 
+		BendingData data = ctx.getData();
+		EntityLivingBase entity = ctx.getBenderEntity();
+		Bender bender = ctx.getBender();
+		AbilityData abilityData = data.getAbilityData(this);
 
-            if (abilityData.getLevel() == 1) {
+		float chi = STATS_CONFIG.chiBuff;
+		if (abilityData.getLevel() == 1){
+			chi *= 1.5f;
+		} else if (abilityData.getLevel() ==2 ){
+			chi *= 2f;
+		} else if (abilityData.getLevel() == 3) {
+			chi *= 2.5f;
+		}
 
-                entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100));
-                entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
-                data.getAbilityData("purify").addXp(xp - 0.5F);
+		if (bender.consumeChi(chi)) {
+			float xp = SKILLS_CONFIG.buffUsed;
 
-            }
+			entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100));
+			data.getAbilityData("purify").addXp(xp);
 
-            if (abilityData.getLevel() == 2) {
+			if (abilityData.getLevel() == 1) {
+				entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100));
+				entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
+				data.getAbilityData("purify").addXp(xp);
+			}
 
-                entity.addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 100));
-                entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100, 1));
-                entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
-                data.getAbilityData("purify").addXp(xp - 1.0F);
-            }
+			if (abilityData.getLevel() == 2) {
+				entity.addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 100));
+				entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100, 1));
+				entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
+				data.getAbilityData("purify").addXp(xp);
+			}
 
+			if (data.getAbilityData("purify").isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
+				entity.addPotionEffect(new PotionEffect(MobEffects.HEALTH_BOOST, 100));
+				entity.addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 100));
+				entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100, 1));
+				entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100, 1));
+			}
 
-            if (data.getAbilityData("purify").isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
-                entity.addPotionEffect(new PotionEffect(MobEffects.HEALTH_BOOST, 100));
-                entity.addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 100));
-                entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100, 1));
-                entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100, 1));
-            }
+			if (data.getAbilityData("purify").isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
+				entity.addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, 200));
+				entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 200, 1));
+				entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 200, 1));
+				entity.addPotionEffect(new PotionEffect(MobEffects.GLOWING, 200));
+			}
 
-            if (data.getAbilityData("purify").isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
-                entity.addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, 200));
-                entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 200, 1));
-                entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 200, 1));
-                entity.addPotionEffect(new PotionEffect(MobEffects.GLOWING, 200));
-            }
-
-            PurifyPowerModifier modifier = new PurifyPowerModifier();
-            modifier.setTicks(20+(20*abilityData.getLevel()));
-            data.getPowerRatingManager(getBendingId()).addModifier(modifier, ctx);
-        }
-    }
+			PurifyPowerModifier modifier = new PurifyPowerModifier();
+			modifier.setTicks(20+(20*abilityData.getLevel()));
+			data.getPowerRatingManager(getBendingId()).addModifier(modifier, ctx);
+		}
+	}
 }
