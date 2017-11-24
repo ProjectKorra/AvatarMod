aba1ce97f1d4265d2d50cc1528b5f0982f96da64
CrowsOfWar <ofwarcrows@gmail.com>
2017-11-24 10:09:08 -0600

    Slipstream status FX & PR modifier has same duration

--- src/main/java/com/crowsofwar/avatar/common/bending/air/AbilitySlipstream.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/air/AbilitySlipstream.java
@@ -42,40 +42,29 @@ public class AbilitySlipstream extends Ability {
 		if (bender.consumeChi(chi)) {
 			float xp = SKILLS_CONFIG.buffUsed;
 
-			entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
+			// 4s base + 1s per level
+			int duration = 80 + 20 * abilityData.getLevel();
+
+			int effectLevel = abilityData.getLevel() >= 2 ? 1 : 0;
+
+			entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, duration, effectLevel));
 			data.getAbilityData("slipstream").addXp(xp);
 
 			if (abilityData.getLevel() == 1) {
-				entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100));
-				entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100));
-				data.getAbilityData("slipstream").addXp(xp - 0.5F);
+				entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, duration, effectLevel));
 			}
 
-			if (abilityData.getLevel() == 2) {
-				entity.addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 100));
-				entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100, 1));
-				entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100, 1));
-				data.getAbilityData("slipstream").addXp(xp - 1.0F);
+			if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
+				entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, duration));
 			}
 
-			if (data.getAbilityData("slipstream").isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
-				entity.addPotionEffect(new PotionEffect(MobEffects.SATURATION, 100));
-				entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 100));
-				entity.addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 100));
-				entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100, 1));
-				entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100, 1));
-			}
+			if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
 
-			if (data.getAbilityData("slipstream").isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
-				entity.addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 200));
-				entity.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 200, 2));
-				entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, 200, 2));
 			}
 
 			SlipstreamPowerModifier modifier = new SlipstreamPowerModifier();
-			modifier.setTicks(20 + (20 * abilityData.getLevel()));
-			data.getPowerRatingManager(getBendingId()).addModifier(new SlipstreamPowerModifier(),
-					ctx);
+			modifier.setTicks(duration);
+			data.getPowerRatingManager(getBendingId()).addModifier(modifier, ctx);
 
 		}
 
