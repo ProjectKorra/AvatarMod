d54c3581bcb88460f528f37d9e9bb058fba52077
CrowsOfWar <ofwarcrows@gmail.com>
2017-11-24 10:11:29 -0600

    Slipstream gives much higher power modifier

--- src/main/java/com/crowsofwar/avatar/common/bending/air/SlipstreamPowerModifier.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/air/SlipstreamPowerModifier.java
@@ -6,12 +6,21 @@ import com.crowsofwar.avatar.common.data.PowerRatingModifier;
 import com.crowsofwar.avatar.common.data.ctx.BendingContext;
 
 public class SlipstreamPowerModifier extends PowerRatingModifier {
-    @Override
-    public double get(BendingContext ctx) {
-        BendingData data = ctx.getData();
-        AbilityData abilityData = data.getAbilityData(new AbilitySlipstream());
-        return 10+(3*abilityData.getLevel());
 
-        }
-    }
+	@Override
+	public double get(BendingContext ctx) {
+
+		BendingData data = ctx.getData();
+		AbilityData abilityData = data.getAbilityData(new AbilitySlipstream());
+
+		double modifier = 20 + 8 * abilityData.getLevel();
+		if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
+			modifier = 60;
+		}
+
+		return modifier;
+
+	}
+
+}
 
