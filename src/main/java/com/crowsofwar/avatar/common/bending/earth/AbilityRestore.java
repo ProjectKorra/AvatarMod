47c5724a1f9c7785bee7b9196ab77473715ef83b
CrowsOfWar <ofwarcrows@gmail.com>
2017-11-25 10:28:26 -0600

    Last restore stats change

--- src/main/java/com/crowsofwar/avatar/common/bending/earth/AbilityRestore.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/earth/AbilityRestore.java
@@ -22,52 +22,54 @@ public class AbilityRestore extends Ability {
 
 	@Override
 	public void execute(AbilityContext ctx) {
+
 		BendingData data = ctx.getData();
 		AbilityData abilityData = data.getAbilityData(this);
 		EntityLivingBase entity = ctx.getBenderEntity();
 		Bender bender = ctx.getBender();
 		float chi = STATS_CONFIG.chiBuff;
+
 		if (abilityData.getLevel() == 1) {
 			chi *= 1.5f;
 		}
 		if (abilityData.getLevel() == 2) {
 			chi *= 2f;
 		}
-		if (ctx.isMasterLevel(AbilityData.AbilityTreePath.FIRST)) {
+		if (abilityData.getLevel() == 3) {
 			chi *= 2.5F;
 		}
-		if (ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND)) {
-			chi *= 2.5F;
-		}
+
 		if (bender.consumeChi(chi)) {
+
+			abilityData.addXp(SKILLS_CONFIG.buffUsed);
 
 			// 3s + 2.5s per level
 			int duration = 60 + 50 * abilityData.getLevel();
 			int effectLevel = 0;
-			int slownessLevel = abilityData.getLevel() >= 2 ? 1 : 0;
+			int slownessLevel = abilityData.getLevel() >= 2 ? 1 : 2;
+			int regenLevel = abilityData.getLevel() >= 2 ? 1 : 0;
 
-			if (data.getAbilityData("restore").isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
+			if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
 				effectLevel = 1;
 			}
 
-			float xp = SKILLS_CONFIG.buffUsed;
+			// Add potion effects
 
 			entity.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, duration, effectLevel));
 			entity.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, duration, slownessLevel));
-			data.getAbilityData("restore").addXp(xp);
 
 			if (abilityData.getLevel() >= 1) {
-				entity.addPotionEffect(new PotionEffect(MobEffects.INSTANT_HEALTH, 1, effectLevel));
-
+				entity.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 1, regenLevel));
 			}
 			if (abilityData.getLevel() >= 2) {
 				entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, duration, effectLevel));
-
 			}
-			if (data.getAbilityData("restore").isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
+			if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
 				entity.addPotionEffect(new PotionEffect(MobEffects.INSTANT_HEALTH, duration));
 				entity.addPotionEffect(new PotionEffect(MobEffects.SATURATION, duration));
 			}
+
+			// Apply power rating modifier
 
 			RestorePowerModifier modifier = new RestorePowerModifier();
 			modifier.setTicks(duration);
@@ -75,7 +77,6 @@ public class AbilityRestore extends Ability {
 			// Ignore warning; we know manager != null if they have the bending style
 			//noinspection ConstantConditions
 			data.getPowerRatingManager(getBendingId()).addModifier(modifier, ctx);
-
 
 		}
 
--- src/main/java/com/crowsofwar/avatar/common/bending/earth/RestorePowerModifier.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/earth/RestorePowerModifier.java
@@ -6,12 +6,21 @@ import com.crowsofwar.avatar.common.data.PowerRatingModifier;
 import com.crowsofwar.avatar.common.data.ctx.BendingContext;
 
 public class RestorePowerModifier extends PowerRatingModifier {
-    @Override
-    public double get(BendingContext ctx) {
-        BendingData data = ctx.getData();
-        AbilityData abilityData = data.getAbilityData(new AbilityRestore());
-        return 10+(3*abilityData.getLevel());
 
-    }
+	@Override
+	public double get(BendingContext ctx) {
+
+		BendingData data = ctx.getData();
+		AbilityData abilityData = data.getAbilityData("restore");
+
+		double modifier = abilityData.getLevel() >= 1 ? 25 : 15;
+		if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
+			modifier = 40;
+		}
+
+		return modifier;
+
+	}
+
 }
 
