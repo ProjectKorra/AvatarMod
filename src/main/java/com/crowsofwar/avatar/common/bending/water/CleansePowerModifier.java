34467203cdd5e5553dcda1d2890e25a6ea252c6d
CrowsOfWar <ofwarcrows@gmail.com>
2017-11-29 20:54:26 -0600

    Revamp Cleanse power modifier

--- src/main/java/com/crowsofwar/avatar/common/bending/water/CleansePowerModifier.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/water/CleansePowerModifier.java
@@ -6,12 +6,24 @@ import com.crowsofwar.avatar.common.data.PowerRatingModifier;
 import com.crowsofwar.avatar.common.data.ctx.BendingContext;
 
 public class CleansePowerModifier extends PowerRatingModifier {
-    @Override
-    public double get(BendingContext ctx) {
-        BendingData data = ctx.getData();
-        AbilityData abilityData = data.getAbilityData(new AbilityCleanse());
-        return 10+(3*abilityData.getLevel());
 
-    }
+	@Override
+	public double get(BendingContext ctx) {
+
+		BendingData data = ctx.getData();
+		AbilityData abilityData = data.getAbilityData("cleanse");
+
+		double modifier = 15;
+		if (abilityData.getLevel() >= 2) {
+            modifier = 25;
+        }
+        if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
+		    modifier = 40;
+        }
+
+		return modifier;
+
+	}
+
 }
 
