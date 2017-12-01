0c0084d22c867059c55b5fef402806b3cd7763cd
CrowsOfWar <ofwarcrows@gmail.com>
2017-12-01 17:18:29 -0600

    Accept map of params instead of list in post method

--- src/main/java/com/crowsofwar/avatar/common/AvatarAnalytics.java
+++ src/main/java/com/crowsofwar/avatar/common/AvatarAnalytics.java
@@ -12,10 +12,14 @@ import org.apache.http.message.BasicNameValuePair;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class AvatarAnalytics {
 
-	private static void post(String url) {
+	private static void post(String url, Map<String, String> params) {
+
+		// https://stackoverflow.com/questions/3324717/sending-http-post-request-in-java
 
 		try {
 
@@ -23,12 +27,16 @@ public class AvatarAnalytics {
 			HttpPost httppost = new HttpPost("http://www.a-domain.com/foo/");
 
 			// Request parameters and other properties.
-			List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-			params.add(new BasicNameValuePair("param-1", "12345"));
-			params.add(new BasicNameValuePair("param-2", "Hello!"));
-			httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
 
-//Execute and get the response.
+			Set<Map.Entry<String, String>> entries = params.entrySet();
+			for (Map.Entry<String, String> entry : entries) {
+				nameValuePairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+			}
+
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+
+			//Execute and get the response.
 			HttpResponse response = httpclient.execute(httppost);
 			HttpEntity entity = response.getEntity();
 
