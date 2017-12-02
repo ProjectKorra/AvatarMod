0d2da4013ccadb8bcbac87430162bfaf348ac6dc
CrowsOfWar <ofwarcrows@gmail.com>
2017-12-01 20:59:05 -0600

    Hopefully fix analytics user stats

--- src/main/java/com/crowsofwar/avatar/common/analytics/AvatarAnalytics.java
+++ src/main/java/com/crowsofwar/avatar/common/analytics/AvatarAnalytics.java
@@ -3,7 +3,6 @@ package com.crowsofwar.avatar.common.analytics;
 import com.crowsofwar.avatar.AvatarMod;
 import com.crowsofwar.avatar.server.AvatarServerProxy;
 import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.Loader;
 
 /**
  * @author CrowsOfWar
@@ -23,12 +22,14 @@ public class AvatarAnalytics {
 
 		boolean isServer = AvatarMod.proxy instanceof AvatarServerProxy;
 		String language = isServer ? "server" : FMLCommonHandler.instance().getCurrentLanguage();
-		int mods = Loader.instance().getModList().size() - 3;
 
 		String params = AnalyticsUtils.getBasicParameters();
-		params += "\nserver=" + isServer;
-		params += "\nlanguage=" + language;
-		params += "\nmodCount=" + mods;
+		params += "&t=event";
+		params += "&ec=userstats";
+		params += "&ea=userstats";
+		params += "&cdIsServer=" + isServer;
+		params += "&cdLanguage=" + language;
+		params += "&cdIsOptifine=" + AvatarMod.proxy.isOptifinePresent();
 
 		AnalyticsUtils.makeSingleApiRequest(params);
 
