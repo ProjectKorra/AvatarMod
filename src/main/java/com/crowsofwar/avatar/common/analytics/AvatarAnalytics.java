fcabbadcde975f7c89deb9fd73911b30663706ac
CrowsOfWar <ofwarcrows@gmail.com>
2017-12-01 20:15:20 -0600

    Basic analytics collection at beginning of session

--- src/main/java/com/crowsofwar/avatar/common/analytics/AvatarAnalytics.java
+++ src/main/java/com/crowsofwar/avatar/common/analytics/AvatarAnalytics.java
@@ -1,5 +1,10 @@
 package com.crowsofwar.avatar.common.analytics;
 
+import com.crowsofwar.avatar.AvatarMod;
+import com.crowsofwar.avatar.server.AvatarServerProxy;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Loader;
+
 /**
  * @author CrowsOfWar
  */
@@ -11,6 +16,22 @@ public class AvatarAnalytics {
 
 	public AvatarAnalytics() {
 		events = new EventAnalytics();
+	}
+
+	public void init() {
+		// Send initial analytics to server
+
+		boolean isServer = AvatarMod.proxy instanceof AvatarServerProxy;
+		String language = isServer ? "server" : FMLCommonHandler.instance().getCurrentLanguage();
+		int mods = Loader.instance().getModList().size() - 3;
+
+		String params = AnalyticsUtils.getBasicParameters();
+		params += "\nserver=" + isServer;
+		params += "\nlanguage=" + language;
+		params += "\nmodCount=" + mods;
+
+		AnalyticsUtils.makeSingleApiRequest(params);
+
 	}
 
 	public void pushEvent(AnalyticEvent event) {
