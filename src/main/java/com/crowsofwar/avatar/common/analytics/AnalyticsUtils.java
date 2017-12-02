693a7d86ba0ff1a8976f24bd91be2f152a0486b7
CrowsOfWar <ofwarcrows@gmail.com>
2017-12-01 21:38:39 -0600

    Generate new user ID every runtime to circumvent rate limiting
    ... there was an accident where 3k events got sent to the server

--- src/main/java/com/crowsofwar/avatar/common/analytics/AnalyticsUtils.java
+++ src/main/java/com/crowsofwar/avatar/common/analytics/AnalyticsUtils.java
@@ -6,13 +6,15 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 
+import java.util.UUID;
+
 /**
  * @author CrowsOfWar
  */
 public class AnalyticsUtils {
 
 	public static final String GA_TRACKING_ID = "UA-110529537-1";
-	public static final String GA_CLIENT_ID = "1";
+	public static final String GA_CLIENT_ID = UUID.randomUUID().toString();
 
 	/**
 	 * Gets the basic parameters to be sent in any GA API request. Then other parameters can be
