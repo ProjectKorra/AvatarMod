0da62979c72c72aeb7ac793c3e2ee15b6b555e5d
CrowsOfWar <ofwarcrows@gmail.com>
2017-12-01 20:03:10 -0600

    Move generically useful methods to AnalyticsUtils

---
+++ src/main/java/com/crowsofwar/avatar/common/analytics/AnalyticsUtils.java
@@ -0,0 +1,61 @@
+package com.crowsofwar.avatar.common.analytics;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+
+/**
+ * @author CrowsOfWar
+ */
+public class AnalyticsUtils {
+
+	public static final String GA_TRACKING_ID = "UA-110529537-1";
+	public static final String GA_CLIENT_ID = "1";
+
+	/**
+	 * Gets the basic parameters to be sent in any GA API request. Then other parameters can be
+	 * added like type, event category, etc. depending on the purpose.
+	 */
+	public static String getBasicParameters() {
+		String params = "v=1";
+		params += "&tid=" + GA_TRACKING_ID;
+		params += "&cid=" + GA_CLIENT_ID;
+		return params;
+	}
+
+	/**
+	 * Makes a request to the GA API
+	 */
+	public static void makeSingleApiRequest(String params) {
+		post("https://www.google-analytics.com/collect", params);
+	}
+
+	/**
+	 * Makes a request to the GA API
+	 */
+	public static void makeBatchApiRequest(String params) {
+		post("https://www.google-analytics.com/batch", params);
+	}
+
+	public static void post(String url, String payload) {
+
+		// https://stackoverflow.com/questions/3324717/sending-http-post-request-in-java
+
+		try {
+
+			HttpClient client = HttpClients.createDefault();
+			HttpPost post = new HttpPost(url);
+
+			post.setEntity(new StringEntity(payload, ContentType.create("text/plain", "UTF-8")));
+
+			client.execute(post);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
--- src/main/java/com/crowsofwar/avatar/common/analytics/EventAnalytics.java
+++ src/main/java/com/crowsofwar/avatar/common/analytics/EventAnalytics.java
@@ -1,18 +1,9 @@
 package com.crowsofwar.avatar.common.analytics;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClients;
-
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class EventAnalytics {
-
-	public static final String GA_TRACKING_ID = "UA-110529537-1";
-	public static final String GA_CLIENT_ID = "1";
 
 	/**
 	 * Maximum amount of events that can be sent in bulk
@@ -49,9 +40,7 @@ public class EventAnalytics {
 	 * Gets the appropriate parameters to use when sending the event to google
 	 */
 	private String getEventParameters(AnalyticEvent event) {
-		String params = "v=1";
-		params += "&tid=" + GA_TRACKING_ID;
-		params += "&cid=" + GA_CLIENT_ID;
+		String params = AnalyticsUtils.getBasicParameters();
 		params += "&t=event";
 		params += "&ec=" + event.getCategory();
 		params += "&ea=" + event.getAction();
@@ -87,32 +76,16 @@ public class EventAnalytics {
 			}
 
 			// In case there's only 1 being sent, just use regular endpoint
-			String url = sentHere == 1 ? "https://www.google-analytics.com/collect"
-					: "https://www.google-analytics.com/batch";
+			// For multiple, use batch endpoint
 
-			post(url, params);
+			if (sentHere == 1) {
+				AnalyticsUtils.makeSingleApiRequest(params);
+			} else {
+				AnalyticsUtils.makeBatchApiRequest(params);
+			}
 
 		}
 
-
-	}
-
-	private void post(String url, String payload) {
-
-		// https://stackoverflow.com/questions/3324717/sending-http-post-request-in-java
-
-		try {
-
-			HttpClient client = HttpClients.createDefault();
-			HttpPost post = new HttpPost(url);
-
-			post.setEntity(new StringEntity(payload, ContentType.create("text/plain", "UTF-8")));
-
-			client.execute(post);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 
 	}
 
