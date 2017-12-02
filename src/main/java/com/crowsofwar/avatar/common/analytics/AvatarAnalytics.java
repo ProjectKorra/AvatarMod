22368b8f91e6d3019e40a31594a944ee115e3114
CrowsOfWar <ofwarcrows@gmail.com>
2017-12-02 13:35:24 -0600

    Properly send custom dimensions/metrics

--- src/main/java/com/crowsofwar/avatar/common/analytics/AvatarAnalytics.java
+++ src/main/java/com/crowsofwar/avatar/common/analytics/AvatarAnalytics.java
@@ -3,6 +3,7 @@ package com.crowsofwar.avatar.common.analytics;
 import com.crowsofwar.avatar.AvatarMod;
 import com.crowsofwar.avatar.server.AvatarServerProxy;
 import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Loader;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -30,14 +31,16 @@ public class AvatarAnalytics {
 
 		boolean isServer = AvatarMod.proxy instanceof AvatarServerProxy;
 		String language = isServer ? "server" : FMLCommonHandler.instance().getCurrentLanguage();
+		int mods = Loader.instance().getModList().size() - 5;
 
 		String params = AnalyticsUtils.getBasicParameters();
 		params += "&t=event";
 		params += "&ec=userstats";
 		params += "&ea=userstats";
-		params += "&cdIsServer=" + isServer;
-		params += "&cdLanguage=" + language;
-		params += "&cdIsOptifine=" + AvatarMod.proxy.isOptifinePresent();
+		params += "&cd1=" + isServer;
+		params += "&cd3=" + language;
+		params += "&cd2=" + AvatarMod.proxy.isOptifinePresent();
+		params += "&cm1=" + mods;
 		params += "&sc=start";
 
 		AnalyticsUtils.makeSingleApiRequest(params);
