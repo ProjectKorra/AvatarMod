6f037ad849f5cb0cf26b26be8b9bde3c069593ef
CrowsOfWar <ofwarcrows@gmail.com>
2017-12-02 20:45:42 -0600

    Setup analytics.yml

--- src/main/java/com/crowsofwar/avatar/common/config/ConfigAnalytics.java
+++ src/main/java/com/crowsofwar/avatar/common/config/ConfigAnalytics.java
@@ -1,6 +1,10 @@
 package com.crowsofwar.avatar.common.config;
 
+import com.crowsofwar.avatar.AvatarLog;
 import com.crowsofwar.gorecore.config.ConfigLoader;
+import com.crowsofwar.gorecore.config.Load;
+
+import java.util.UUID;
 
 public class ConfigAnalytics {
 
@@ -8,8 +12,34 @@ public class ConfigAnalytics {
 
 	private ConfigAnalytics() {}
 
+	@Load
+	private String analyticsIdStr = UUID.randomUUID().toString();
+	public UUID analyicsId;
+
+	@Load
+	public boolean analyticsEnabled = true;
+
 	public static void load() {
-		ConfigLoader.load(ANALYTICS_CONFIG, "avatar/analytics.yml");
+		ConfigLoader.load(ANALYTICS_CONFIG, "avatar/analytics.yml", true);
+		ANALYTICS_CONFIG.setupAnalyticsId();
+	}
+
+	private void setupAnalyticsId() {
+
+		try {
+
+			analyicsId = UUID.fromString(analyticsIdStr);
+			
+		} catch (IllegalArgumentException e) {
+
+			analyicsId = UUID.randomUUID();
+			analyticsEnabled = false;
+			AvatarLog.warn(AvatarLog.WarningType.CONFIGURATION, "Invalid analytics id string, " +
+					"analytics temporarily disabled. Suggested to edit analytics.yml and set " +
+					"analyticsIdStr to '" + UUID.randomUUID().toString() + "'");
+
+		}
+
 	}
 
 }
