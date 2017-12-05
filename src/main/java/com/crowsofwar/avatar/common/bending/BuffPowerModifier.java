926a4f257ef810fbfc9815187679bfb3be1759d9
CrowsOfWar <ofwarcrows@gmail.com>
2017-12-04 21:02:22 -0600

    Start adding common superclass for buff PR mods

---
+++ src/main/java/com/crowsofwar/avatar/common/bending/BuffPowerModifier.java
@@ -0,0 +1,59 @@
+package com.crowsofwar.avatar.common.bending;
+
+import com.crowsofwar.avatar.common.data.AbilityData;
+import com.crowsofwar.avatar.common.data.PowerRatingModifier;
+import com.crowsofwar.avatar.common.data.Vision;
+import com.crowsofwar.avatar.common.data.ctx.BendingContext;
+
+public abstract class BuffPowerModifier extends PowerRatingModifier {
+
+	/**
+	 * As the player levels up, buff abilities typically apply more "powerful" looking vision
+	 * shaders, in which the visual effects/distortions are the same, but of higher intensity.
+	 * Returns the different Vision shaders to be applied when the player is at different ability
+	 * levels.
+	 * <p>
+	 * This should return a 3-element array, with the first element being the weak vision shader
+	 * to be used at levels I and II, the second element being the medium vision shader to be
+	 * used at level III, and finally the third element being the powerful vision shader to be
+	 * used at level IV.
+	 */
+	protected abstract Vision[] getVisions();
+
+	protected abstract String getAbilityName();
+
+	private Vision getVision(BendingContext ctx) {
+
+		AbilityData abilityData = ctx.getData().getAbilityData(getAbilityName());
+		switch (abilityData.getLevel()) {
+			case 0:
+			case 1:
+				return getVisions()[0];
+			case 2:
+				return getVisions()[1];
+			case 3:
+			default:
+				return getVisions()[2];
+		}
+
+	}
+
+	@Override
+	public void onAdded(BendingContext ctx) {
+		if (ctx.getData().getVision() == null) {
+			ctx.getData().setVision(getVision(ctx));
+		}
+		super.onAdded(ctx);
+	}
+
+	@Override
+	public void onRemoval(BendingContext ctx) {
+		Vision vision = ctx.getData().getVision();
+		if (vision != null && vision.name().startsWith("RESTORE")) {
+			ctx.getData().setVision(null);
+		}
+		super.onRemoval(ctx);
+	}
+
+}
+
