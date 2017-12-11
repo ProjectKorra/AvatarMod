2ebe441572dec90f7ebea7c309dae8c3f6dfa8d5
CrowsOfWar <ofwarcrows@gmail.com>
2017-12-10 20:05:56 -0600

    Change score by calculated amount every tick

--- src/main/java/com/crowsofwar/avatar/common/bending/BattlePerformance.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/BattlePerformance.java
@@ -1,5 +1,7 @@
 package com.crowsofwar.avatar.common.bending;
 
+import net.minecraft.util.math.MathHelper;
+
 /**
  * Represents a numeric score of how well a bender is doing in a battle. This score is increased by
  * actions like damaging the enemy, and decreased by taking damage. It can be used for modifiers
@@ -13,12 +15,51 @@ package com.crowsofwar.avatar.common.bending;
  */
 public class BattlePerformance {
 
+	private double score;
+
+	/**
+	 * Updates the battle performance number, to be called every tick
+	 */
+	public void update() {
+		double changePerSecond = getScoreChangePerSecond(score);
+		modifyScore(changePerSecond / 20);
+	}
+
+	/**
+	 * Add or subtract a certain amount of score from the bender.
+	 */
+	public void modifyScore(double amount) {
+		score = MathHelper.clamp(score + amount, -100, 100);
+	}
+
 	/**
 	 * Returns the numeric rating of how well the bender is doing in combat right now, ranging from
 	 * -100 to 100 (0 is neutral).
 	 */
 	public double getScore() {
 		return 0;
+	}
+
+	/**
+	 * Attaining a high battle performance score is temporary; it passively moves towards zero every
+	 * second. Gets the amount of score to change per second, given the current score. Values of
+	 * higher magnitude (i.e. closer to 100 or -100) move towards zero faster, while scores already
+	 * close to zero move towards zero more slowly.
+	 * <p>
+	 */
+	private static final double getScoreChangePerSecond(double currentScore) {
+
+		// Minimum / maximum score change per second, where min. change occurs when score is near 0,
+		// and max. change occurs when score is at 100
+		final double min = 2;
+		final double max = 4;
+
+		// Generates a curve from minimum to maximum
+
+		// not sure how to explain this but got it using math. K then...
+		double k = 10000 / (max - min);
+		return -2 * Math.signum(currentScore) * (currentScore * currentScore + k * min) / min;
+
 	}
 
 }
