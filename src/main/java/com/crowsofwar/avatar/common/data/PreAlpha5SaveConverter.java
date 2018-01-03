c8562050120e40d306b4f25fd94f9ebe6d360e68
CrowsOfWar <ofwarcrows@gmail.com>
2018-01-02 18:11:56 -0600

    Fix ability data NBT

--- src/main/java/com/crowsofwar/avatar/common/data/PreAlpha5SaveConverter.java
+++ src/main/java/com/crowsofwar/avatar/common/data/PreAlpha5SaveConverter.java
@@ -1,6 +1,7 @@
 package com.crowsofwar.avatar.common.data;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 
 /**
  * Provides support for reading pre-a5.0 BendingData from NBT.
@@ -28,8 +29,34 @@ public class PreAlpha5SaveConverter {
 
 		NBTTagCompound converted = preA5.copy();
 
+		fixAbilityData(converted);
 
 		return converted;
+
+
+	}
+
+	private static void fixAbilityData(NBTTagCompound nbt) {
+
+		NBTTagList listTag = nbt.getTagList("AbilityData", 10);
+		for (int i = 0; i < listTag.tagCount(); i++) {
+			NBTTagCompound item = listTag.getCompoundTagAt(i);
+
+			// Fix ability data key
+			// Simple fix, need to move String value with key "_AbilityName" to new key "Name"
+			NBTTagCompound keyTag = item.getCompoundTag("Key");
+			String abilityName = keyTag.getString("_AbilityName");
+			keyTag.setString("Name", abilityName);
+			keyTag.removeTag("_AbilityName");
+			keyTag.removeTag("Id");
+
+			// Fix ability data value
+			// Previously used "AbilityId" tag, now just needs "Name" tag
+			NBTTagCompound valueTag = item.getCompoundTag("Value");
+			valueTag.removeTag("AbilityId");
+			valueTag.setString("Name", abilityName);
+
+		}
 
 
 	}
