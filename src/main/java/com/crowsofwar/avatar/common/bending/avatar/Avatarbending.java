da6f3654c079ff3615cd1c13819456637fb0304f
FavouriteDragon <ninjadragon008@icloud.com>
2018-01-05 07:09:00 -0800

    started doing Avatarbending

--- src/main/java/com/crowsofwar/avatar/common/bending/avatar/Avatarbending.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/avatar/Avatarbending.java
@@ -1,4 +1,24 @@
 package com.crowsofwar.avatar.common.bending.avatar;
 
-public class Avatarbending {
+import com.crowsofwar.avatar.common.bending.BendingStyle;
+import com.crowsofwar.avatar.common.gui.BendingMenuInfo;
+
+import java.util.UUID;
+
+public class Avatarbending extends BendingStyle {
+	public static UUID ID = UUID.fromString("53076e40-a677-4c51-84e3-0b4814a829ca");
+	@Override
+	public BendingMenuInfo getRadialMenu() {
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public UUID getId() {
+		return ID;
+	}
 }
