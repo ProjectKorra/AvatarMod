291b65e7eca7f83d00f20c94e3ec04d6aae8ec0c
FavouriteDragon <ninjadragon008@icloud.com>
2018-01-05 18:40:21 -0800

    started doing blockbase

---
+++ src/main/java/com/crowsofwar/avatar/common/blocks/BlockBase.java
@@ -0,0 +1,37 @@
+package com.crowsofwar.avatar.common.blocks;
+
+import com.crowsofwar.avatar.AvatarMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+public class BlockBase extends Block {
+
+	protected String name;
+
+	public BlockBase(Material material, String name) {
+		super(material);
+
+		this.name = name;
+
+		setUnlocalizedName(name);
+		setRegistryName(name);
+	}
+
+	public void registerItemModel(Item itemBlock) {
+		//AvatarMod.proxy.registerItemRenderer(itemBlock, 0, name);
+	}
+
+	public Item createItemBlock() {
+		return new ItemBlock(this).setRegistryName(getRegistryName());
+	}
+
+	@Override
+	public BlockBase setCreativeTab(CreativeTabs tab) {
+		super.setCreativeTab(tab);
+		return this;
+	}
+
+}
