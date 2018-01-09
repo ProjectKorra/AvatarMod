e610acd801dd7f17c7d117ea84aac6d86c606435
CrowsOfWar <ofwarcrows@gmail.com>
2018-01-08 19:57:33 -0600

    Add announcements object to store timestamp

--- src/main/java/com/crowsofwar/avatar/common/AvatarAnnouncements.java
+++ src/main/java/com/crowsofwar/avatar/common/AvatarAnnouncements.java
@@ -10,8 +10,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Manages public announcements for Avatar Mod 2, which are short messages which can be sent by the
@@ -25,6 +24,17 @@ public class AvatarAnnouncements {
 	public static final String ANNOUNCEMENT_URL = "https://pastebin.com/raw/Mm8grAfe";
 
 	private static List<String> announcements;
+
+	public static void main(String[] args) {
+		Announcement announcement = new Announcement("2018-01-08 Here is the first announcement");
+		System.out.println(announcement);
+
+		Date start = announcement.timestamp;
+		Date end = new Date();
+		long diffInSeconds = (end.getTime() - start.getTime()) / 1000;
+		long diffInHours = diffInSeconds / 60 / 60;
+
+	}
 
 	@SubscribeEvent
 	public static void onLogin(PlayerEvent.PlayerLoggedInEvent e) {
@@ -62,6 +72,62 @@ public class AvatarAnnouncements {
 		}
 
 		AvatarLog.info("Finished loading announcements");
+
+	}
+
+	private static class Announcement {
+
+		private final String contents;
+		private final Date timestamp;
+
+		/**
+		 * Constructs an announcement from the code string. The code should be in the following
+		 * format:
+		 * <p>
+		 * YYYY-MM-DD
+		 */
+		public Announcement(String code) {
+
+			if (!code.matches("\\d{4}-\\d{2}-\\d{2} .+")) {
+				throw new IllegalArgumentException("Announcement code in incorrect format: " + code);
+			}
+
+			String[] timeStrings = code.split(" ")[0].split("-");
+			int year = Integer.parseInt(timeStrings[0]);
+			int month = Integer.parseInt(timeStrings[1]) - 1;
+			int day = Integer.parseInt(timeStrings[2]);
+
+			Calendar cal = new GregorianCalendar(year, month, day);
+			cal.setTimeZone(getSystemTimeZone());
+			timestamp = cal.getTime();
+
+			contents = code.substring(code.indexOf(" ") + 1);
+
+		}
+
+		public Announcement(String contents, Date timestamp) {
+			this.contents = contents;
+			this.timestamp = timestamp;
+		}
+
+		@Override
+		public String toString() {
+			return "[" + timestamp + "] " + contents;
+		}
+	}
+
+	/**
+	 * Gets the timezone which the user has configured the system to use. If unavailable, just uses
+	 * UTC time.
+	 */
+	private static TimeZone getSystemTimeZone() {
+
+		String timezone = System.getProperty("user.timezone");
+		if (timezone != null) {
+			return TimeZone.getTimeZone(timezone);
+		} else {
+			return TimeZone.getTimeZone("UTC");
+		}
 
 	}
 
