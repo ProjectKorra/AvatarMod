8bf16dac35a8ec3fb061e3c59df0bfa6744f6b0c
FavouriteDragon <ninjadragon008@icloud.com>
2018-01-08 20:02:33 -0800

    implemented everything into the ability of elementshard

--- src/main/java/com/crowsofwar/avatar/common/bending/avatar/AbilityElementshard.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/avatar/AbilityElementshard.java
@@ -1,44 +1,87 @@
 package com.crowsofwar.avatar.common.bending.avatar;
 
 import com.crowsofwar.avatar.common.bending.Ability;
+import com.crowsofwar.avatar.common.bending.BendingAi;
+import com.crowsofwar.avatar.common.bending.StatusControl;
+import com.crowsofwar.avatar.common.bending.fire.AiFireball;
+import com.crowsofwar.avatar.common.bending.fire.Firebending;
+import com.crowsofwar.avatar.common.data.AbilityData;
 import com.crowsofwar.avatar.common.data.Bender;
 import com.crowsofwar.avatar.common.data.BendingData;
 import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
 import com.crowsofwar.avatar.common.entity.EntityElementshard;
+import com.crowsofwar.avatar.common.entity.EntityFireball;
+import com.crowsofwar.avatar.common.entity.data.ElementshardBehavior;
+import com.crowsofwar.avatar.common.entity.data.FireballBehavior;
+import com.crowsofwar.gorecore.util.Vector;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.world.World;
 
 import java.util.UUID;
 
+import static com.crowsofwar.avatar.common.config.ConfigStats.STATS_CONFIG;
+import static com.crowsofwar.gorecore.util.Vector.getEyePos;
+import static com.crowsofwar.gorecore.util.Vector.getLookRectangular;
+
 public class AbilityElementshard extends Ability {
 
 	public AbilityElementshard(UUID bendingType, String name) {
 		super(Avatarbending.ID, "element_shard");
+		requireRaytrace(2.5, false);
+	}
+	@Override
+	public void execute(AbilityContext ctx) {
+
+		EntityLivingBase entity = ctx.getBenderEntity();
+		Bender bender = ctx.getBender();
+		World world = ctx.getWorld();
+		BendingData data = ctx.getData();
+
+		float chi = 4F;
+		float damage = 0.5F;
+		int shardsLeft = 5;
+
+		if (ctx.getLevel() == 1){
+			damage = 1F;
+			shardsLeft = 8;
+		}
+		if (ctx.getLevel() == 2){
+			shardsLeft = 10;
+			//create an explosion; code is in fireball but 2 lazy to do it right now
+		}
+		if (data.hasStatusControl(StatusControl.THROW_ELEMENTSHARD)) return;
+
+		if (bender.consumeChi(STATS_CONFIG.chiFireball)) {
+
+			Vector target;
+			if (ctx.isLookingAtBlock()) {
+				target = ctx.getLookPos();
+			} else {
+				Vector playerPos = getEyePos(entity);
+				target = playerPos.plus(getLookRectangular(entity).times(2.5));
+			}
+
+			damage *= ctx.getPowerRatingDamageMod();
+
+			EntityElementshard elementshard = new EntityElementshard(world);
+			elementshard.setPosition(target);
+			elementshard.setOwner(entity);
+			elementshard.setBehavior(new ElementshardBehavior.PlayerControlled());
+			elementshard.setDamage(damage);
+			elementshard.setPowerRating(bender.calcPowerRating(Firebending.ID));
+			if (ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND)) elementshard.setSize(20);
+			world.spawnEntity(elementshard);
+
+			data.addStatusControl(StatusControl.THROW_FIREBALL);
+
+		}
+
 	}
 
 	@Override
-	public void execute(AbilityContext ctx) {
-		EntityLivingBase entity = ctx.getBenderEntity();
-		BendingData data = ctx.getData();
-		Bender bender = ctx.getBender();
-		World world = ctx.getWorld();
-
-		float chi = 4F;
-		int shardsleft = 5;
-		if (ctx.getLevel() == 1 ){
-			shardsleft = 8;
-		}
-		if (ctx.getLevel() == 2){
-			shardsleft = 10;
-		}
-		if (bender.consumeChi(chi)){
-			EntityElementshard elementshard = new EntityElementshard(world);
-			for (int i = 0; i< shardsleft;){
-				elementshard.posX = entity.posX;
-				elementshard.posY = entity.posY;
-
-			}
-		}
-
+	public BendingAi getAi(EntityLiving entity, Bender bender) {
+		return new AiElementshard(this, entity, bender);
 	}
+
 }
