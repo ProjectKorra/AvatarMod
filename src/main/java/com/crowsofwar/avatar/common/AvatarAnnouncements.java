068f19a8d0fbe52a857420e3407646ad60634993
CrowsOfWar <ofwarcrows@gmail.com>
2018-01-08 19:19:59 -0600

    Send player announcement chat message

--- src/main/java/com/crowsofwar/avatar/common/AvatarAnnouncements.java
+++ src/main/java/com/crowsofwar/avatar/common/AvatarAnnouncements.java
@@ -1,6 +1,11 @@
 package com.crowsofwar.avatar.common;
 
+import com.crowsofwar.avatar.AvatarInfo;
 import com.crowsofwar.avatar.AvatarLog;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -15,19 +20,30 @@ import java.util.List;
  *
  * @author CrowsOfWar
  */
+@Mod.EventBusSubscriber(modid = AvatarInfo.MOD_ID)
 public class AvatarAnnouncements {
 
 	public static final String ANNOUNCEMENT_URL = "https://pastebin.com/raw/Mm8grAfe";
 
 	private static List<String> announcements;
 
+	@SubscribeEvent
+	public static void onLogin(PlayerEvent.PlayerLoggedInEvent e) {
+
+		if (announcements == null) {
+			fetchAnnouncements();
+		}
+
+		e.player.sendMessage(new TextComponentString(announcements.get(0)));
+
+	}
+
 	public static void fetchAnnouncements() {
 
 		AvatarLog.info("Fetching latest AvatarMod2 announcements...");
+		announcements = new ArrayList<>();
 
 		try {
-
-			announcements = new ArrayList<>();
 
 			URL u = new URL(ANNOUNCEMENT_URL);
 			URLConnection conn = u.openConnection();
