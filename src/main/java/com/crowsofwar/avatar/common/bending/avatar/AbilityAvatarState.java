b748e065fced9bdf6cce4156c63f3fa13486779c
FavouriteDragon <ninjadragon008@icloud.com>
2018-02-14 16:20:35 -0800

    Continued work on avatar state
    added a power modifier and started doing status effects

--- src/main/java/com/crowsofwar/avatar/common/bending/avatar/AbilityAvatarState.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/avatar/AbilityAvatarState.java
@@ -1,4 +1,47 @@
 package com.crowsofwar.avatar.common.bending.avatar;
 
-public class AbilityAvatarState {
+import com.crowsofwar.avatar.common.bending.Ability;
+import com.crowsofwar.avatar.common.data.AbilityData;
+import com.crowsofwar.avatar.common.data.Bender;
+import com.crowsofwar.avatar.common.data.BendingData;
+import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
+
+import static com.crowsofwar.avatar.common.config.ConfigSkills.SKILLS_CONFIG;
+import static com.crowsofwar.avatar.common.config.ConfigStats.STATS_CONFIG;
+
+public class AbilityAvatarState extends Ability {
+
+	public AbilityAvatarState() {
+		super(Avatarbending.ID, "avatr_state");
+	}
+
+	@Override
+	public boolean isBuff() {
+		return true;
+	}
+
+	@Override
+	public void execute(AbilityContext ctx) {
+		BendingData data = ctx.getData();
+		AbilityData abilityData = data.getAbilityData(this);
+		EntityLivingBase entity = ctx.getBenderEntity();
+		Bender bender = ctx.getBender();
+		float chi = STATS_CONFIG.chiBuff;
+
+		if (bender.consumeChi(chi)) {
+			float xp = SKILLS_CONFIG.buffUsed;
+
+			// 5s base + 1s per level
+			int duration = 100 + 20 * abilityData.getLevel();
+
+			int effectLevel = abilityData.getLevel() >= 2 ? 1 : 0;
+
+			entity.addPotionEffect(new PotionEffect(MobEffects.SPEED, duration, effectLevel));
+			entity.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, duration, effectLevel));
+
+		}
+	}
 }
