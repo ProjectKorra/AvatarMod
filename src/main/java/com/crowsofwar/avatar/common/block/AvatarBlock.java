a3588ae2bd08aca914fa015fc56361b60ee83bac
Mahtaran <luka.leer@gmail.com>
2018-03-20 18:56:07 +0100

    That didn't happen

--- src/main/java/com/crowsofwar/avatar/common/block/AvatarBlock.java
+++ src/main/java/com/crowsofwar/avatar/common/block/AvatarBlock.java
@@ -7,7 +7,7 @@ import net.minecraft.block.material.Material;
  * @author Mahtaran
  */
 public class AvatarBlock extends Block {
-	public CloudBlock(Material material, String name) {
+	public AvatarBlock(Material material, String name) {
 		super(material);
 		this.setUnlocalizedName(name);
         this.setRegistryName(name);
