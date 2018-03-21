6676204da6e363d390a949141e0949bd4d17b6b8
Mahtaran <luka.leer@gmail.com>
2018-03-21 21:55:44 +0100

    This class needs some logic (Switch to Semver)
    going to continue tomorrow

--- src/main/java/com/crowsofwar/avatar/AvatarInfo.java
+++ src/main/java/com/crowsofwar/avatar/AvatarInfo.java
@@ -18,23 +18,29 @@
 package com.crowsofwar.avatar;
 
 /**
- * Not using semantic versioning. This isn't an API, and I'm too lazy to be
+ * Using semantic versioning. This isn't an API, but I'm not too lazy to be
  * backwards-compatible.
  * <p>
- * Versioning scheme: AV{DEV_STAGE}{UPDATE}.{PATCH}{DEV_BUILD}
+ * Versioning scheme: {RELEASE}.{UPDATE}.{PATCH}{DEV_STAGE}
+ * </p>
+ *
  * <p>
- * DEV_STAGE is for alpha("a"), beta("b"), or full release("").
+ * DEV_STAGE is alpha ("-alpha"), beta ("-beta"), or full release ("").
+ * Append "-dev" if a dev build
+ * </p>
+ *
  * <p>
- * If development version, DEV_BUILD is "_dev"
- * <p>
- * E.g. AV_B3.2-dev -> Beta, Update 3, patch 2, development build
+ * E.g. 1.3.2-beta-dev -> Beta, Release 1, Update 3, patch 2, development build
+ * </p>
  *
  * @author CrowsOfWar
  */
 public class AvatarInfo {
-
-	// Things that are adjustable
-
+	public static final String VERSION = "@VERSION@";
+	/**
+	 * Incremented for complete rewrites
+	 */
+	public static final int VERSION_RELEASE;
 	/**
 	 * Incremented for every major update.
 	 */
@@ -43,6 +49,14 @@ public class AvatarInfo {
 	 * Incremented for minor bug fixes.
 	 */
 	public static final int VERSION_PATCH = 3;
+	
+	static {
+		String[] appends = VERSION.split("-");
+		String[] versions = appends[0].split("\\.");
+		VERSION_RELEASE = Integer.parseInt(versions[0]);
+		VERSION_RELEASE = Integer.parseInt(versions[1]);
+		VERSION_RELEASE = Integer.parseInt(versions[2]);
+	}
 	/**
 	 * "a" for alpha.
 	 * <p>
