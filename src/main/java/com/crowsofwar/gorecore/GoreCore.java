a3a5bd2aaafd0a837c9d7b10e93abb7f145c86dd
Mahtaran <luka.leer@gmail.com>
2018-03-21 21:20:07 +0100

    Oh frick I should remove this

--- src/main/java/com/crowsofwar/gorecore/GoreCore.java
+++ src/main/java/com/crowsofwar/gorecore/GoreCore.java
@@ -56,16 +56,16 @@ public class GoreCore {
 	public void preInit(FMLPreInitializationEvent event) {
 		config = new GoreCoreModConfig(event);
 		ConverterRegistry.addDefaultConverters();
-		
-		AccountUUIDs.readCache();
+		File oldFile = GoreCore.proxy.getUUIDCacheFile();
+		if (oldFile.exists()) {
+			// We don't need a cache anymore
+			oldFile.delete();
+		}
 		GoreCoreChatMessages.register();
 		
 		proxy.sideSpecifics();
 		
 		ChatSender.load();
-		
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> AccountUUIDs.saveCache()));
-		
 	}
 	
 	@EventHandler
