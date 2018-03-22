0eb48fae839726cad9efabc1f49d963ea26ec899
Mahtaran <luka.leer@gmail.com>
2018-03-22 15:26:29 +0100

    Oops

--- src/main/java/com/crowsofwar/avatar/common/block/AvatarBlock.java
+++ src/main/java/com/crowsofwar/avatar/common/block/AvatarBlock.java
@@ -3,13 +3,15 @@ package com.crowsofwar.avatar.common.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
+import com.crowsofwar.avatar.AvatarInfo;
+
 /**
  * @author Mahtaran
  */
 public class AvatarBlock extends Block {
 	public AvatarBlock(Material material, String name) {
 		super(material);
-		this.setUnlocalizedName(name);
-        this.setRegistryName(name);
+		this.setUnlocalizedName(AvatarInfo.MOD_ID + ":" + name);
+		this.setRegistryName(name);
 	}
 }
