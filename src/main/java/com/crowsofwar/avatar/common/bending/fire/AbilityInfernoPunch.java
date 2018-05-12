3b9d4a3a5beaaf7a8e318acc1d2d619f5c481fdb
FavouriteDragon <ninjadragon008@icloud.com>
2018-05-11 19:51:21 -0700

    fixed a crash; can do "multiple punches", but you technically only punch once

--- src/main/java/com/crowsofwar/avatar/common/bending/fire/AbilityInfernoPunch.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/fire/AbilityInfernoPunch.java
@@ -1,32 +1,12 @@
 package com.crowsofwar.avatar.common.bending.fire;
 
-import com.crowsofwar.avatar.AvatarInfo;
-import com.crowsofwar.avatar.common.AvatarDamageSource;
 import com.crowsofwar.avatar.common.bending.Ability;
-import com.crowsofwar.avatar.common.bending.StatusControl;
 import com.crowsofwar.avatar.common.data.AbilityData;
 import com.crowsofwar.avatar.common.data.Bender;
 import com.crowsofwar.avatar.common.data.BendingData;
-import com.crowsofwar.avatar.common.data.TickHandler;
 import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
-import com.crowsofwar.avatar.common.data.ctx.BendingContext;
-import com.crowsofwar.avatar.common.entity.mob.EntityBender;
-import com.crowsofwar.avatar.common.util.Raytrace;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.DamageSource;
-import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.lwjgl.Sys;
 
-import static com.crowsofwar.avatar.common.bending.StatusControl.FIRE_JUMP;
 import static com.crowsofwar.avatar.common.bending.StatusControl.INFERNO_PUNCH;
-import static com.crowsofwar.avatar.common.config.ConfigStats.STATS_CONFIG;
 
 
 public class AbilityInfernoPunch extends Ability {
@@ -34,16 +14,11 @@ public class AbilityInfernoPunch extends Ability {
 		super(Firebending.ID, "inferno_punch");
 	}
 
-	public int punchesLeft = 1;
+	private int punchesLeft;
 
-	public float damage = 5;
-
-	public float knockBack = 0.5F;
-
-	public int fireTime = 5;
-
-	public boolean removeStatusControl = false;
-
+	public int getPunchesLeft() {
+		return this.punchesLeft;
+	}
 
 	@Override
 	public void execute(AbilityContext ctx) {
@@ -52,20 +27,35 @@ public class AbilityInfernoPunch extends Ability {
 		Bender bender = ctx.getBender();
 		float chi = 3;
 
-		if (ctx.getLevel() >= 1){
+		if (ctx.getLevel() >= 1) {
 			chi = 4;
 		}
-		if (ctx.getLevel() >= 2){
+		if (ctx.getLevel() >= 2) {
 			chi = 5;
 		}
-		if (ctx.isMasterLevel(AbilityData.AbilityTreePath.FIRST)){
+		if (ctx.isMasterLevel(AbilityData.AbilityTreePath.FIRST)) {
 			chi = 8;
 		}
-		if (ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND)){
+		if (ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND)) {
 			chi = 3;
 		}
 		if (!data.hasStatusControl(INFERNO_PUNCH) && bender.consumeChi(chi)) {
 			data.addStatusControl(INFERNO_PUNCH);
+			 punchesLeft = 1;
+			AbilityData abilityData = ctx.getData().getAbilityData("inferno_punch");
+
+			if (abilityData.getLevel() >= 2) {
+				punchesLeft = 2;
+			}
+
+			if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
+				punchesLeft = 1;
+				//Creates a bunch of fire blocks around the target
+			}
+			if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
+				punchesLeft = 3;
+			}
+
 		}
 
 	}
--- src/main/java/com/crowsofwar/avatar/common/bending/fire/StatCtrlInfernoPunch.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/fire/StatCtrlInfernoPunch.java
@@ -1,15 +1,10 @@
 package com.crowsofwar.avatar.common.bending.fire;
 
 import com.crowsofwar.avatar.AvatarInfo;
-import com.crowsofwar.avatar.common.AvatarDamageSource;
 import com.crowsofwar.avatar.common.bending.StatusControl;
-import com.crowsofwar.avatar.common.controls.AvatarControl;
 import com.crowsofwar.avatar.common.data.AbilityData;
 import com.crowsofwar.avatar.common.data.Bender;
-import com.crowsofwar.avatar.common.data.BendingData;
-import com.crowsofwar.avatar.common.data.DataCategory;
 import com.crowsofwar.avatar.common.data.ctx.BendingContext;
-import com.crowsofwar.avatar.common.data.ctx.PlayerBender;
 import com.crowsofwar.avatar.common.entity.mob.EntityBender;
 import com.crowsofwar.avatar.common.util.AvatarUtils;
 import com.crowsofwar.gorecore.util.Vector;
@@ -17,12 +12,10 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
-import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.Sys;
 
 import static com.crowsofwar.avatar.common.controls.AvatarControl.CONTROL_LEFT_CLICK;
 
@@ -36,11 +29,7 @@ public class StatCtrlInfernoPunch extends StatusControl {
 
 	@Override
 	public boolean execute(BendingContext ctx) {
-		EntityLivingBase entity = ctx.getBenderEntity();
-		AbilityData abilityData = ctx.getData().getAbilityData("inferno_punch");
-
 		return false;
-
 
 	}
 
@@ -49,60 +38,60 @@ public class StatCtrlInfernoPunch extends StatusControl {
 	public static void onInfernoPunch(LivingAttackEvent event) {
 		EntityLivingBase entity = (EntityLivingBase) event.getSource().getTrueSource();
 		EntityLivingBase target = (EntityLivingBase) event.getEntity();
-		Bender ctx = Bender.get(entity);
-		if (event.getSource().getTrueSource() == entity && (entity instanceof EntityBender || entity instanceof EntityPlayer) && ctx.getData() != null) {
-			Vector direction = Vector.toRectangular(Math.toRadians(entity.rotationYaw), 0);
-			int punchesLeft = 1;
-			float knockBack = 1F;
-			int fireTime = 5;
-			float damage = 3;
-			AbilityData abilityData = ctx.getData().getAbilityData("inferno_punch");
 
-			if (abilityData.getLevel() >= 1) {
-				damage = 4;
-				knockBack = 1.25F;
-				fireTime = 6;
+		if (event.getSource().getTrueSource() == entity && (entity instanceof EntityBender || entity instanceof EntityPlayer)) {
+			Bender ctx = Bender.get(entity);
+			if (ctx.getData() != null) {
+				Vector direction = Vector.toRectangular(Math.toRadians(entity.rotationYaw), 0);
+				AbilityData abilityData = ctx.getData().getAbilityData("inferno_punch");
+				float knockBack = 1F;
+				int fireTime = 5;
+				float damage = 3;
+				int punchesLeft = 1;
 
 
-			}
-			if (abilityData.getLevel() >= 2) {
-				damage = 5;
-				knockBack = 1.5F;
-				fireTime = 8;
-				punchesLeft = 2;
-			}
+				if (abilityData.getLevel() >= 1) {
+					damage = 4;
+					knockBack = 1.25F;
+					fireTime = 6;
+				}
+				if (abilityData.getLevel() >= 2) {
+					damage = 5;
+					knockBack = 1.5F;
+					fireTime = 8;
+					punchesLeft = 2;
+				}
 
-			if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
-				damage = 10;
-				knockBack = 1.75F;
-				fireTime = 15;
-				punchesLeft = 1;
-				//Creates a bunch of fire blocks around the target
-			}
-			if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
-				damage = 2;
-				punchesLeft = 3;
-				knockBack = 0.75F;
-				fireTime = 4;
-			}
+				if (abilityData.isMasterPath(AbilityData.AbilityTreePath.FIRST)) {
+					damage = 10;
+					knockBack = 1.75F;
+					fireTime = 15;
+					//Creates a bunch of fire blocks around the target
+				}
+				if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
+					damage = 2;
+					knockBack = 0.25F;
+					fireTime = 4;
+					punchesLeft = 3;
+				}
+				if (ctx.getData().hasStatusControl(INFERNO_PUNCH)) {
+					for (int i = 0; i < punchesLeft; i++) {
+						if (entity.getHeldItemMainhand() == ItemStack.EMPTY) {
+							//DamageSource ds = DamageSource.LAVA;
+							DamageSource ds = DamageSource.MAGIC;
+							target.attackEntityFrom(ds, damage);
+							target.setFire(fireTime);
+							System.out.println(damage);
+							target.motionX += direction.x() * knockBack;
+							target.motionY += direction.y() * knockBack >= 0 ? knockBack / 2 + (direction.y() * knockBack / 2) : knockBack / 2;
+							target.motionZ += direction.z() * knockBack;
+							target.isAirBorne = true;
+							// this line is needed to prevent a bug where players will not be pushed in multiplayer
+							AvatarUtils.afterVelocityAdded(target);
 
-			if (ctx.getData().hasStatusControl(INFERNO_PUNCH)) {
-				if (entity.getHeldItemMainhand() == ItemStack.EMPTY) {
-					//DamageSource ds = DamageSource.LAVA;
-					DamageSource ds = DamageSource.MAGIC;
-					target.attackEntityFrom(ds, damage);
-					target.setFire(fireTime);
-					ctx.getData().removeStatusControl(INFERNO_PUNCH);
-					punchesLeft--;
-					target.motionX += direction.x() * knockBack;
-					target.motionY += direction.y() * knockBack >= 0 ? knockBack / 2 + (direction.y() * knockBack / 2) : knockBack / 2;
-					target.motionZ += direction.z() * knockBack;
-					target.isAirBorne = true;
-					// this line is needed to prevent a bug where players will not be pushed in multiplayer
-					AvatarUtils.afterVelocityAdded(target);
-					if (punchesLeft > 0) {
-						ctx.getData().addStatusControl(INFERNO_PUNCH);
+						}
 					}
+					ctx.getData().removeStatusControl(INFERNO_PUNCH);
 				}
 			}
 		}
