07a44b42e5c87c899f63d4ff3c4082c57486aa2c
FavouriteDragon <ninjadragon008@icloud.com>
2018-06-13 10:05:45 -0700

    fixed collision between your own moves for water arc

--- src/main/java/com/crowsofwar/avatar/common/entity/EntityWaterArc.java
+++ src/main/java/com/crowsofwar/avatar/common/entity/EntityWaterArc.java
@@ -244,7 +244,7 @@ public class EntityWaterArc extends EntityArc<EntityWaterArc.WaterControlPoint> 
 
 	@Override
 	protected void onCollideWithEntity(Entity entity) {
-		if (entity instanceof AvatarEntity && getBehavior() instanceof WaterArcBehavior.Thrown) {
+		if (entity instanceof AvatarEntity && getBehavior() instanceof WaterArcBehavior.Thrown && ((AvatarEntity) entity).getOwner() != getOwner()) {
 			((AvatarEntity) entity).onMinorWaterContact();
 			if (!isSpear) {
 				Splash();
