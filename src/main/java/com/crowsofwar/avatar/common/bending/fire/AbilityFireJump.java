545d9c1bb6e4faa53384325989542a3563d3f99f
FavouriteDragon <ninjadragon008@icloud.com>
2018-07-20 08:21:53 -0700

    cooldowns for firejump

--- src/main/java/com/crowsofwar/avatar/common/bending/fire/AbilityFireJump.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/fire/AbilityFireJump.java
@@ -2,12 +2,15 @@ package com.crowsofwar.avatar.common.bending.fire;
 
 import com.crowsofwar.avatar.common.bending.Ability;
 import com.crowsofwar.avatar.common.bending.StatusControl;
+import com.crowsofwar.avatar.common.data.AbilityData;
 import com.crowsofwar.avatar.common.data.Bender;
 import com.crowsofwar.avatar.common.data.BendingData;
 import com.crowsofwar.avatar.common.data.TickHandler;
 import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
 import com.crowsofwar.avatar.common.data.ctx.BendingContext;
 import com.crowsofwar.avatar.common.util.Raytrace;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 
 import static com.crowsofwar.avatar.common.bending.StatusControl.FIRE_JUMP;
 import static com.crowsofwar.avatar.common.config.ConfigStats.STATS_CONFIG;
@@ -41,6 +44,31 @@ public class AbilityFireJump extends Ability {
 			}
 
 		}
+	}
+
+	@Override
+	public int getCooldown(AbilityContext ctx) {
+		EntityLivingBase entity = ctx.getBenderEntity();
+
+		int coolDown = 120;
+
+		if (entity instanceof EntityPlayer && ((EntityPlayer) entity).isCreative()) {
+			coolDown = 0;
+		}
+
+		if (ctx.getLevel() == 1) {
+			coolDown = 110;
+		}
+		if (ctx.getLevel() == 2) {
+			coolDown = 100;
+		}
+		if (ctx.isMasterLevel(AbilityData.AbilityTreePath.FIRST)) {
+			coolDown = 80;
+		}
+		if (ctx.isMasterLevel(AbilityData.AbilityTreePath.SECOND)) {
+			coolDown = 15;
+		}
+		return coolDown;
 	}
 
 }
