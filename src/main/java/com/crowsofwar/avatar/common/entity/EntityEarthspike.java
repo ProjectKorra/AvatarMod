54b5b4db41edeb0b21b91ad36262f4e16718589c
FavouriteDragon <ninjadragon008@icloud.com>
2018-07-21 17:41:37 -0700

    adjusted knockback again

--- src/main/java/com/crowsofwar/avatar/common/entity/EntityEarthspike.java
+++ src/main/java/com/crowsofwar/avatar/common/entity/EntityEarthspike.java
@@ -161,7 +161,7 @@ public class EntityEarthspike extends AvatarEntity {
 
 	private void pushEntity(Entity entity) {
 		entity.motionX = this.motionX / 4;
-		entity.motionY = STATS_CONFIG.earthspikeSettings.push + damage/20;
+		entity.motionY = STATS_CONFIG.earthspikeSettings.push/1.5 + damage/20;
 		entity.motionZ = this.motionZ / 4;
 	}
 
