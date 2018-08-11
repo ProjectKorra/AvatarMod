0de23471c358e3c86741f177ca03534e4e22a142
FavouriteDragon <ninjadragon008@icloud.com>
2018-08-10 20:28:01 -0700

    adjusted smash knockback

--- src/main/java/com/crowsofwar/avatar/common/bending/air/SmashGroundHandler.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/air/SmashGroundHandler.java
@@ -98,7 +98,7 @@ public class SmashGroundHandler extends TickHandler {
 
 		Vector velocity = Vector.getEntityPos(target).minus(Vector.getEntityPos(entity));
 		double distance = Vector.getEntityPos(target).dist(Vector.getEntityPos(entity));
-		double direction = (getRange() - distance) * (getSpeed() / 5) / getRange();
+		double direction = (getRange() - distance) * (getSpeed() / 2) / getRange();
 		velocity = velocity.times(direction).withY(getKnockbackHeight() / 4);
 		target.addVelocity(velocity.x(), velocity.y(), velocity.z());
 	}
