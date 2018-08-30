3f2bd7a0b3881ae01a57c5379099ad0f61ef7078
FavouriteDragon <ninjadragon008@icloud.com>
2018-08-29 19:58:14 -0700

    advancements; particle stuff

--- src/main/java/com/crowsofwar/avatar/common/bending/fire/FireDevourTickHandler.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/fire/FireDevourTickHandler.java
@@ -49,32 +49,25 @@ public class FireDevourTickHandler extends TickHandler {
 
 				if (result.hitSomething() && result.getPosPrecise() != null) {
 					double position = result.getPosPrecise().dist(Vector.getEntityPos(entity));
+
 					Vector pos = Vector.getEntityPos(entity).plusY(entity.getEyeHeight());
 					if (ctx.getData().getTickHandlerDuration(this) % 5 == 0) {
 						if (position > 0) {
-							for (double h = position; h > 0; ) {
+							for (double h = 1; h > 0; ) {
 								if (world instanceof WorldServer && !world.isRemote) {
+									position = position * h;
 									WorldServer World = (WorldServer) world;
-									World.spawnParticle(EnumParticleTypes.FLAME, pos.x() + h, pos.y(), pos.z() + h, 1, 0, 0, 0, 0D);
+									World.spawnParticle(EnumParticleTypes.FLAME, pos.x() + position, pos.y(), pos.z() + position, 1, 0, 0, 0, 0D);
 								} else {
 									particles.spawnParticles(world, EnumParticleTypes.FLAME, 1, 2, pos.x() + h, pos.y(), pos.z() + h, 0, 0, 0);
 								}
 								h -= 0.1;
 							}
 						}
-						else if (position < 0) {
-							for (double h = position; h < 0; ) {
-								if (world instanceof WorldServer && !world.isRemote) {
-									WorldServer World = (WorldServer) world;
-									World.spawnParticle(EnumParticleTypes.FLAME, pos.x() + h, pos.y(), pos.z() + h, 1, 0, 0, 0, 0D);
-								} else {
-									particles.spawnParticles(world, EnumParticleTypes.FLAME, 1, 2, pos.x() + h, pos.y(), pos.z() + h, 0, 0, 0);
-								}
-								h += 0.1;
-							}
-						}
+						world.setBlockToAir(Objects.requireNonNull(result.getPosPrecise()).toBlockPos());
+
 					}
-					world.setBlockToAir(Objects.requireNonNull(result.getPosPrecise()).toBlockPos());
+
 				}
 
 			}
