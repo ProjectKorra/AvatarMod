da8c66603af69bf926322f25eb58f5c59bcff3b6
FavouriteDragon <ninjadragon008@icloud.com>
2018-08-30 20:16:15 -0700

    adjusting knockback for airbender staff

--- src/main/java/com/crowsofwar/avatar/common/item/ItemAirbenderStaff.java
+++ src/main/java/com/crowsofwar/avatar/common/item/ItemAirbenderStaff.java
@@ -1,13 +1,19 @@
 package com.crowsofwar.avatar.common.item;
 
+import com.crowsofwar.avatar.common.bending.air.AbilityAirGust;
 import com.crowsofwar.avatar.common.data.Bender;
 import com.crowsofwar.avatar.common.data.BendingData;
+import com.crowsofwar.avatar.common.entity.EntityAirGust;
 import com.crowsofwar.gorecore.util.Vector;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
 public class ItemAirbenderStaff extends Item implements AvatarItem {
@@ -27,9 +33,9 @@ public class ItemAirbenderStaff extends Item implements AvatarItem {
 
 	@Override
 	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-		Vector velocity = Vector.getLookRectangular(attacker).times(2);
+		Vector velocity = Vector.getLookRectangular(attacker).times(3);
 		target.motionX += velocity.x();
-		target.motionY += velocity.y();
+		target.motionY += velocity.y() > 0 ? velocity.y() + 0.2 : 0.3;
 		target.motionZ += velocity.z();
 		return true;
 	}
@@ -42,6 +48,18 @@ public class ItemAirbenderStaff extends Item implements AvatarItem {
 	@Override
 	public Item item() {
 		return this;
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		EntityAirGust gust = new EntityAirGust(worldIn);
+		gust.setPosition(Vector.getLookRectangular(playerIn).plus(Vector.getEntityPos(playerIn)));
+		gust.setAbility(new AbilityAirGust());
+		gust.setOwner(playerIn);
+		gust.setVelocity(Vector.getLookRectangular(playerIn).times(30));
+		worldIn.spawnEntity(gust);
+		//Add cooldown
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
 	}
 
 	@Override
