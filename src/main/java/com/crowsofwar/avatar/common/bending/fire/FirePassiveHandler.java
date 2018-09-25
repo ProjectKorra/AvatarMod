b327dedf10f625e2e0f1877e75599aad333448cd
FavouriteDragon <ninjadragon008@icloud.com>
2018-09-24 20:16:59 -0700

    added fire resistance for firebenders

---
+++ src/main/java/com/crowsofwar/avatar/common/bending/fire/FirePassiveHandler.java
@@ -0,0 +1,33 @@
+package com.crowsofwar.avatar.common.bending.fire;
+
+import com.crowsofwar.avatar.AvatarInfo;
+import com.crowsofwar.avatar.common.data.Bender;
+import com.crowsofwar.avatar.common.data.BendingData;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber(modid = AvatarInfo.MOD_ID)
+public class FirePassiveHandler {
+
+	@SubscribeEvent
+	public static void fireResistance(LivingEvent.LivingUpdateEvent event) {
+		if (event.getEntity() instanceof EntityLivingBase) {
+			EntityLivingBase entity = event.getEntityLiving();
+			Bender b = Bender.get(entity);
+			if (b != null) {
+				BendingData data = b.getData();
+				if (data != null){
+					if(data.hasBendingId(Firebending.ID)) {
+						if (entity.ticksExisted % 400 == 0) {
+							entity.addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 400));
+						}
+					}
+				}
+			}
+		}
+	}
+}
