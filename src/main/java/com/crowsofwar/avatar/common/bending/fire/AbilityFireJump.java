683b0de5b9bed3d6150d21a370a6cb01a0339123
FavouriteDragon <ninjadragon008@icloud.com>
2018-10-02 12:34:39 -0700

    hopefully reduced some lag and fixed some errors

--- src/main/java/com/crowsofwar/avatar/common/bending/fire/AbilityFireJump.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/fire/AbilityFireJump.java
@@ -5,6 +5,7 @@ import com.crowsofwar.avatar.common.bending.StatusControl;
 import com.crowsofwar.avatar.common.data.Bender;
 import com.crowsofwar.avatar.common.data.BendingData;
 import com.crowsofwar.avatar.common.data.ctx.AbilityContext;
+import com.crowsofwar.avatar.common.data.ctx.BendingContext;
 import com.crowsofwar.avatar.common.util.Raytrace;
 
 import static com.crowsofwar.avatar.common.bending.StatusControl.FIRE_JUMP;
@@ -33,10 +34,10 @@ public class AbilityFireJump extends Ability {
 			if (data.hasTickHandler(FIRE_PARTICLE_SPAWNER)) {
 				StatusControl sc = FIRE_JUMP;
 				Raytrace.Result raytrace = Raytrace.getTargetBlock(ctx.getBenderEntity(), -1);
-				/*if (FIRE_JUMP.execute(
+				if (FIRE_JUMP.execute(
 						new BendingContext(data, ctx.getBenderEntity(), ctx.getBender(), raytrace))) {
 					data.removeStatusControl(FIRE_JUMP);
-				}**/
+				}
 			}
 
 		}
