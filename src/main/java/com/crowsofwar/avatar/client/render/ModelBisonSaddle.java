8703673676057aff1ae9455b356c8a232cec7711
Mnesikos <mnesikos@gmail.com>
2018-10-13 17:30:02 -0400

    bison changes documented

--- src/main/java/com/crowsofwar/avatar/client/render/ModelBisonSaddle.java
+++ src/main/java/com/crowsofwar/avatar/client/render/ModelBisonSaddle.java
@@ -23,11 +23,11 @@ import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * BisonSaddle - Captn_Dubz & Mnesikos Created using Tabula 5.1.0
+ *
+ * Note: This model + texture were edited by Mnesikos to better fit the new Bison model altogether,
+ * heavily based on original model + texture by Captn_Dubz.
  */
 public class ModelBisonSaddle extends ModelBase {
 
@@ -88,25 +88,12 @@ public class ModelBisonSaddle extends ModelBase {
 		this.saddleBase.addChild(this.wall2);
 		this.saddleBase.addChild(this.wall1);
 		this.saddleBase.addChild(this.wall4);
-
-		// CrowsOfWar: Slightly adjust position of saddle to make it more
-		// on-center on the bison
-		/*List<ModelRenderer> allBoxes = Arrays.asList(saddleBase, wall1, wall2, wall3, wall4, cargo, wallTop,
-				wallSide1, wallSide2);
-		for (ModelRenderer box : allBoxes) {
-			box.rotationPointX += 40;
-			if (box != saddleBase) {
-				box.rotationPointX -= 40;
-			}
-		}*/
-
 	}
 
 	@Override
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
 		Minecraft.getMinecraft().renderEngine.bindTexture(texture);
 		this.saddleBase.render(f5);
-
 	}
 
 	/**
