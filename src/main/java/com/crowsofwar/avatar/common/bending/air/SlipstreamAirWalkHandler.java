a554dff3c4275aabca6bdf92f59c5426b2976d67
FavouriteDragon <ninjadragon008@icloud.com>
2019-06-12 19:53:26 -0700

    Got the path working!

--- src/main/java/com/crowsofwar/avatar/common/bending/air/SlipstreamAirWalkHandler.java
+++ src/main/java/com/crowsofwar/avatar/common/bending/air/SlipstreamAirWalkHandler.java
@@ -4,19 +4,22 @@ import com.crowsofwar.avatar.common.data.BendingData;
 import com.crowsofwar.avatar.common.data.Chi;
 import com.crowsofwar.avatar.common.data.TickHandler;
 import com.crowsofwar.avatar.common.data.ctx.BendingContext;
-import com.crowsofwar.gorecore.util.Vector;
+import com.crowsofwar.avatar.common.particle.NetworkParticleSpawner;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 import java.util.Objects;
 
-import static com.crowsofwar.gorecore.util.Vector.toRectangular;
-import static java.lang.Math.toRadians;
-
 public class SlipstreamAirWalkHandler extends TickHandler {
+	private NetworkParticleSpawner p;
+
 	public SlipstreamAirWalkHandler(int id) {
 		super(id);
+		this.p = new NetworkParticleSpawner();
 	}
 
 	@Override
@@ -24,48 +27,24 @@ public class SlipstreamAirWalkHandler extends TickHandler {
 		EntityLivingBase entity = ctx.getBenderEntity();
 		BendingData bD = ctx.getData();
 		Chi chi = bD.chi();
+		World world = ctx.getWorld();
 		boolean hasModifier = Objects.requireNonNull(bD.getPowerRatingManager(Airbending.ID)).hasModifier(SlipstreamPowerModifier.class);
 		if (hasModifier) {
 			boolean hasChi = chi.getTotalChi() > 0 && chi.getAvailableChi() > 0;
 			boolean isCreative = (entity instanceof EntityPlayer && ((EntityPlayer) entity).isCreative());
 			if (hasChi || isCreative) {
-				double targetSpeed = 1.2;
-				targetSpeed += ctx.getBender().calcPowerRating(Airbending.ID) / 400;
-				if (entity.moveForward != 0) {
-					if (entity.moveForward < 0) {
-						targetSpeed /= 2;
-					} else {
-						targetSpeed *= 1.3;
-					}
-				}
-				entity.setPosition(entity.posX, entity.posY, entity.posZ);
-				Vector currentVelocity = new Vector(entity.motionX, entity.motionY, entity.motionZ);
-				Vector targetVelocity = toRectangular(toRadians(entity.rotationYaw), 0).times(targetSpeed);
-
-				double targetWeight = 0.1;
-				currentVelocity = currentVelocity.times(1 - targetWeight);
-				targetVelocity = targetVelocity.times(targetWeight);
-
-				double targetSpeedWeight = 0.2;
-				double speed = currentVelocity.magnitude() * (1 - targetSpeedWeight)
-						+ targetSpeed * targetSpeedWeight;
-
-				Vector newVelocity = currentVelocity.plus(targetVelocity).normalize().times(speed);
-
-				Vector playerMovement = toRectangular(toRadians(entity.rotationYaw - 90),
-						toRadians(entity.rotationPitch)).times(entity.moveStrafing * 0.02);
-
-				newVelocity = newVelocity.plus(playerMovement);
-
-				entity.motionX = newVelocity.x();
-				entity.motionY = 0;
-				entity.motionZ = newVelocity.z();
+				Vec3d targetVel = entity.getLookVec().add(new Vec3d(entity.motionX, entity.motionY, entity.motionZ));
+				Vec3d currentVel = new Vec3d(entity.motionX, entity.motionY, entity.motionZ);
+				entity.setNoGravity((targetVel.y <= 0 || currentVel.y <= 0) && entity.motionY <= 0);
+				if (entity.getActivePotionEffect(MobEffects.INVISIBILITY) == null)
+					p.spawnParticles(world, EnumParticleTypes.EXPLOSION_NORMAL, 1, 2, entity.posX, entity.getEntityBoundingBox().minY - 0.05, entity.posZ,
+							0, 0, 0);
 				if (entity.ticksExisted % 5 == 0 && !isCreative) {
 					chi.setAvailableChi(chi.getAvailableChi() - 1);
 				}
 			}
 
 		}
-		return hasModifier;
+		return !hasModifier;
 	}
 }
