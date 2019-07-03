6e9f987d122790d28af8c51fd6ceaca3fe45364c
Aang23 <qwerty15@gmx.fr>
2019-07-03 21:20:07 +0200

    Fixed restore particles

--- src/main/java/com/crowsofwar/avatar/client/particles/AvatarParticleRestore.java
+++ src/main/java/com/crowsofwar/avatar/client/particles/AvatarParticleRestore.java
@@ -12,7 +12,7 @@ public class AvatarParticleRestore extends AvatarParticle {
 
 	static {
 		for (int i = 0; i < FRAMES.length; i++) {
-			FRAMES[i] = new ParticleFrame(TEXTURE, 256, (i % 4) * 64, i / 64, 64, 64);
+			FRAMES[i] = new ParticleFrame(TEXTURE, 256, 0, 7, 64, 64);
 		}
 	}
 
