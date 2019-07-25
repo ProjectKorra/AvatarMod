5d8a2c863895163ea31380ee5b5f253b19cd5dbb
Aang23 <qwerty15@gmx.fr>
2019-07-25 23:00:39 +0200

    Fix tempblock collisions

--- src/main/java/com/crowsofwar/avatar/common/blocks/BlockTemp.java
+++ src/main/java/com/crowsofwar/avatar/common/blocks/BlockTemp.java
@@ -7,6 +7,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
@@ -14,6 +15,7 @@ import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -54,6 +56,17 @@ public class BlockTemp extends Block implements ITileEntityProvider {
     @Override
     public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
         worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+        return ((TileBlockTemp) ((World) worldIn).getTileEntity(pos)).getRenderBlock().getBlock()
+                .getCollisionBoundingBox(blockState, worldIn, pos);
     }
 
     @Override
