8c4cab62b15e3fa521b62e60bc3b7b24c5f9e61e
FavouriteDragon <ninjadragon008@icloud.com>
2019-07-26 09:29:14 -0700

    trying and failing to get rotation to work

--- src/main/java/com/crowsofwar/avatar/client/render/RenderWaterBubble.java
+++ src/main/java/com/crowsofwar/avatar/client/render/RenderWaterBubble.java
@@ -58,19 +58,18 @@ public class RenderWaterBubble extends Render<EntityWaterBubble> {
 		mat.translate((float) x - 0.5f, (float) y * bubble.getSize(), (float) z - 0.5f);
 
 		//TIL that putting the rotation code here makes it orbit around you. Very cool, but not the intended effect xD
-		float rotation = ticks * 20F;
-		GlStateManager.rotate(rotation * 0.2F, 1, 0, 0);
-		GlStateManager.rotate(rotation, 0, 1, 0);
-		GlStateManager.rotate(rotation * 0.2F, 0, 0, 1);
+		//Using mul.rotate instead of GlStateManager does the same thing to the entity, BUT it just spins the water bubble around a corner
 
 		//TODO: Add rotations, size scaling, and wobbling (only wobble when degrees per second is 0)
 
 		// (0 Left)/(1 Right), (0 Bottom)/(1 Top), (0 Front)/(1 Back)
 		//Vector4f mid = new Vector4f((float) x, (float) y + .5f, (float) z, 1);
 
-		//mat.rotate(ticks / 25f * bubble.getDegreesPerSecond(), 1, 0, 0);
-		//mat.rotate(ticks / 25f * bubble.getDegreesPerSecond(), 0, 1, 0);
-		//mat.rotate(ticks / 25f * bubble.getDegreesPerSecond(), 0, 0, 1);
+
+		mat.rotate(ticks / 25f * bubble.getDegreesPerSecond(), 1, 0, 0);
+		mat.rotate(ticks / 25f * bubble.getDegreesPerSecond(), 0, 1, 0);
+		mat.rotate(ticks / 25f * bubble.getDegreesPerSecond(), 0, 0, 1);
+
 
 		// @formatter:off
 		Vector4f
@@ -83,9 +82,11 @@ public class RenderWaterBubble extends Render<EntityWaterBubble> {
 				ltb = new Vector4f(0, 1, 1, 1).mul(mat),
 				rtb = new Vector4f(1, 1, 1, 1).mul(mat);
 
+
 		float t1 = ticks * (float) Math.PI / 10f;
 		float t2 = t1 + (float) Math.PI / 2f;
 		float amt = .05f;
+		//lbf.rotate()
 
 		lbf.add(cos(t1) * amt, sin(t2) * amt, cos(t2) * amt, 0);
 		rbf.add(sin(t1) * amt, cos(t2) * amt, sin(t2) * amt, 0);
@@ -99,6 +100,8 @@ public class RenderWaterBubble extends Render<EntityWaterBubble> {
 
 		// @formatter:on
 
+
+
 		float existed = ticks / 4f;
 		int anim = ((int) existed % 16);
 		float v1 = anim / 16f, v2 = v1 + 1f / 16;
@@ -109,6 +112,12 @@ public class RenderWaterBubble extends Render<EntityWaterBubble> {
 		drawQuad(2, rtb, rtf, ltf, ltb, 0, v1, 1, v2); // +y
 		drawQuad(2, rtf, rbf, lbf, ltf, 0, v1, 1, v2); // -z
 		drawQuad(2, rtb, rbb, lbb, ltb, 0, v1, 1, v2); // +z
+
+		float rotation = ticks * 20F;
+		GlStateManager.rotate(rotation * 0.2F, 1, 0, 0);
+		GlStateManager.rotate(rotation, 0, 1, 0);
+		GlStateManager.rotate(rotation * 0.2F, 0, 0, 1);
+
 
 
 		GlStateManager.color(1, 1, 1, 1);
