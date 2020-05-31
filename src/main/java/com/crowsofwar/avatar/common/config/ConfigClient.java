db2b72149719148846d2724151254c5390b31bce
FavouriteDragon <ninjadragon008@icloud.com>
2020-05-31 10:15:40 -0700

    uh- I fixed chi? w h a t

--- src/main/java/com/crowsofwar/avatar/common/config/ConfigClient.java
+++ src/main/java/com/crowsofwar/avatar/common/config/ConfigClient.java
@@ -189,7 +189,7 @@ public class ConfigClient {
 		public final double widthScale = 1.1F, heightScale = 1.1F;
 
 		@Load
-		public final int xPos = 120, yPos = 3;
+		public final int xPos = 115, yPos = 3;
 
 	}
 
