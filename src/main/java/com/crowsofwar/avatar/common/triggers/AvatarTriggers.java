40295de1f839c54a828e6922e1e6934dea7d3bb1
Aidan <aidanpauldavis@yahoo.co.uk>
2020-07-10 00:55:35 +0100

    staching changes

--- src/main/java/com/crowsofwar/avatar/common/triggers/AvatarTriggers.java
+++ src/main/java/com/crowsofwar/avatar/common/triggers/AvatarTriggers.java
@@ -5,13 +5,11 @@ import net.minecraft.advancements.ICriterionTrigger;
 
 public class AvatarTriggers {
     public static final UnlockBendingTrigger UNLOCK_ELEMENT = new UnlockBendingTrigger("unlock_bending");
-    public static final UnlockAbilityTrigger ABILITY_USE = new UnlockAbilityTrigger("unlock_ability");
     public static final LevelAbilityTrigger ABILITY_LEVEL = new LevelAbilityTrigger("level_ability");
     public static final ICriterionTrigger ELEMENT_RANKUP = new UnlockBendingTrigger("");
 
     public static void init() {
         CriteriaTriggers.register(UNLOCK_ELEMENT);
-        CriteriaTriggers.register(ABILITY_USE);
         CriteriaTriggers.register(ABILITY_LEVEL);
     }
 }
