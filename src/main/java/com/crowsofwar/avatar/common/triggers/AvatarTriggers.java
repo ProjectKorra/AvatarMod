39317055a96b4fcd3207f57ee77e6d656c391b04
Aidan <aidanpauldavis@yahoo.co.uk>
2020-07-12 14:05:40 +0100

    continued trigger

--- src/main/java/com/crowsofwar/avatar/common/triggers/AvatarTriggers.java
+++ src/main/java/com/crowsofwar/avatar/common/triggers/AvatarTriggers.java
@@ -7,6 +7,7 @@ public class AvatarTriggers {
     public static final UnlockBendingTrigger UNLOCK_ELEMENT = new UnlockBendingTrigger("unlock_bending");
     public static final LevelAbilityTrigger ABILITY_LEVEL = new LevelAbilityTrigger("level_ability");
     public static final UseAbilityTrigger ABILITY_USE = new UseAbilityTrigger("use_ability");
+    //TODO: [AD] Discuss how this one should be done, is there a total levels per element we can hook into?
     public static final ICriterionTrigger ELEMENT_RANKUP = new UnlockBendingTrigger("");
 
     public static void init() {
