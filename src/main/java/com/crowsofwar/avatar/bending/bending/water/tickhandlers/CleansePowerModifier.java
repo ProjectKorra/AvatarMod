ee9d0876754a1a6e32ea8ea6dc2b4d16eabb5889
John <ninjadragon008@icloud.com>
2021-06-20 16:37:03 -0700

    pain

--- src/main/java/com/crowsofwar/avatar/bending/bending/water/tickhandlers/CleansePowerModifier.java
+++ src/main/java/com/crowsofwar/avatar/bending/bending/water/tickhandlers/CleansePowerModifier.java
@@ -1,43 +1,62 @@
 package com.crowsofwar.avatar.bending.bending.water.tickhandlers;
 
+import com.crowsofwar.avatar.bending.bending.Abilities;
+import com.crowsofwar.avatar.bending.bending.Ability;
 import com.crowsofwar.avatar.bending.bending.BuffPowerModifier;
+import com.crowsofwar.avatar.bending.bending.water.AbilityCleanse;
+import com.crowsofwar.avatar.client.particle.ParticleBuilder;
 import com.crowsofwar.avatar.util.data.AbilityData;
 import com.crowsofwar.avatar.util.data.BendingData;
 import com.crowsofwar.avatar.util.data.Vision;
 import com.crowsofwar.avatar.util.data.ctx.BendingContext;
+import net.minecraft.world.World;
 
 public class CleansePowerModifier extends BuffPowerModifier {
 
-	@Override
-	public double get(BendingContext ctx) {
+    @Override
+    public double get(BendingContext ctx) {
 
-		BendingData data = ctx.getData();
-		AbilityData abilityData = data.getAbilityData("cleanse");
+        BendingData data = ctx.getData();
+        AbilityData abilityData = data.getAbilityData("cleanse");
+        AbilityCleanse cleanse = (AbilityCleanse) Abilities.get("cleanse");
 
-		double modifier = 50;
-		if (abilityData.getLevel() >= 2) {
-			modifier = 70;
-		}
-		if (abilityData.isMasterPath(AbilityData.AbilityTreePath.SECOND)) {
-			modifier = 100;
-		}
+        double modifier = 50;
+        if (cleanse != null) {
+            modifier = cleanse.getProperty(Ability.POWERRATING, abilityData).floatValue();
+        }
+        return modifier;
 
-		return modifier;
+    }
 
-	}
+    @Override
+    protected Vision[] getVisions() {
+        //if (CLIENT_CONFIG.shaderSettings.useCleanseShaders) {
+        return new Vision[]{Vision.CLEANSE_WEAK, Vision.CLEANSE_MEDIUM, Vision.CLEANSE_POWERFUL};
+        //}
+        //else return null;
+    }
 
-	@Override
-	protected Vision[] getVisions() {
-		//if (CLIENT_CONFIG.shaderSettings.useCleanseShaders) {
-		return new Vision[]{Vision.CLEANSE_WEAK, Vision.CLEANSE_MEDIUM, Vision.CLEANSE_POWERFUL};
-		//}
-		//else return null;
-	}
+    @Override
+    public boolean onUpdate(BendingContext ctx) {
+        //Particle time
+        //Swirl with cubes and flash
+        AbilityCleanse cleanse = (AbilityCleanse) Abilities.get("cleanse");
+        World world = ctx.getWorld();
+        if (cleanse != null) {
+            float radius = cleanse.getProperty(Ability.RADIUS, AbilityData.get(ctx.getBenderEntity(),
+                    getAbilityName())).floatValue();
+            if (world.isRemote) {
+                ParticleBuilder.create(ParticleBuilder.Type.CUBE);
+                ParticleBuilder.create(ParticleBuilder.Type.FLASH);
+            }
+        }
+        return super.onUpdate(ctx);
+    }
 
-	@Override
-	protected String getAbilityName() {
-		return "cleanse";
-	}
+    @Override
+    protected String getAbilityName() {
+        return "cleanse";
+    }
 
 }
 
