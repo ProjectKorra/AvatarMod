3c97f54f94833a747c5780edd654efe512f93146
John <ninjadragon008@icloud.com>
2021-08-10 14:21:22 -0700

    working on frost claws

--- src/main/java/com/crowsofwar/avatar/bending/bending/ice/AbilityFrostClaws.java
+++ src/main/java/com/crowsofwar/avatar/bending/bending/ice/AbilityFrostClaws.java
@@ -1,11 +1,25 @@
 package com.crowsofwar.avatar.bending.bending.ice;
 
 import com.crowsofwar.avatar.bending.bending.Ability;
+import com.crowsofwar.avatar.bending.bending.BendingStyle;
+import com.crowsofwar.avatar.util.data.ctx.AbilityContext;
 
 public class AbilityFrostClaws extends Ability {
 
     public AbilityFrostClaws() {
         super(Icebending.ID, "frost_claws");
+    }
+
+    @Override
+    public void execute(AbilityContext ctx) {
+
+        
+        super.execute(ctx);
+    }
+
+    @Override
+    public void init() {
+        super.init();
     }
 
     @Override
