faa46cb109d8e16e4ef214f22c4e5ee3670aba5c
FavouriteDragon <ninjadragon008@icloud.com>
2022-03-23 12:08:46 +1100

    fixed cleanse

--- src/main/java/com/crowsofwar/avatar/bending/bending/water/AbilityCleanse.java
+++ src/main/java/com/crowsofwar/avatar/bending/bending/water/AbilityCleanse.java
@@ -54,6 +54,7 @@ public class AbilityCleanse extends Ability {
                 SATURATION_DURATION, SATURATION_LEVEL, RADIUS, REGEN_DURATION, REGEN_LEVEL,
                 INSTANT_HEALTH_LEVEL, SOURCE_RANGE, SOURCE_ANGLES, HIGHEST_LEVEL_CLEANSED, WATER_LEVEL,
                 ABSORPTION_DURATION, ABSORPTION_LEVEL);
+        addBooleanProperties(PLANT_BEND);
     }
 
     @Override
